26809c253db69b7a67ab555a4f7b29278f5fc5c8
Jay Adkisson <jjmadkisson@gmail.com>
2012-09-01 01:59:05 +0200

    spec the autofilling

--- spec/theme_spec.rb
+++ spec/theme_spec.rb
@@ -1,3 +1,17 @@
 describe Rouge::Theme do
-  # TODO
+  it 'auto-fills css classes' do
+    class MyTheme < Rouge::CSSTheme
+      style 'Literal.String', :bold => true
+      style 'Literal.String.Backtick', :italic => true
+    end
+
+    rendered = MyTheme.new.render
+
+    # should also style, for example, String.Char
+    assert { rendered =~ /\.sc/ }
+
+    # and it should only style String.Backtick once
+    assert { rendered =~ /\.sb/ }
+    assert { $~.size == 1 }
+  end
 end

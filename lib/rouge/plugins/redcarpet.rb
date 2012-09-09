ddfbf334fff2796786bf28aadc0cc924a3acd168
Jay Adkisson <jjmadkisson@gmail.com>
2012-09-09 22:16:51 +0100

    allow passing options with ? parameters in markdown

--- lib/rouge/plugins/redcarpet.rb
+++ lib/rouge/plugins/redcarpet.rb
@@ -5,12 +5,31 @@
 # this plugin depends on redcarpet
 require 'redcarpet'
 
+# stdlib
+require 'cgi'
+
 module Rouge
   module Plugins
     module Redcarpet
       def block_code(code, language)
-        lexer = Lexer.find(language) || Lexers::Text
-        formatter = Formatters::HTML.new(:css_class => "highlight #{lexer.tag}")
+        name, opts = language.split('?')
+
+        # parse the options hash from a cgi-style string
+        opts = CGI.parse(opts || '').map do |k, vals|
+          [ k.to_sym, vals.empty? ? true : vals[0] ]
+        end
+
+        opts = Hash[opts]
+
+        lexer_class = case name
+        when 'guess', nil
+          lexer = Lexer.guess(:source => code, :mimetype => opts[:mimetype])
+        when String
+          Lexer.find(name)
+        end || Lexers::Text
+
+        lexer = lexer_class.new(opts)
+        formatter = Formatters::HTML.new(:css_class => "highlight #{lexer_class.tag}")
 
         Rouge.highlight(code, lexer, formatter)
       end
--- spec/plugins/redcarpet_spec.rb
+++ spec/plugins/redcarpet_spec.rb
@@ -24,4 +24,25 @@ foo=1
     assert { result.include?(%<<pre class="highlight shell">>) }
     assert { result.include?(%<<pre class="highlight javascript">>) }
   end
+
+  it 'guesses' do
+    result = markdown.render <<-mkd
+``` guess
+<xml>an xml code block</xml>
+```
+    mkd
+
+    assert { result.include?(%<<pre class="highlight xml">>) }
+  end
+
+  it 'passes options' do
+    result = markdown.render <<-mkd
+``` shell?k=v
+foo=1
+```
+    mkd
+
+    # TODO: test that an option is actually there
+    assert { result.include?(%<<pre class="highlight shell">>) }
+  end
 end

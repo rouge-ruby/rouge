5afd33119903dd374c2442ee7ffdcfc93acda065
Jay Adkisson <jjmadkisson@gmail.com>
2012-09-15 09:23:44 +0100

    properly highlight <!DOCTYPE .*?> in html

--- lib/rouge/lexers/html.rb
+++ lib/rouge/lexers/html.rb
@@ -13,6 +13,7 @@ module Rouge
       state :root do
         rule /[^<&]+/m, 'Text'
         rule /&\S*?;/, 'Name.Entity'
+        rule /<!DOCTYPE .*?>/i, 'Comment.Preproc'
         rule /<!\[CDATA\[.*?\]\]>/m, 'Comment.Preproc'
         rule /<!--/, 'Comment', :comment
         rule /<\?.*?\?>/m, 'Comment.Preproc' # php? really?

cdf26afeac303fe8d867223b23ab9d51bf99c339
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-02 13:37:22 +0100

    refactor Support::Lexing
    add {assert,deny}_has_token

--- spec/support/lexing.rb
+++ spec/support/lexing.rb
@@ -1,17 +1,25 @@
 module Support
   module Lexing
-    def assert_no_errors(text, lexer=nil)
+    def filter_by_token(tokname, text, lexer=nil)
       lexer ||= subject
 
       tokens = lexer.lex(text)
 
-      errors = tokens.select do |(tok, val)|
-        tok.name.include? 'Error'
+      tokens.select do |(tok, val)|
+        tok.name.start_with? tokname
       end
+    end
 
-      assert { errors.empty? }
+    def deny_has_token(tokname, text, lexer=nil)
+      deny { filter_by_token(tokname, text, lexer).any? }
+    end
 
-      tokens
+    def assert_has_token(tokname, text, lexer=nil)
+      assert { filter_by_token(tokname, text, lexer).any? }
+    end
+
+    def assert_no_errors(*a)
+      deny_has_token('Error', *a)
     end
   end
 end

357859e9a42496f14779673861a490d9e497b777
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-09 17:28:04 +0100

    add a Factor lexer (closes #21)

--- lib/rouge.rb
+++ lib/rouge.rb
@@ -41,6 +41,7 @@ load load_dir.join('rouge/lexers/tcl.rb')
 load load_dir.join('rouge/lexers/python.rb')
 load load_dir.join('rouge/lexers/ruby.rb')
 load load_dir.join('rouge/lexers/perl.rb')
+load load_dir.join('rouge/lexers/factor.rb')
 
 load load_dir.join('rouge/lexers/haskell.rb')
 load load_dir.join('rouge/lexers/scheme.rb')
---
+++ spec/lexers/factor_spec.rb
@@ -0,0 +1,19 @@
+describe Rouge::Lexers::Ruby do
+  let(:subject) { Rouge::Lexers::Factor.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.factor'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'text/x-factor'
+    end
+
+    it 'guesses by source' do
+      assert_guess :source => '#!/usr/local/bin/factor'
+    end
+  end
+end

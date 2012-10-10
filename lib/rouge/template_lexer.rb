aaf4490a89a2f3cc591415be873b6691dcdb6cc7
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-10 19:16:12 +0100

    document TemplateLexer

--- lib/rouge/template_lexer.rb
+++ lib/rouge/template_lexer.rb
@@ -1,5 +1,11 @@
 module Rouge
+  # @abstract
+  # A TemplateLexer is one that accepts a :parent option, to specify
+  # which language is being templated.  The lexer class can specify its
+  # own default for the parent lexer, which is otherwise defaulted to
+  # HTML.
   class TemplateLexer < RegexLexer
+    # the parent lexer - the one being templated.
     def parent
       return @parent if instance_variable_defined? :@parent
       @parent = option(:parent) || 'html'

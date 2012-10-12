62685d0c863b848cefa09e747e97c8e48babe7ed
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-12 13:53:57 +0100

    add a clojure lexer

--- lib/rouge.rb
+++ lib/rouge.rb
@@ -50,6 +50,7 @@ load load_dir.join('rouge/lexers/python.rb')
 load load_dir.join('rouge/lexers/ruby.rb')
 load load_dir.join('rouge/lexers/perl.rb')
 load load_dir.join('rouge/lexers/factor.rb')
+load load_dir.join('rouge/lexers/clojure.rb')
 
 load load_dir.join('rouge/lexers/haskell.rb')
 load load_dir.join('rouge/lexers/scheme.rb')
---
+++ spec/lexers/clojure_spec.rb
@@ -0,0 +1,16 @@
+describe Rouge::Lexers::Clojure do
+  let(:subject) { Rouge::Lexers::Clojure.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.clj'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'text/x-clojure'
+      assert_guess :mimetype => 'application/x-clojure'
+    end
+  end
+end

5f4bc12b5bd1e2093b49ec4daad84777448744f7
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-18 19:18:24 +0100

    show the demos in the root of the test app

--- spec/visual/app.rb
+++ spec/visual/app.rb
@@ -15,12 +15,15 @@ class VisualTestApp < Sinatra::Application
     set :views, BASE.join('templates')
   end
 
+  before do
+    theme_class = Rouge::Theme.find(params[:theme] || 'thankful_eyes')
+    halt 404 unless theme_class
+    @theme = theme_class.new
+  end
+
   get '/:lexer' do |lexer_name|
     lexer_class = Rouge::Lexer.find(lexer_name)
     halt 404 unless lexer_class
-    theme_class = Rouge::Theme.find(params[:theme] || 'thankful_eyes')
-    halt 404 unless theme_class
-
     @sample = File.read(SAMPLES.join(lexer_class.tag))
 
     lexer_options = {}
@@ -30,7 +33,6 @@ class VisualTestApp < Sinatra::Application
 
     formatter = Rouge::Formatters::HTML.new
     @lexer = lexer_class.new(lexer_options)
-    @theme = theme_class.new
     @highlighted = Rouge.highlight(@sample, @lexer, formatter)
 
     erb :lexer
@@ -39,6 +41,8 @@ class VisualTestApp < Sinatra::Application
 
   get '/' do
     @samples = SAMPLES.entries.reject { |s| s.basename.to_s =~ /^\.|~$/ }
+    @samples.map!(&Rouge::Lexer.method(:find))
+
     erb :index
   end
 end

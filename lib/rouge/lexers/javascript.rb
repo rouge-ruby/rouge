88486614b6178f861faaf3bd68133df63cfcb22e
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-19 22:45:06 +0100

    javascript: don't use massive interpolated regexes

--- lib/rouge/lexers/javascript.rb
+++ lib/rouge/lexers/javascript.rb
@@ -46,29 +46,40 @@ module Rouge
         rule /[^\n]+/, 'Error', :pop!
       end
 
-      keywords = %w(
-        for in while do break return continue switch case default if else
-        throw try catch finally new delete typeof instanceof void this
-      ).join('|')
+      def self.keywords
+        @keywords ||= Set.new %w(
+          for in while do break return continue switch case default
+          if else throw try catch finally new delete typeof instanceof
+          void this
+        )
+      end
 
-      declarations = %w(var let with function).join('|')
+      def self.declarations
+        @declarations ||= Set.new %w(var let with function)
+      end
 
-      reserved = %w(
-        abstract boolean byte char class const debugger double enum export
-        extends final float goto implements import int interface long
-        native package private protected public short static super
-        synchronized throws transient volatile
-      ).join('|')
+      def self.reserved
+        @reserved ||= Set.new %w(
+          abstract boolean byte char class const debugger double enum
+          export extends final float goto implements import int interface
+          long native package private protected public short static
+          super synchronized throws transient volatile
+        )
+      end
 
-      constants = %w(true false null NaN Infinity undefined).join('|')
+      def self.constants
+        @constants ||= Set.new %w(true false null NaN Infinity undefined)
+      end
 
-      builtins = %w(
-        Array Boolean Date Error Function Math netscape
-        Number Object Packages RegExp String sun decodeURI
-        decodeURIComponent encodeURI encodeURIComponent
-        Error eval isFinite isNaN parseFloat parseInt document this
-        window
-      ).join('|')
+      def self.builtins
+        @builtins ||= %w(
+          Array Boolean Date Error Function Math netscape
+          Number Object Packages RegExp String sun decodeURI
+          decodeURIComponent encodeURI encodeURIComponent
+          Error eval isFinite isNaN parseFloat parseInt document this
+          window
+        )
+      end
 
       state :root do
         rule /\A\s*#!.*?\n/m, 'Comment.Preproc'
@@ -80,12 +91,23 @@ module Rouge
         rule %r([-<>+*%&|\^/!=]=?), 'Operator', :slash_starts_regex
         rule /[{(\[;,]/, 'Punctuation', :slash_starts_regex
         rule /[})\].]/, 'Punctuation'
-        rule /(?:#{keywords})\b/, 'Keyword', :slash_starts_regex
-        rule /(?:#{declarations})\b/, 'Keyword.Declaration', :slash_starts_regex
-        rule /(?:#{reserved})\b/, 'Keyword.Reserved'
-        rule /(?:#{constants})\b/, 'Keyword.Constant'
-        rule /(?:#{builtins})\b/, 'Name.Builtin'
-        rule /[$a-zA-Z_][a-zA-Z0-9_]*/, 'Name.Other'
+        rule /[$a-zA-Z_][a-zA-Z0-9_]*/ do |m|
+          if self.class.keywords.include? m[0]
+            token 'Keyword'
+            push :slash_starts_regex
+          elsif self.class.declarations.include? m[0]
+            token 'Keyword.Declaration'
+            push :slash_starts_regex
+          elsif self.class.reserved.include? m[0]
+            token 'Keyword.Reserved'
+          elsif self.class.constants.include? m[0]
+            token 'Keyword.Constant'
+          elsif self.class.builtins.include? m[0]
+            token 'Name.Builtin'
+          else
+            token 'Name.Other'
+          end
+        end
 
         rule /[0-9][0-9]*\.[0-9]+([eE][0-9]+)?[fd]?/, 'Literal.Number.Float'
         rule /0x[0-9a-fA-F]+/, 'Literal.Number.Hex'

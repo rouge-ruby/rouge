a01f8b8cab32c1c727c306e904fab666eea31727
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-27 21:38:45 +0100

    move the badRegex thing to the automated specs
    the visual tests shouldn't have errors.

--- spec/lexers/javascript_spec.rb
+++ spec/lexers/javascript_spec.rb
@@ -1,6 +1,15 @@
 describe Rouge::Lexers::Javascript do
   let(:subject) { Rouge::Lexers::Javascript.new }
 
+  describe 'lexing' do
+    include Support::Lexing
+
+    it %(doesn't let a bad regex mess up the whole lex) do
+      assert_has_token 'Error',          "var a = /foo;\n1"
+      assert_has_token 'Literal.Number', "var a = /foo;\n1"
+    end
+  end
+
   describe 'guessing' do
     include Support::Guessing
 

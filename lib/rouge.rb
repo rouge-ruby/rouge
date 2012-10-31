fe0ddf1d45cf58cff935880d6fe96dff5fee091b
Jay Adkisson <jjmadkisson@gmail.com>
2012-10-31 10:08:38 +0100

    document Rouge.highlight #doc

--- lib/rouge.rb
+++ lib/rouge.rb
@@ -3,6 +3,16 @@ require 'pathname'
 
 module Rouge
   class << self
+    # Highlight some text with a given lexer and formatter.
+    #
+    # @example
+    #   Rouge.highlight('@foo = 1', 'ruby', 'html')
+    #   Rouge.highlight('var foo = 1;', 'js', 'terminal256')
+    #
+    #   # streaming - chunks become available as they are lexed
+    #   Rouge.highlight(large_string, 'ruby', 'html') do |chunk|
+    #     $stdout.print chunk
+    #   end
     def highlight(text, lexer, formatter, &b)
       lexer = Lexer.find(lexer) unless lexer.respond_to? :lex
       raise "unknown lexer #{lexer}" unless lexer

7e4accd83ad6243e41adf8f4733e4185a07d69d3
Jay Adkisson <jjmadkisson@gmail.com>
2012-11-26 20:30:27 +0100

    add a lexer for sed

--- lib/rouge.rb
+++ lib/rouge.rb
@@ -51,6 +51,7 @@ load load_dir.join('rouge/lexers/shell.rb')
 load load_dir.join('rouge/lexers/viml.rb')
 load load_dir.join('rouge/lexers/nginx.rb')
 load load_dir.join('rouge/lexers/conf.rb')
+load load_dir.join('rouge/lexers/sed.rb')
 
 load load_dir.join('rouge/lexers/javascript.rb')
 load load_dir.join('rouge/lexers/css.rb')

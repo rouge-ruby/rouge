e2bb2b2942c9b4d651c89f2fc2c8d35bc43cf8ad
Michael Baudino <michael.baudino@alpine-lab.com>
2013-03-12 17:07:36 +0100

    Added CSS3 attributes to CSS Lexer

--- lib/rouge/lexers/css.rb
+++ lib/rouge/lexers/css.rb
@@ -12,32 +12,102 @@ module Rouge
 
       def self.attributes
         @attributes ||= Set.new %w(
-          azimuth background background-attachment background-color
-          background-image background-position background-repeat
-          border border-bottom border-bottom-color border-bottom-style
-          border-bottom-width border-collapse border-color border-left
-          border-left-color border-left-style border-left-width
-          border-right border-right-color border-right-style
-          border-right-width border-spacing border-style border-top
-          border-top-color border-top-style border-top-width
-          border-width bottom caption-side clear clip color content
-          counter-increment counter-reset cue cue-after cue-before cursor
-          direction display elevation empty-cells float font font-family
-          font-size font-size-adjust font-stretch font-style font-variant
-          font-weight height left letter-spacing line-height list-style
-          list-style-image list-style-position list-style-type margin
-          margin-bottom margin-left margin-right margin-top marker-offset
-          marks max-height max-width min-height min-width opacity orphans
-          outline outline-color outline-style outline-width overflow-x
-          overflow-y padding padding-bottom padding-left padding-right
-          padding-top page page-break-after page-break-before
-          page-break-inside pause pause-after pause-before pitch
-          pitch-range play-during position quotes richness right size
-          speak speak-header speak-numeral speak-punctuation speech-rate
-          src stress table-layout text-align text-decoration text-indent
-          text-shadow text-transform top unicode-bidi vertical-align
-          visibility voice-family volume white-space widows width
-          word-spacing z-index
+          align-content align-items align-self alignment-adjust
+          alignment-baseline all anchor-point animation
+          animation-delay animation-direction animation-duration
+          animation-fill-mode animation-iteration-count animation-name
+          animation-play-state animation-timing-function appearance
+          azimuth backface-visibility background background-attachment
+          background-clip background-color background-image
+          background-origin background-position background-repeat
+          background-size baseline-shift binding bleed bookmark-label
+          bookmark-level bookmark-state bookmark-target border
+          border-bottom border-bottom-color border-bottom-left-radius
+          border-bottom-right-radius border-bottom-style
+          border-bottom-width border-collapse border-color
+          border-image border-image-outset border-image-repeat
+          border-image-slice border-image-source border-image-width
+          border-left border-left-color border-left-style
+          border-left-width border-radius border-right
+          border-right-color border-right-style border-right-width
+          border-spacing border-style border-top border-top-color
+          border-top-left-radius border-top-right-radius
+          border-top-style border-top-width border-width bottom
+          box-align box-decoration-break box-direction box-flex
+          box-flex-group box-lines box-ordinal-group box-orient
+          box-pack box-shadow box-sizing break-after break-before
+          break-inside caption-side @charset clear clip clip-path
+          clip-rule color color-profile columns column-count
+          column-fill column-gap column-rule column-rule-color
+          column-rule-style column-rule-width column-span
+          column-width content counter-increment counter-reset
+          crop cue cue-after cue-before cursor direction display
+          dominant-baseline drop-initial-after-adjust
+          drop-initial-after-align drop-initial-before-adjust
+          drop-initial-before-align drop-initial-size
+          drop-initial-value elevation empty-cells filter fit
+          fit-position flex flex-basis flex-direction flex-flow
+          flex-grow flex-shrink flex-wrap float float-offset
+          font @font-face font-family font-feature-settings
+          font-kerning font-language-override font-size
+          font-size-adjust font-stretch font-style font-synthesis
+          font-variant font-variant-alternates font-variant-caps
+          font-variant-east-asian font-variant-ligatures
+          font-variant-numeric font-variant-position font-weight
+          grid-cell grid-column grid-column-align grid-column-sizing
+          grid-column-span grid-columns grid-flow grid-row
+          grid-row-align grid-row-sizing grid-row-span
+          grid-rows grid-template hanging-punctuation height
+          hyphenate-after hyphenate-before hyphenate-character
+          hyphenate-lines hyphenate-resource hyphens icon
+          image-orientation image-rendering image-resolution
+          ime-mode @import inline-box-align justify-content
+          @keyframes left letter-spacing line-break line-height
+          line-stacking line-stacking-ruby line-stacking-shift
+          line-stacking-strategy list-style list-style-image
+          list-style-position list-style-type margin
+          margin-bottom margin-left margin-right margin-top
+          mark marker-offset marks mark-after mark-before
+          marquee-direction marquee-loop marquee-play-count
+          marquee-speed marquee-style mask max-height max-width
+          @media min-height min-width move-to @namespace nav-down
+          nav-index nav-left nav-right nav-up object-fit
+          object-position opacity order orphans outline
+          outline-color outline-offset outline-style
+          outline-width overflow overflow-style overflow-wrap
+          overflow-x overflow-y padding padding-bottom
+          padding-left padding-right padding-top @page
+          page page-break-after page-break-before
+          page-break-inside page-policy pause pause-after
+          pause-before perspective perspective-origin
+          phonemes pitch pitch-range play-during pointer-events
+          position presentation-level punctuation-trim quotes
+          rendering-intent resize rest rest-after rest-before
+          richness right rotation rotation-point ruby-align
+          ruby-overhang ruby-position ruby-span size speak
+          speak-as speak-header speak-numeral speak-punctuation
+          speech-rate src stress string-set @supports
+          tab-size table-layout target target-name
+          target-new target-position text-align
+          text-align-last text-combine-horizontal
+          text-decoration text-decoration-color
+          text-decoration-line text-decoration-skip
+          text-decoration-style text-emphasis
+          text-emphasis-color text-emphasis-position
+          text-emphasis-style text-height text-indent
+          text-justify text-orientation text-outline
+          text-overflow text-rendering text-shadow
+          text-space-collapse text-transform
+          text-underline-position text-wrap top
+          transform transform-origin transform-style
+          transition transition-delay transition-duration
+          transition-property transition-timing-function
+          unicode-bidi vertical-align @viewport
+          visibility voice-balance voice-duration
+          voice-family voice-pitch voice-pitch-range
+          voice-range voice-rate voice-stress voice-volume
+          volume white-space widows width word-break
+          word-spacing word-wrap writing-mode z-index
         )
       end
 

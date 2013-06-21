d5559d285d5e19e2fa2d2f486aae869969854713
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-06-21 10:54:06 -0700

    use the builtin Wrong adapter #specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,9 @@ Bundler.require
 
 require 'rouge'
 require 'minitest/spec'
+require 'wrong/adapters/minitest'
 
 Wrong.config[:color] = true
-
-class MiniTest::Unit::TestCase
-  include Wrong
-end
 
 Dir[File.expand_path('support/**/*.rb', File.dirname(__FILE__))].each {|f|
   require f

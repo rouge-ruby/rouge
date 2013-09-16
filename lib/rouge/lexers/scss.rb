254abe65aa49536e53ecd27f45f8e888793bb3a5
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-09-16 13:53:07 -0700

    make SassCommon a shared subclass

--- lib/rouge/lexers/scss.rb
+++ lib/rouge/lexers/scss.rb
@@ -2,7 +2,7 @@ require 'rouge/lexers/sass/common'
 
 module Rouge
   module Lexers
-    class Scss < RegexLexer
+    class Scss < SassCommon
       desc "SCSS stylesheets (sass-lang.com)"
       tag 'scss'
       filenames '*.scss'
@@ -26,8 +26,6 @@ module Rouge
         rule /\n/, Text
         rule(/[;{}]/) { token Punctuation; reset_stack }
       end
-
-      include SassCommon
     end
   end
 end

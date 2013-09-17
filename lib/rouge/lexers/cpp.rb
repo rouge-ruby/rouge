02a50ea0a7776b3db1eb307ccea94978930b2e6f
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-09-16 23:37:19 -0700

    use `super` in C++ keyword defs

--- lib/rouge/lexers/cpp.rb
+++ lib/rouge/lexers/cpp.rb
@@ -13,35 +13,19 @@ module Rouge
       mimetypes 'text/x-c++hdr', 'text/x-c++src'
 
       def self.keywords
-        @keywords ||= Set.new %w(
-          asm auto break case catch const const_cast continue
-          default delete do dynamic_cast else enum explicit export
-          extern for friend goto if mutable namespace new operator
-          private protected public register reinterpret_cast return
-          restrict sizeof static static_cast struct switch template
-          this throw throws try typedef typeid typename union using
-          volatile virtual while
-        )
-      end
-
-      def self.keywords_type
-        @keywords_type ||= Set.new %w(
-          bool int long float short double char unsigned signed void wchar_t
-        )
+        @keywords ||= super + Set.new(%w(
+          asm catch const_cast delete dynamic_cast explicit export
+          friend mutable namespace new operator private protected public
+          reinterpret_cast restrict static_cast template this throw
+          throws typeid typename using virtual
+        ))
       end
 
       def self.reserved
-        @reserved ||= Set.new %w(
-          __asm __int8 __based __except __int16 __stdcall __cdecl
-          __fastcall __int32 __declspec __finally __int64 __try
-          __leave __wchar_t __w64 __virtual_inheritance __uuidof
-          __unaligned __super __single_inheritance __raise __noop
-          __multiple_inheritance __m128i __m128d __m128 __m64 __interface
-          __identifier __forceinline __event __assume
-          inline _inline __inline
-          naked _naked __naked
-          thread _thread __thread
-        )
+        @reserved ||= super + Set.new(%w(
+          __virtual_inheritance __uuidof __super __single_inheritance
+          __multiple_inheritance __interface __event
+        ))
       end
 
       id = /[a-zA-Z_][a-zA-Z0-9]*/

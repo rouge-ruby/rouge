a88bd41914127fc6734c9717bf48d16531fe321b
Kornelius Kalnbach <murphy@rubychan.de>
2013-10-27 16:06:25 +0100

    another one

--- lib/rouge/regex_lexer.rb
+++ lib/rouge/regex_lexer.rb
@@ -417,7 +417,7 @@ module Rouge
     end
 
   private
-    def with_output_stream(output_stream, &b)
+    def with_output_stream(output_stream)
       old_output_stream = @output_stream
       @output_stream = Enumerator::Yielder.new do |tok, val|
         debug { "    yielding #{tok.qualname}, #{val.inspect}" }

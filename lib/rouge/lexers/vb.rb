949868e442afc24de1ef6cd75f022635428b2ae9
Adam <adam@adambard.com>
2013-10-27 21:57:11 -0700

    Fake the version

---
+++ lib/rouge/lexers/vb.rb
@@ -0,0 +1,45 @@
+module Rouge
+  module Lexers
+    class VisualBasic < RegexLexer
+      desc "Matlab"
+      tag 'vb'
+      aliases 'visualbasic'
+      filenames '*.vbs'
+      mimetypes 'text/x-visualbasic', 'application/x-visualbasic'
+
+      keywords = %w(
+        Select Case Function Sub Property Class Structure Enum Namespace Dim Module Private As If Then Else ElseIf End Const ExternalSource Region ExternalChecksum
+      )
+
+      builtins = %w(
+        Console ConsoleColor
+      )
+
+      state :root do
+        rule /\s+/m, Text # Whitespace
+        rule /(?:#{keywords.join('|')})\b/, Keyword
+        rule /(?:#{builtins.join('|')})\b/, Name::Builtin
+        rule /[a-zA-Z][_a-zA-Z0-9]*/m, Name
+        rule %r{[(){};:,\/\\\]\[]}, Punctuation
+
+        rule %r(%\{.*?%\})m, Comment::Multiline
+        rule /'.*$/, Comment::Single
+
+        rule /&=|[*]=|\/=|\\=|\^=|\+=|-=|<<=|>>=|<<|>>|:=|<=|>=|<>|[-&*\/\\^+=<>.]/, Operator
+
+        rule /(\d+\.\d*|\d*\.\d+)(e[+-]?[0-9]+)?/i, Num::Float
+        rule /\d+e[+-]?[0-9]+/i, Num::Float
+        rule /\d+L/, Num::Integer::Long
+        rule /\d+/, Num::Integer
+
+        mixin :strings
+      end
+
+      state :strings do
+        rule /'.*?'/, Str::Single
+        rule /".*?"/, Str::Single
+      end
+    end
+  end
+end
+
--- lib/rouge/version.rb
+++ lib/rouge/version.rb
@@ -1,5 +1,5 @@
 module Rouge
   def self.version
-    "0.4.0"
+    "0.3.9"
   end
 end

1efb4f101e6904329a05bc9779600667d0d78a35
http://jneen.net/ <jneen@jneen.net>
2014-07-11 20:51:34 -0700

    whitespace fixes

--- lib/rouge/lexers/swift.rb
+++ lib/rouge/lexers/swift.rb
@@ -5,7 +5,7 @@ module Rouge
     class Swift < RegexLexer
       tag 'swift'
       filenames '*.swift'
-     
+
       desc 'Multi paradigm, compiled programming language developed by Apple for iOS and OS X development. (developer.apple.com/swift)'
 
       id = /([0-9]|[_#A-Z]|\u00A8\u00AA\u00AD\u00AF\u2054|[\u00B2-\u00B5]|[\u00B7-\u00BA]|[\u00BC–\u00BE]|[\u00C0–\u00D6]|[\u00D8–\u00F6]|[\u00F8–\u00FF]|[\u0100–\u02FF]|[\u0370–\u167F]|[\u1681–\u180D]|[\u180F–\u1DBF]|[\u1E00–\u1FFF]|[\u200B–\u200D]|[\u202A–\u202E]|[\u203F–\u2040]|[\u2060-\u206F]|[\u2070–\u20CF]|[\u2100–\u218F]|[\u2460–\u24FF]|[\u2776–\u2793]|[\u2C00–\u2DFF]|[\u2E80–\u2FFF]|[\u3004–\u3007]|[\u3021–\u302F]|[\u3031–\u303F]|[\u3040–\uD7FF]|[\uF900–\uFD3D]|[\uFD40–\uFDCF]|[\uFDF0–\uFE1F]|[\uFE30–\uFE44]|[\uFE47–\uFFFD]|[\u{10000}–\u{1FFFD}]|[\u{20000}–\u{2FFFD}]|[\u{30000}–\u{3FFFD}]|[\u{40000}–\u{4FFFD}]|[\u{50000}–\u{5FFFD}]|[\u{60000}–\u{6FFFD}]|[\u{70000}–\u{7FFFD}]|[\u{80000}–\u{8FFFD}]|[\u{90000}–\u{9FFFD}]|[\u{A0000}–\u{AFFFD}]|[\u{B0000}–\u{BFFFD}]|[\u{C0000}–\u{CFFFD}]|[\u{D0000}–\u{DFFFD}]|[\u{E0000}–\u{EFFFD}]|[\u0300–\u036F]|[\u1DC0–\u1DFF]|[\u20D0–\u20FF]|[\uFE20–\uFE2F]|[\u{10000}-\u{1FFFD}]|[\u{20000}-\u{2FFFD}]|[\u{30000}-\u{3FFFD}]|[\u{40000}-\u{4FFFD}]|[\u{50000}-\u{5FFFD}]|[\u{60000}-\u{6FFFD}]|[\u{70000}-\u{7FFFD}]|[\u{80000}-\u{8FFFD}]|[\u{90000}-\u{9FFFD}]|[\u{A0000}-\u{AFFFD}]|[\u{B0000}-\u{BFFFD}]|[\u{C0000}-\u{CFFFD}]|[\u{D0000}–\u{DFFFD}])+/i
@@ -13,16 +13,16 @@ module Rouge
       def self.keywords
         @keywords ||= Set.new %w(
           break case continue default do else fallthrough if in for return switch where while
-          
+
           as dynamicType is new super self Self Type __COLUMN__ __FILE__ __FUNCTION__ __LINE__
-          
+
           associativity didSet get infix inout left mutating none nonmutating operator override postfix precedence prefix right set unowned unowned(safe) unowned(unsafe) weak willSet
         )
       end
 
       def self.declarations
         @declarations ||= Set.new %w(
-          class deinit enum extension func import init let protocol static struct subscript typealias var 
+          class deinit enum extension func import init let protocol static struct subscript typealias var
         )
       end
 
@@ -30,8 +30,8 @@ module Rouge
         @types ||= Set.new %w(
           Int8 Int16 Int32 Int64 UInt8 UInt16 UInt32 UInt64 Int
           Double Float
-          Bool 
-          String Character 
+          Bool
+          String Character
         )
       end
 

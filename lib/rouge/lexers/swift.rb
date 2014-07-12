e5d2f3980fc2eed060a1622387b50595c0d2a9b2
http://jneen.net/ <jneen@jneen.net>
2014-07-11 20:54:30 -0700

    simplify the implementation of id
    according to "soft" description in the spec, includes japanese and
    georgian

--- lib/rouge/lexers/swift.rb
+++ lib/rouge/lexers/swift.rb
@@ -8,7 +8,9 @@ module Rouge
 
       desc 'Multi paradigm, compiled programming language developed by Apple for iOS and OS X development. (developer.apple.com/swift)'
 
-      id = /([0-9]|[_#A-Z]|\u00A8\u00AA\u00AD\u00AF\u2054|[\u00B2-\u00B5]|[\u00B7-\u00BA]|[\u00BC–\u00BE]|[\u00C0–\u00D6]|[\u00D8–\u00F6]|[\u00F8–\u00FF]|[\u0100–\u02FF]|[\u0370–\u167F]|[\u1681–\u180D]|[\u180F–\u1DBF]|[\u1E00–\u1FFF]|[\u200B–\u200D]|[\u202A–\u202E]|[\u203F–\u2040]|[\u2060-\u206F]|[\u2070–\u20CF]|[\u2100–\u218F]|[\u2460–\u24FF]|[\u2776–\u2793]|[\u2C00–\u2DFF]|[\u2E80–\u2FFF]|[\u3004–\u3007]|[\u3021–\u302F]|[\u3031–\u303F]|[\u3040–\uD7FF]|[\uF900–\uFD3D]|[\uFD40–\uFDCF]|[\uFDF0–\uFE1F]|[\uFE30–\uFE44]|[\uFE47–\uFFFD]|[\u{10000}–\u{1FFFD}]|[\u{20000}–\u{2FFFD}]|[\u{30000}–\u{3FFFD}]|[\u{40000}–\u{4FFFD}]|[\u{50000}–\u{5FFFD}]|[\u{60000}–\u{6FFFD}]|[\u{70000}–\u{7FFFD}]|[\u{80000}–\u{8FFFD}]|[\u{90000}–\u{9FFFD}]|[\u{A0000}–\u{AFFFD}]|[\u{B0000}–\u{BFFFD}]|[\u{C0000}–\u{CFFFD}]|[\u{D0000}–\u{DFFFD}]|[\u{E0000}–\u{EFFFD}]|[\u0300–\u036F]|[\u1DC0–\u1DFF]|[\u20D0–\u20FF]|[\uFE20–\uFE2F]|[\u{10000}-\u{1FFFD}]|[\u{20000}-\u{2FFFD}]|[\u{30000}-\u{3FFFD}]|[\u{40000}-\u{4FFFD}]|[\u{50000}-\u{5FFFD}]|[\u{60000}-\u{6FFFD}]|[\u{70000}-\u{7FFFD}]|[\u{80000}-\u{8FFFD}]|[\u{90000}-\u{9FFFD}]|[\u{A0000}-\u{AFFFD}]|[\u{B0000}-\u{BFFFD}]|[\u{C0000}-\u{CFFFD}]|[\u{D0000}–\u{DFFFD}])+/i
+      id_head = /_|(?!\p{Mc})\p{Alpha}|[^\u0000-\uFFFF]/
+      id_rest = /[\p{Alnum}_]|[^\u0000-\uFFFF]/
+      id = /#{id_head}#{id_rest}*/
 
       def self.keywords
         @keywords ||= Set.new %w(

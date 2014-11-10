95ca82388339648bb66147e1f67e78baaeb83efa
m16a1 <a741su@gmail.com>
2014-11-10 18:58:06 +0300

    Newlines, interpolations parsing, fix floating point values issue

--- lib/rouge/lexers/dart.rb
+++ lib/rouge/lexers/dart.rb
@@ -21,12 +21,12 @@ module Rouge
 
       types = %w(bool double Dynamic enum int num Object Set String void)
 
-      id = /[a-zA-Z_][a-zA-Z0-9_]*/
+      id = /[a-zA-Z_]\w*/
 
       state :root do
         rule %r(^
-          (\s*(?:[a-zA-Z_][a-zA-Z0-9_.\[\]]*\s+)+?) # return arguments
-          ([a-zA-Z_][a-zA-Z0-9_]*)                  # method name
+          (\s*(?:[a-zA-Z_][a-zA-Z\d_.\[\]]*\s+)+?) # return arguments
+          ([a-zA-Z_][\w]*)                          # method name
           (\s*)(\()                                 # signature start
         )mx do |m|
           # TODO: do this better, this shouldn't need a delegation
@@ -39,8 +39,8 @@ module Rouge
         rule /\s+/, Text
         rule %r(//.*?$), Comment::Single
         rule %r(/\*.*?\*/)m, Comment::Multiline
-        rule /"(\\\\|\\"|[^"])*"/, Str::Double
-        rule /'(\\\\|\\'|[^'])*'/, Str::Single
+        rule /"/, Str, :dqs
+        rule /'/, Str, :sqs
         rule /(?:#{keywords.join('|')})\b/, Keyword
         rule /(?:#{declarations.join('|')})\b/, Keyword::Declaration
         rule /(?:#{types.join('|')})\b/, Keyword::Type
@@ -53,15 +53,37 @@ module Rouge
         rule /#{id}:/, Name::Label
         rule /\$?#{id}/, Name
         rule /[~^*!%&\[\](){}<>\|+=:;,.\/?-]/, Operator
-        rule /[0-9][0-9]*\.[0-9]+([eE][0-9]+)?[fd]?/, Num::Float
-        rule /0x[0-9a-f]+/, Num::Hex
-        rule /[0-9]+L?/, Num::Integer
+        rule /\d*\.\d+([eE]\-?\d+)?/, Num::Float
+        rule /0x[\da-fA-F]+/, Num::Hex
+        rule /\d+L?/, Num::Integer
         rule /\n/, Text
       end
 
       state :class do
         rule /\s+/m, Text
         rule id, Name::Class, :pop!
+      end
+
+      state :dqs do
+        rule /"/, Str, :pop!
+        rule /[^\\\$"]+/, Str
+        mixin :string
+      end
+
+      state :sqs do
+        rule /'/, Str, :pop!
+        rule /[^\\\$']+/, Str
+        mixin :string
+      end
+
+      state :string do
+        mixin :interpolation
+        rule /\\[nrt\"\'\\]/, Str::Escape
+      end
+
+      state :interpolation do
+        rule /\$[a-z]\w*/i, Str::Interpol
+        rule /\$\{[^\}]+\}/, Str::Interpol
       end
     end
   end
--- spec/lexers/dart_spec.rb
+++ spec/lexers/dart_spec.rb
@@ -14,4 +14,33 @@ describe Rouge::Lexers::Dart do
       assert_guess :mimetype => 'text/x-dart'
     end
   end
+  
+  describe 'lexer' do
+    include Support::Lexing
+
+    it 'lexes exponential float values' do
+      assert_tokens_equal '34.3e-7', ['Literal.Number.Float', '34.3e-7']
+    end
+
+    it 'lexes variable interpolation' do
+      assert_tokens_equal %('Value: $value'),
+        ['Literal.String', "'Value: "],
+        ['Literal.String.Interpol', '$value'],
+        ['Literal.String', "'"]
+    end
+
+    it 'lexes interpolated expression' do
+      assert_tokens_equal %('Value: ${value + 1}'),
+        ['Literal.String', "'Value: "],
+        ['Literal.String.Interpol', '${value + 1}'],
+        ['Literal.String', "'"]
+    end
+
+    it 'lexes escapes' do
+      assert_tokens_equal %q('Line1\nLine2'),
+        ['Literal.String', "'Line1"],
+        ['Literal.String.Escape', '\n'],
+        ['Literal.String', "Line2'"]
+    end
+  end
 end

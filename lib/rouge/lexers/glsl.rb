5b8bea7e1d1342ef24205a1eb42f686c75d2bf28
Sri Harsha Chilakapati <hello@goharsha.com>
2015-04-09 18:50:09 +0530

    Made the GLSL lexer extend the C lexer

--- lib/rouge/lexers/glsl.rb
+++ lib/rouge/lexers/glsl.rb
@@ -2,12 +2,13 @@
 
 module Rouge
   module Lexers
+    load_const :C, 'c.rb'
     
     # This file defines the GLSL language lexer to the Rouge
     # syntax highlighter.
     #
     # Author: Sri Harsha Chilakapati
-    class Glsl < RegexLexer
+    class Glsl < C
       tag 'glsl'
       filenames '*.glsl', '*.frag', '*.vert', '*.geom', '*.fs', '*.vs', '*.gs', '*.shader'
       mimetypes 'x-shader/x-vertex', 'x-shader/x-fragment', 'x-shader/x-geometry'
@@ -29,7 +30,6 @@ module Rouge
           if else
           subroutine
           in out inout
-          float double int void bool true false
           invariant
           discard return struct precision
         )
@@ -87,11 +87,6 @@ module Rouge
         )
       end
 
-      # high priority for filename matches
-      def self.analyze_text(*)
-        0.3
-      end
-
       def self.builtins
         @builtins ||= Set.new %w(
           gl_VertexID gl_InstanceID gl_PerVertex gl_Position gl_PointSize gl_ClipDistance
@@ -134,145 +129,6 @@ module Rouge
           gl_EyePlaneQ gl_ObjectPlaneS gl_ObjectPlaneT gl_ObjectPlaneR gl_ObjectPlaneQ
           gl_FogParameters gl_Fog
         )
-      end
-
-      start { push :bol }
-
-      state :expr_bol do
-        mixin :inline_whitespace
-
-        rule /#if\s0/, Comment, :if_0
-        rule /#/, Comment::Preproc, :macro
-
-        rule(//) { pop! }
-      end
-
-      # :expr_bol is the same as :bol but without labels, since
-      # labels can only appear at the beginning of a statement.
-      state :bol do
-        rule /#{id}:(?!:)/, Name::Label
-        mixin :expr_bol
-      end
-
-      state :inline_whitespace do
-        rule /[ \t\r]+/, Text
-        rule /\\\n/, Text # line continuation
-        rule %r(/(\\\n)?[*].*?[*](\\\n)?/)m, Comment::Multiline
-      end
-
-      state :whitespace do
-        rule /\n+/m, Text, :bol
-        rule %r(//(\\.|.)*?\n), Comment::Single, :bol
-        mixin :inline_whitespace
-      end
-
-      state :expr_whitespace do
-        rule /\n+/m, Text, :expr_bol
-        mixin :whitespace
-      end
-
-      state :statements do
-        mixin :whitespace
-        rule %r((\d+[.]\d*|[.]?\d+)e[+-]?\d+[lu]*)i, Num::Float
-        rule %r(\d+e[+-]?\d+[lu]*)i, Num::Float
-        rule /0x[0-9a-f]+[lu]*/i, Num::Hex
-        rule /0[0-7]+[lu]*/i, Num::Oct
-        rule /\d+[lu]*/i, Num::Integer
-        rule %r(\*/), Error
-        rule %r([~!%^&*+=\|?:<>/-]), Operator
-        rule /[()\[\],.]/, Punctuation
-        rule /\bcase\b/, Keyword, :case
-        rule /(?:true|false|NULL)\b/, Name::Builtin
-        rule id do |m|
-          name = m[0]
-
-          if self.class.keywords.include? name
-            token Keyword
-          elsif self.class.keywords_type.include? name
-            token Keyword::Type
-          elsif self.class.reserved.include? name
-            token Keyword::Reserved
-          elsif self.class.builtins.include? name
-            token Name::Builtin
-          else
-            token Name
-          end
-        end
-      end
-
-      state :case do
-        rule /:/, Punctuation, :pop!
-        mixin :statements
-      end
-
-      state :root do
-        mixin :expr_whitespace
-
-        # functions
-        rule %r(
-          ([\w*\s]+?[\s*]) # return arguments
-          (#{id})          # function name
-          (\s*\([^;]*?\))  # signature
-          (#{ws})({)         # open brace
-        )mx do |m|
-          # TODO: do this better.
-          recurse m[1]
-          token Name::Function, m[2]
-          recurse m[3]
-          recurse m[4]
-          token Punctuation, m[5]
-          push :function
-        end
-
-        # function declarations
-        rule %r(
-          ([\w*\s]+?[\s*]) # return arguments
-          (#{id})          # function name
-          (\s*\([^;]*?\))  # signature
-          (#{ws})(;)       # semicolon
-        )mx do |m|
-          # TODO: do this better.
-          recurse m[1]
-          token Name::Function
-          recurse m[3]
-          recurse m[4]
-          token Punctuation
-          push :statement
-        end
-
-        rule(//) { push :statement }
-      end
-
-      state :statement do
-        rule /;/, Punctuation, :pop!
-        mixin :expr_whitespace
-        mixin :statements
-        rule /[{}]/, Punctuation
-      end
-
-      state :function do
-        mixin :whitespace
-        mixin :statements
-        rule /;/, Punctuation
-        rule /{/, Punctuation, :function
-        rule /}/, Punctuation, :pop!
-      end
-
-      state :macro do
-        # NB: pop! goes back to :bol
-        rule /\n/, Comment::Preproc, :pop!
-        rule %r([^/\n\\]+), Comment::Preproc
-        rule /\\./m, Comment::Preproc
-        mixin :inline_whitespace
-        rule %r(/), Comment::Preproc
-      end
-
-      state :if_0 do
-        # NB: no \b here, to cover #ifdef and #ifndef
-        rule /^\s*#if/, Comment, :if_0
-        rule /^\s*#\s*el(?:se|if)/, Comment, :pop!
-        rule /^\s*#\s*endif\b.*?(?<!\\)\n/m, Comment, :pop!
-        rule /.*?\n/, Comment
       end
     end
   end

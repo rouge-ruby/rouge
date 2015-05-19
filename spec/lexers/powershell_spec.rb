581ec4215d494066873fcc00fc6adb1ef1e4632f
Aaron <Aaron@aaroneg.com>
2015-05-19 01:00:45 -0500

    build checks succed
    Bare minimum required to make the build system quiet down.

---
+++ spec/lexers/powershell_spec.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- #
+
+describe Rouge::Lexers::Powershell do
+  let(:subject) { Rouge::Lexers::Powershell.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.ps1'
+      assert_guess :filename => 'foo.psm1'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'application/x-powershell'
+    end
+
+  end
+end

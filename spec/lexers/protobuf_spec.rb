5e18d2f671d25a5a4c0fee54a53e0feed9e09280
Fabian Diet <fabian.diet@icloud.com>
2015-10-26 18:23:15 +0100

    Add protobuf lexer
    Ported from pygments

---
+++ spec/lexers/protobuf_spec.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*- #
+
+describe Rouge::Lexers::Protobuf do
+  let(:subject) { Rouge::Lexers::Protobuf.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.proto'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'text/x-proto'
+    end
+  end
+end

48b436f8ec500e2ee4c6e51be98904e111f884bc
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-02-03 14:22:06 +0000

    Define more predefined names

--- lib/rouge/lexers/r.rb
+++ lib/rouge/lexers/r.rb
@@ -12,11 +12,9 @@ module Rouge
 
       mimetypes 'text/x-r', 'application/x-r'
 
-      def self.keywords
-        @keywords ||= %w(
-          if else for while repeat in next break switch function
-        )
-      end
+      KEYWORDS = %w(if else for while repeat in next break switch function)
+
+      BUILTIN_CONSTANTS = %w(LETTERS letters month.abb month.name pi T F)
 
       def self.analyze_text(text)
         return 1 if text.shebang? 'Rscript'
@@ -34,13 +32,14 @@ module Rouge
         rule /\b(NULL|Inf|TRUE|FALSE|NaN)(?!\.)\b/, Keyword::Constant
         rule /\bNA(_(integer|real|complex|character)_)?\b/,
           Keyword::Constant
-        rule /\b[TF]\b/, Name::Constant
 
         rule /%[^%]*?%/, Operator
 
         rule /[a-zA-Z.]([a-zA-Z_][\w.]*)?/ do |m|
-          if self.class.keywords.include? m[0]
+          if KEYWORDS.include? m[0]
             token Keyword
+          elsif BUILTIN_CONSTANTS.include? m[0]
+            token Name::Constant
           else
             token Name
           end

9c6e5e65fd740a1adbed8e879c8bb44814c25c61
Matt Layman <matthewlayman@gmail.com>
2016-02-28 21:04:23 -0500

    Clean up the TAP spec.

--- spec/lexers/tap_spec.rb
+++ spec/lexers/tap_spec.rb
@@ -5,16 +5,12 @@ describe Rouge::Lexers::Tap do
     include Support::Guessing
 
     it 'guesses by filename' do
-      assert_guess :filename => 'foo.???'
+      assert_guess :filename => 'foo.tap'
     end
 
     it 'guesses by mimetype' do
       assert_guess :mimetype => 'text/x-tap'
       assert_guess :mimetype => 'application/x-tap'
-    end
-
-    it 'guesses by source' do
-      assert_guess :source => '????'
     end
   end
 end

494b349d7a31d6780c6a5c05b6278d275df4b3fc
Bastien Jansen <bastien.jansen@gmx.com>
2016-06-02 21:29:04 +0200

    Indent with 2 spaces everywhere

--- lib/rouge/lexers/ceylon.rb
+++ lib/rouge/lexers/ceylon.rb
@@ -48,39 +48,39 @@ module Rouge
         mixin :whitespace
 
         rule %r((shared|abstract|formal|default|actual|variable|deprecated|small|
-             late|literal|doc|by|see|throws|optional|license|tagged|final|native|
-             annotation|sealed)\b), Name::Decorator
+          late|literal|doc|by|see|throws|optional|license|tagged|final|native|
+          annotation|sealed)\b), Name::Decorator
 
         rule %r((break|case|catch|continue|else|finally|for|in|
-                if|return|switch|this|throw|try|while|is|exists|dynamic|
-                nonempty|then|outer|assert|let)\b), Keyword
+          if|return|switch|this|throw|try|while|is|exists|dynamic|
+          nonempty|then|outer|assert|let)\b), Keyword
 
         rule %r((abstracts|extends|satisfies|super|given|of|out|assign)\b), Keyword::Declaration
 
         rule %r((function|value|void|new)\b), Keyword::Type
         
         rule %r((assembly|module|package)(\s+)) do
-            groups Keyword::Namespace, Text
-            push :import
+          groups Keyword::Namespace, Text
+          push :import
         end
         
         rule %r((true|false|null)\b), Keyword::Constant
         
         rule %r((class|interface|object|alias)(\s+)) do
-            groups Keyword::Declaration, Text
-            push :class
+          groups Keyword::Declaration, Text
+          push :class
         end
 
         rule %r((import)(\s+)) do
-            groups Keyword::Namespace, Text
-            push :import
+          groups Keyword::Namespace, Text
+          push :import
         end
         
         rule %r("(\\\\|\\"|[^"])*"), Literal::String
         rule %r('\\.'|'[^\\]'|'\\\{#[0-9a-fA-F]{4}\}'), Literal::String::Char
         rule %r(".*``.*``.*"', String::Interpol
         rule %r(\.)([a-z_]\w*)) do
-            groups Operator, Name::Attribute
+          groups Operator, Name::Attribute
         end
         rule %r([a-zA-Z_]\w*:), Name::Label
         rule %r((\\I[a-z]|[A-Z])\w*), Name::Decorator
@@ -88,10 +88,10 @@ module Rouge
         rule %r([~^*!%&\[\](){}<>|+=:;,./?-`]), Operator
         rule %r(\d{1,3}(_\d{3})+\.\d{1,3}(_\d{3})+[kMGTPmunpf]?), Literal::Number::Float
         rule %r(\d{1,3}(_\d{3})+\.[0-9]+([eE][+-]?[0-9]+)?[kMGTPmunpf]?),
-            Literal::Number::Float
+          Literal::Number::Float
         rule %r([0-9][0-9]*\.\d{1,3}(_\d{3})+[kMGTPmunpf]?), Literal::Number::Float
         rule %r([0-9][0-9]*\.[0-9]+([eE][+-]?[0-9]+)?[kMGTPmunpf]?),
-            Literal::Number::Float
+          Literal::Number::Float
         rule %r(#([0-9a-fA-F]{4})(_[0-9a-fA-F]{4})+), Literal::Number::Hex
         rule %r(#[0-9a-fA-F]+), Literal::Number::Hex
         rule %r(\$([01]{4})(_[01]{4})+), Literal::Number::Bin

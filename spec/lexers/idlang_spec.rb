55c31ba88af923f6508b2d796afa9353592adf19
Joe Sapp <joe.sapp@noaa.gov>
2016-06-03 11:39:48 -0400

    Remove .pro from the lexer guessing routine (see jneen/rouge#395)

--- spec/lexers/idlang_spec.rb
+++ spec/lexers/idlang_spec.rb
@@ -7,7 +7,8 @@ describe Rouge::Lexers::IDLang do
     include Support::Guessing
 
     it 'guesses by filename' do
-      assert_guess :filename => 'foo.pro'
+      #assert_guess :filename => 'foo.pro'
+      assert_guess :filename => 'foo.idl'
     end
   end
 end

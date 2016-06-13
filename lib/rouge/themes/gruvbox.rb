50b9f9fe7b1f5372d093084d7a4f7c44907aa07b
http://jneen.net/ <jneen@jneen.net>
2016-06-13 14:08:36 -0700

    use constants for color names

--- lib/rouge/themes/gruvbox.rb
+++ lib/rouge/themes/gruvbox.rb
@@ -10,50 +10,50 @@ module Rouge
       name 'gruvbox'
 
       # global Gruvbox colours {{{
-      @dark0_hard = '#1d2021'
-      @dark0 ='#282828'
-      @dark0_soft = '#32302f'
-      @dark1 = '#3c3836'
-      @dark2 = '#504945'
-      @dark3 = '#665c54'
-      @dark4 = '#7c6f64'
-      @dark4_256 = '#7c6f64'
+      C_dark0_hard = '#1d2021'
+      C_dark0 ='#282828'
+      C_dark0_soft = '#32302f'
+      C_dark1 = '#3c3836'
+      C_dark2 = '#504945'
+      C_dark3 = '#665c54'
+      C_dark4 = '#7c6f64'
+      C_dark4_256 = '#7c6f64'
 
-      @gray_245 = '#928374'
-      @gray_244 = '#928374'
+      C_gray_245 = '#928374'
+      C_gray_244 = '#928374'
 
-      @light0_hard = '#f9f5d7'
-      @light0 = '#fbf1c7'
-      @light0_soft = '#f2e5bc'
-      @light1 = '#ebdbb2'
-      @light2 = '#d5c4a1'
-      @light3 = '#bdae93'
-      @light4 = '#a89984'
-      @light4_256 = '#a89984'
+      C_light0_hard = '#f9f5d7'
+      C_light0 = '#fbf1c7'
+      C_light0_soft = '#f2e5bc'
+      C_light1 = '#ebdbb2'
+      C_light2 = '#d5c4a1'
+      C_light3 = '#bdae93'
+      C_light4 = '#a89984'
+      C_light4_256 = '#a89984'
 
-      @bright_red = '#fb4934'
-      @bright_green = '#b8bb26'
-      @bright_yellow = '#fabd2f'
-      @bright_blue = '#83a598'
-      @bright_purple = '#d3869b'
-      @bright_aqua = '#8ec07c'
-      @bright_orange = '#fe8019'
+      C_bright_red = '#fb4934'
+      C_bright_green = '#b8bb26'
+      C_bright_yellow = '#fabd2f'
+      C_bright_blue = '#83a598'
+      C_bright_purple = '#d3869b'
+      C_bright_aqua = '#8ec07c'
+      C_bright_orange = '#fe8019'
 
-      @neutral_red = '#cc241d'
-      @neutral_green = '#98971a'
-      @neutral_yellow = '#d79921'
-      @neutral_blue = '#458588'
-      @neutral_purple = '#b16286'
-      @neutral_aqua = '#689d6a'
-      @neutral_orange = '#d65d0e'
+      C_neutral_red = '#cc241d'
+      C_neutral_green = '#98971a'
+      C_neutral_yellow = '#d79921'
+      C_neutral_blue = '#458588'
+      C_neutral_purple = '#b16286'
+      C_neutral_aqua = '#689d6a'
+      C_neutral_orange = '#d65d0e'
 
-      @faded_red = '#9d0006'
-      @faded_green = '#79740e'
-      @faded_yellow = '#b57614'
-      @faded_blue = '#076678'
-      @faded_purple = '#8f3f71'
-      @faded_aqua = '#427b58'
-      @faded_orange = '#af3a03'
+      C_faded_red = '#9d0006'
+      C_faded_green = '#79740e'
+      C_faded_yellow = '#b57614'
+      C_faded_blue = '#076678'
+      C_faded_purple = '#8f3f71'
+      C_faded_aqua = '#427b58'
+      C_faded_orange = '#af3a03'
       # }}}
 
       extend HasModes
@@ -69,59 +69,60 @@ module Rouge
       end
 
       def self.make_dark!
-        palette bg0: @dark0
-        palette bg1: @dark1
-        palette bg2: @dark2
-        palette bg3: @dark3
-        palette bg4: @dark4
+        palette bg0: C_dark0
+        palette bg1: C_dark1
+        palette bg2: C_dark2
+        palette bg3: C_dark3
+        palette bg4: C_dark4
 
-        palette gray: @gray_245
+        palette gray: C_gray_245
 
-        palette fg0: @light0
-        palette fg1: @light1
-        palette fg2: @light2
-        palette fg3: @light3
-        palette fg4: @light4
+        palette fg0: C_light0
+        palette fg1: C_light1
+        palette fg2: C_light2
+        palette fg3: C_light3
+        palette fg4: C_light4
 
-        palette fg4_256: @light4_256
+        palette fg4_256: C_light4_256
 
-        palette red: @bright_red
-        palette green: @bright_green
-        palette yellow: @bright_yellow
-        palette blue: @bright_blue
-        palette purple: @bright_purple
-        palette aqua: @bright_aqua
-        palette orange: @bright_orange
+        palette red: C_bright_red
+        palette green: C_bright_green
+        palette yellow: C_bright_yellow
+        palette blue: C_bright_blue
+        palette purple: C_bright_purple
+        palette aqua: C_bright_aqua
+        palette orange: C_bright_orange
 
       end
 
       def self.make_light!
-        palette bg0: @light0
-        palette bg1: @light1
-        palette bg2: @light2
-        palette bg3: @light3
-        palette bg4: @light4
+        palette bg0: C_light0
+        palette bg1: C_light1
+        palette bg2: C_light2
+        palette bg3: C_light3
+        palette bg4: C_light4
 
-        palette gray: @gray_244
+        palette gray: C_gray_244
 
-        palette fg0: @dark0
-        palette fg1: @dark1
-        palette fg2: @dark2
-        palette fg3: @dark3
-        palette fg4: @dark4
+        palette fg0: C_dark0
+        palette fg1: C_dark1
+        palette fg2: C_dark2
+        palette fg3: C_dark3
+        palette fg4: C_dark4
 
-        palette fg4_256: @dark4_256
+        palette fg4_256: C_dark4_256
 
-        palette red: @faded_red
-        palette green: @faded_green
-        palette yellow: @faded_yellow
-        palette blue: @faded_blue
-        palette purple: @faded_purple
-        palette aqua: @faded_aqua
-        palette orange: @faded_orange
+        palette red: C_faded_red
+        palette green: C_faded_green
+        palette yellow: C_faded_yellow
+        palette blue: C_faded_blue
+        palette purple: C_faded_purple
+        palette aqua: C_faded_aqua
+        palette orange: C_faded_orange
       end
 
       dark!
+      mode :light
 
       style Text, :fg => :fg0, :bg => :bg0
       style Error, :fg => :red, :bg => :bg0, :bold => true

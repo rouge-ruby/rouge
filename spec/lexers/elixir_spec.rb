22d13f107c6a5ffd0560f4b09d926c456520f961
Krzysztof Zych <k33rni@gmail.com>
2016-08-17 17:10:39 +0200

    Using Ruby's sigil strings, implement Elixir ones. Distinguish regexes.
    Remove old rules that were (incorrectly?) matching %r() instead of ~r().

--- spec/lexers/elixir_spec.rb
+++ spec/lexers/elixir_spec.rb
@@ -63,8 +63,85 @@ describe Rouge::Lexers::Elixir do
     end
 
     it 'lexes regexp sigils' do
-      assert_tokens_equal %{~r//},
-        ['Literal.String.Regex', '~r//']
+      %w(() [] <> '' "" || //).each do |pair|
+        l, r = pair[0], pair[1]
+        assert_tokens_equal %{~r#{l}#{r}},
+          ['Literal.String.Regex', "~r#{l}#{r}"]
+
+        assert_tokens_equal %{~r#{l}#{r}gif},
+          ['Literal.String.Regex', "~r#{l}#{r}gif"]
+
+        assert_tokens_equal %{~r#{l}abc+foo#{r}rsx},
+          ['Literal.String.Regex', "~r#{l}abc+foo#{r}rsx"]
+
+        assert_tokens_equal %{~r//p}, # Wrong regex modifier gets turned into a Name
+          ['Literal.String.Regex', '~r//'],
+          ['Name', 'p']
+      end
+    end
+
+    it 'lexes regexp sigil avoiding greedy errors' do
+      text = 'string |> String.split(~r/[ -]/) |> Enum.map(&abbreviate_word/1) |> Enum.join()'
+      
+      assert_tokens_equal text,
+        ['Name', 'string'],
+        ['Text', ' '],
+        ['Operator', '|>'],
+        ['Text', ' '],
+        ['Name.Constant', 'String'],
+        ['Operator', '.'],
+        ['Name', 'split'],
+        ['Punctuation', '('],
+        ['Literal.String.Regex', '~r/[ -]/'],
+        ['Punctuation', ')'],
+        ['Text', ' '],
+        ['Operator', '|>'],
+        ['Text', ' '],
+        ['Name.Constant', 'Enum'],
+        ['Operator', '.'],
+        ['Name', 'map'],
+        ['Punctuation', '('],
+        ['Operator', '&'],
+        ['Name', 'abbreviate_word'],
+        ['Operator', '/'],
+        ['Literal.Number', '1'],
+        ['Punctuation', ')'],
+        ['Text', ' '],
+        ['Operator', '|>'],
+        ['Text', ' '],
+        ['Name.Constant', 'Enum'],
+        ['Operator', '.'],
+        ['Name', 'join'],
+        ['Punctuation', '()']
+
+    end
+
+    it 'lexes other sigils' do
+      %w(() [] <> '' "" || //).each do |pair|
+        l, r = pair[0], pair[1]
+        assert_tokens_equal %{~c#{l}#{r}},
+          ['Literal.String.Other', "~c#{l}#{r}"]
+
+        assert_tokens_equal %{~s#{l}ABC#{r}},
+          ['Literal.String.Other', "~s#{l}ABC#{r}"]
+
+
+        assert_tokens_equal %{~w#{l}lorem ipsum dolor#{r}s},
+          ['Literal.String.Other', "~w#{l}lorem ipsum dolor#{r}s"]
+
+        assert_tokens_equal %{~S#{l}#\{inter <> "polation"\}#{r}},
+          ['Literal.String.Other', "~S#{l}"],
+          ['Literal.String.Interpol', '#{'],
+          ['Name', 'inter'],
+          ['Text', ' '],
+          ['Operator', '<>'],
+          ['Text', ' '],
+          ['Literal.String.Doc', '"'],
+          ['Literal.String.Double', 'polation"'],
+          ['Literal.String.Interpol', '}'],
+          ['Literal.String.Other', r]
+      end
+
     end
 
     it 'lexes & operator' do

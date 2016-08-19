98db7a22f0ca0b9dcefd4a1081f2aaece912673c
Drew Blessing <drew@gitlab.com>
2016-08-18 20:52:22 -0500

    Add a prometheus lexer

---
+++ spec/lexers/prometheus_spec.rb
@@ -0,0 +1,17 @@
+describe Rouge::Lexers::Prometheus do
+  let(:subject) { Rouge::Lexers::Prometheus.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.prometheus'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'text/x-prometheus'
+      assert_guess :mimetype => 'application/x-prometheus'
+    end
+  end
+end
+

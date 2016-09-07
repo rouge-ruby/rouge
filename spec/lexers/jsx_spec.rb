ba205213d3edada76b78858e6c03b2e6417f60f9
http://jneen.net/ <jneen@jneen.net>
2016-09-07 10:16:35 -0700

    fix specs for jsx

--- spec/lexers/jsx_spec.rb
+++ spec/lexers/jsx_spec.rb
@@ -1,20 +1,16 @@
-describe Rouge::Lexers::Jsx do
-  let(:subject) { Rouge::Lexers::Jsx.new }
+describe Rouge::Lexers::JSX do
+  let(:subject) { Rouge::Lexers::JSX.new }
 
   describe 'guessing' do
     include Support::Guessing
 
     it 'guesses by filename' do
-      assert_guess :filename => 'foo.???'
+      assert_guess :filename => 'foo.jsx'
     end
 
     it 'guesses by mimetype' do
       assert_guess :mimetype => 'text/x-jsx'
       assert_guess :mimetype => 'application/x-jsx'
-    end
-
-    it 'guesses by source' do
-      assert_guess :source => '????'
     end
   end
 end

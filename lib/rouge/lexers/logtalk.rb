d962eaa6adb3599bafe5f50de0485e5e353ec78b
Paulo Moura <pmoura@logtalk.org>
2016-09-20 18:13:25 +0100

    Use full keywords instead of hand-optimized regular expressions as requested

--- lib/rouge/lexers/logtalk.rb
+++ lib/rouge/lexers/logtalk.rb
@@ -33,72 +33,80 @@ module Rouge
         # Message forwarding handler
         rule /forward(?=[(])/, Keyword
         # Execution-context methods
-        rule /(parameter|this|se(lf|nder))(?=[(])/, Keyword
+        rule /(parameter|this|self|sender)(?=[(])/, Keyword
         # Reflection
         rule /(current_predicate|predicate_property)(?=[(])/, Keyword
         # DCGs and term expansion
-        rule /(expand_(goal|term)|(goal|term)_expansion|phrase)(?=[(])/, Keyword
+        rule /(expand_goal|expand_term|goal_expansion|term_expansion|phrase)(?=[(])/, Keyword
         # Entity
-        rule /(abolish|c(reate|urrent))_(object|protocol|category)(?=[(])/, Keyword
-        rule /(object|protocol|category)_property(?=[(])/, Keyword
+        rule /(abolish_object|abolish_protocol|abolish_category)(?=[(])/, Keyword
+        rule /(create_object|create_protocol|create_category)(?=[(])/, Keyword
+        rule /(current_object|current_protocol|current_category)(?=[(])/, Keyword
+        rule /(object_property|protocol_property|category_property)(?=[(])/, Keyword
         # Entity relations
-        rule /co(mplements_object|nforms_to_protocol)(?=[(])/, Keyword
-        rule /extends_(object|protocol|category)(?=[(])/, Keyword
-        rule /imp(lements_protocol|orts_category)(?=[(])/, Keyword
-        rule /(instantiat|specializ)es_class(?=[(])/, Keyword
+        rule /(complements_object|conforms_to_protocol)(?=[(])/, Keyword
+        rule /(extends_object|extends_protocol|extends_category)(?=[(])/, Keyword
+        rule /(implements_protocol|imports_category)(?=[(])/, Keyword
+        rule /(instantiates_class|specializes_class)(?=[(])/, Keyword
         # Events
-        rule /(current_event|(abolish|define)_events)(?=[(])/, Keyword
+        rule /(current_event|abolish_events|define_events)(?=[(])/, Keyword
         # Flags
-        rule /(create|current|set)_logtalk_flag(?=[(])/, Keyword
+        rule /(create_logtalk_flag|current_logtalk_flag|set_logtalk_flag)(?=[(])/, Keyword
         # Compiling, loading, and library paths
-        rule /logtalk_(compile|l(ibrary_path|oad|oad_context)|make)(?=[(])/, Keyword
+        rule /(logtalk_compile|logtalk_load)(?=[(])/, Keyword
+        rule /(logtalk_library_path|logtalk_load_context|logtalk_make)(?=[(])/, Keyword
         rule /\blogtalk_make\b/, Keyword
         # Database
-        rule /(clause|retract(all)?)(?=[(])/, Keyword
-        rule /a(bolish|ssert(a|z))(?=[(])/, Keyword
+        rule /(clause|retract|retractall)(?=[(])/, Keyword
+        rule /(abolish|asserta|assertz)(?=[(])/, Keyword
         # Control constructs
-        rule /(ca(ll|tch)|throw)(?=[(])/, Keyword
-        rule /(fa(il|lse)|true)\b/, Keyword
+        rule /(call|catch|throw)(?=[(])/, Keyword
+        rule /(fail|false|true)\b/, Keyword
         # All solutions
-        rule /((bag|set)of|f(ind|or)all)(?=[(])/, Keyword
+        rule /(bagof|setof|findall|forall)(?=[(])/, Keyword
         # Multi-threading meta-predicates
-        rule /threaded(_(call|once|ignore|exit|peek|wait|notify))?(?=[(])/, Keyword
+        rule /threaded(?=[(])/, Keyword
+        rule /(threaded_call|threaded_once|threaded_ignore|threaded_exit|threaded_peek)(?=[(])/, Keyword
+        rule /(threaded_wait|threaded_notify)(?=[(])/, Keyword
         # Term unification
         rule /(subsumes_term|unify_with_occurs_check)(?=[(])/, Keyword
         # Term creation and decomposition
         rule /(functor|arg|copy_term|numbervars|term_variables)(?=[(])/, Keyword
         # Evaluable functors
-        rule /(div|rem|m(ax|in|od)|abs|sign)(?=[(])/, Keyword
-        rule /float(_(integer|fractional)_part)?(?=[(])/, Keyword
-        rule /(floor|t(an|runcate)|round|ceiling)(?=[(])/, Keyword
+        rule /(div|rem|max|min|mod|abs|sign)(?=[(])/, Keyword
+        rule /(float_integer_part|float_fractional_part)(?=[(])/, Keyword
+        rule /(floor|truncate|round|ceiling)(?=[(])/, Keyword
         # Other arithmetic functors
-        rule /(cos|a(cos|sin|tan|tan2)|exp|log|s(in|qrt)|xor)(?=[(])/, Keyword
+        rule /(cos|acos|asin|atan|atan2|exp|log|sin|sqrt|tan|xor)(?=[(])/, Keyword
         # Term testing
-        rule /(var|atom(ic)?|integer|float|c(allable|ompound)|n(onvar|umber)|ground|acyclic_term)(?=[(])/, Keyword
+        rule /(var|atom|atomic|integer|float|callable|compound|nonvar|number|ground|acyclic_term)(?=[(])/, Keyword
         # Term comparison
         rule /compare(?=[(])/, Keyword
         # Stream selection and control
-        rule /(curren|se)t_(in|out)put(?=[(])/, Keyword
+        rule /(current_input|current_output|set_input|set_output)(?=[(])/, Keyword
         rule /(open|close)(?=[(])/, Keyword
         rule /flush_output(?=[(])/, Keyword
         rule /(at_end_of_stream|flush_output)\b/, Keyword
         rule /(stream_property|at_end_of_stream|set_stream_position)(?=[(])/, Keyword
         # Character and byte input/output
-        rule /(nl|(get|peek|put)_(byte|c(har|ode)))(?=[(])/, Keyword
+        rule /nl(?=[(])/, Keyword
+        rule /(get_byte|get_char|get_code)(?=[(])/, Keyword
+        rule /(peek_byte|peek_char|peek_code)(?=[(])/, Keyword
+        rule /(put_byte|put_char|put_code)(?=[(])/, Keyword
         rule /\bnl\b/, Keyword
         # Term input/output
-        rule /read(_term)?(?=[(])/, Keyword
-        rule /write(q|_(canonical|term))?(?=[(])/, Keyword
-        rule /(current_)?op(?=[(])/, Keyword
-        rule /(current_)?char_conversion(?=[(])/, Keyword
+        rule /(read|read_term)(?=[(])/, Keyword
+        rule /(write|writeq|write_canonical|write_term)(?=[(])/, Keyword
+        rule /(current_op|op)(?=[(])/, Keyword
+        rule /(char_conversion|current_char_conversion)(?=[(])/, Keyword
         # Atomic term processing
-        rule /atom_(length|c(hars|o(ncat|des)))(?=[(])/, Keyword
+        rule /(atom_length|atom_chars|atom_concat|atom_codes)(?=[(])/, Keyword
         rule /(char_code|sub_atom)(?=[(])/, Keyword
-        rule /number_c(har|ode)s(?=[(])/, Keyword
+        rule /(number_chars|number_codes)(?=[(])/, Keyword
         # Implementation defined hooks functions
-        rule /(se|curren)t_prolog_flag(?=[(])/, Keyword
-        rule /\bhalt\b/, Keyword
+        rule /(set_prolog_flag|current_prolog_flag)(?=[(])/, Keyword
         rule /halt(?=[(])/, Keyword
+        rule /\bhalt\b/, Keyword
         # Message sending operators
         rule /(::|:|\^\^)/, Operator
         # External call
@@ -107,7 +115,7 @@ module Rouge
         rule /(ignore|once)(?=[(])/, Keyword
         rule /\brepeat\b/, Keyword
         # Sorting
-        rule /(key)?sort(?=[(])/, Keyword
+        rule /(keysort|sort)(?=[(])/, Keyword
         # Bitwise functors
         rule %r(>>|<<|/\\|\\\\|\\), Operator
         # Predicate aliases
@@ -149,20 +157,21 @@ module Rouge
         # Whitespace
         mixin :whitespace
         # Conditional compilation directives
-        rule /(el)?if(?=[(])/, Keyword, :pop!
-        rule /(e(lse|ndif))(?=[.])/, Keyword, :pop!
+        rule /(elif|if)(?=[(])/, Keyword, :pop!
+        rule /(else|endif)(?=[.])/, Keyword, :pop!
         # Entity directives
         rule /(category|object|protocol)(?=[(])/, Keyword, :entity_relations
-        rule /(end_(category|object|protocol))(?=[.])/, Keyword, :pop!
+        rule /(end_category|end_object|end_protocol)(?=[.])/, Keyword, :pop!
         # Predicate scope directives
         rule /(public|protected|private)(?=[(])/, Keyword, :pop!
         # Other directives
-        rule /e(n(coding|sure_loaded)|xport)(?=[(])/, Keyword, :pop!
-        rule /in(clude|itialization|fo)(?=[(])/, Keyword, :pop!
+        rule /(encoding|ensure_loaded|export)(?=[(])/, Keyword, :pop!
+        rule /(include|initialization|info)(?=[(])/, Keyword, :pop!
         rule /(built_in|dynamic|synchronized|threaded)(?=[.])/, Keyword, :pop!
-        rule /(alias|d(ynamic|iscontiguous)|m(eta_(non_terminal|predicate)|ode|ultifile)|s(et_(logtalk|prolog)_flag|ynchronized))(?=[(])/, Keyword, :pop!
+        rule /(alias|dynamic|discontiguous|meta_non_terminal|meta_predicate|mode|multifile|synchronized)(?=[(])/, Keyword, :pop!
+        rule /(set_logtalk_flag|set_prolog_flag)(?=[(])/, Keyword, :pop!
         rule /op(?=[(])/, Keyword, :pop!
-        rule /(c(alls|oinductive)|module|reexport|use(s|_module))(?=[(])/, Keyword, :pop!
+        rule /(calls|coinductive|module|reexport|uses|use_module)(?=[(])/, Keyword, :pop!
         rule /[a-z][a-zA-Z0-9_]*(?=[(])/, Text, :pop!
         rule /[a-z][a-zA-Z0-9_]*(?=[.])/, Text, :pop!
         # End of entity-opening directive
@@ -170,7 +179,7 @@ module Rouge
       end
 
       state :entity_relations do
-        rule /(complements|extends|i(nstantiates|mp(lements|orts))|specializes)(?=[(])/, Keyword
+        rule /(complements|extends|instantiates|implements|imports|specializes)(?=[(])/, Keyword
         # Numbers
         mixin :numbers
         # Variables

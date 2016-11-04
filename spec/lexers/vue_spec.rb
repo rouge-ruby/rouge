6db0ac11a5d9fceceb0e2b5c372305afb8992c87
http://jneen.net/ <jneen@jneen.net>
2016-11-04 10:21:33 -0700

    new lexer: vue

---
+++ spec/lexers/vue_spec.rb
@@ -0,0 +1,12 @@
+describe Rouge::Lexers::Vue do
+  let(:subject) { Rouge::Lexers::Vue.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.vue'
+    end
+  end
+end
+

91793fb138aef144d81220030c150c25e980f56d
EricFromCanada <eric@lassosoft.com>
2016-12-03 17:20:38 -0500

    add lexer for Lasso

---
+++ spec/lexers/lasso_spec.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- #
+
+describe Rouge::Lexers::Lasso do
+  let(:subject) { Rouge::Lexers::Lasso.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.lasso'
+      assert_guess :filename => 'foo.lasso8'
+      assert_guess :filename => 'foo.lasso9'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'text/x-lasso'
+      assert_guess :mimetype => 'application/x-httpd-lasso'
+    end
+
+    it 'guesses by source' do
+      assert_guess :source => '#!/usr/bin/env lasso9'
+      assert_guess :source => '<?lasso define today => date ?>'
+    end
+  end
+end

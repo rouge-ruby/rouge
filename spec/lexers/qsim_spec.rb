a9321c4bf20541b6d9265580f2102e0c340c8478
Charlyzzz <erwincdl@gmail.com>
2016-12-27 02:12:49 -0300

    Aditional lexing tests

--- spec/lexers/qsim_spec.rb
+++ spec/lexers/qsim_spec.rb
@@ -21,5 +21,28 @@ describe Rouge::Lexers::QSim do
                           ['Text.Whitespace', ' '],
                           ['Error', 'R16']
     end
+
+    it 'label definition' do
+      assert_tokens_equal 'label1: ',
+                          ['Name.Label', 'label1'],
+                          ['Punctuation', ':'],
+                          ['Text.Whitespace', ' ']
+    end
+
+    it 'comments' do
+      assert_tokens_equal 'MOV --Hey! ',
+                          ['Keyword', 'MOV'],
+                          ['Text.Whitespace', ' '],
+                          ['Comment.Single', '--Hey! ']
+    end
+
+    it 'correct and wrong hexadecimal numbers' do
+      assert_tokens_equal 'MOV 0x11FF 0X111G',
+                          ['Keyword', 'MOV'],
+                          ['Text.Whitespace', ' '],
+                          ['Literal.Number.Hex', '0x11FF'],
+                          ['Text.Whitespace', ' '],
+                          ['Error', '0X111G']
+    end
   end
 end

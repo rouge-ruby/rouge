dce186492bc403f5b6872afcf7f6c0285b3302b0
Charlyzzz <erwincdl@gmail.com>
2016-12-27 00:08:51 -0300

    Basic operands support + example

--- lib/rouge/lexers/qsim.rb
+++ lib/rouge/lexers/qsim.rb
@@ -10,6 +10,34 @@ module Rouge
       def self.analyze_text(*)
         0.3
       end
+
+      operators = %w(ADD SUB MUL DIV MOV OR NOT AND CALL RET JMP JE CMP)
+
+      #Reverse helps matching first R15 instead of R1
+      registers = (0..15)
+                      .map { |n| "R#{n}" }
+                      .reverse
+
+
+      state :root do
+
+        def any(expressions)
+          /#{expressions.join('|')}/
+        end
+
+
+        rule any(operators), Keyword
+        rule any(registers), Name::Attribute
+
+        rule /\w+:/, Name::Label
+
+        rule /\b0x[0-9A-F]{4}\b/, Literal::Number::Hex
+        rule /\b[01]+\b/, Literal::Number
+
+        rule /,/, Punctuation
+
+        rule /\s+/, Text::Whitespace
+      end
     end
   end
 end

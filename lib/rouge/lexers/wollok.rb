d6d1ecdc81548c2575edc0c3d21cf11fac12fb99
Charlyzzz <erwincdl@gmail.com>
2016-12-30 20:16:20 -0300

    method definitions and variables

--- lib/rouge/lexers/wollok.rb
+++ lib/rouge/lexers/wollok.rb
@@ -10,7 +10,7 @@ module Rouge
         0.3
       end
 
-      keywords = %w(new super return inherits import constructor)
+      keywords = %w(new super return)
 
       entityName = /[a-zA-Z][a-zA-Z0-9]*/
 
@@ -20,12 +20,9 @@ module Rouge
 
       state :root do
         mixin :whitespace
-
         rule /import/, Keyword::Reserved, :import
-
-        rule /class|object|program/, Keyword::Declaration, :entity_naming
-
-        rule /test/, Keyword::Declaration, :test_naming
+        rule /class|object|program/, Keyword::Reserved, :entity_naming
+        rule /test/, Keyword::Reserved, :test_naming
       end
 
       state :import do
@@ -35,23 +32,56 @@ module Rouge
 
       state :entity_naming do
         mixin :whitespace
-        rule entityName, Keyword::Declaration
+        rule entityName, Name::Class
+        rule /inherits/, Keyword::Reserved
         rule /{/, Text, :entity_definition
+        rule /}/, Text, :pop!
       end
 
       state :entity_definition do
         mixin :whitespace
-        rule
+        rule /var|const\b/, Keyword::Declaration
+        rule /method|constructor/, Keyword::Declaration, :method_naming
+        mixin :definition
+        rule /}/, Text, :pop!
       end
 
-      state :object do
+      state :method_naming do
         mixin :whitespace
+        rule entityName, Name::Function, :parameters
+        rule /\(/, Text, :parameters
+      end
 
-        rule entityName do
-          pop!
-          token Name
+      state :parameters do
+        mixin :whitespace
+        rule /\(|\)/, Text
+        rule entityName, Name::Variable
+        rule /,/, Punctuation
+
+        rule /{/ do
+          token Text
+          pop!(2)
         end
       end
+
+
+      state :definition do
+        mixin :whitespace
+        rule /#{keywords.join('|')}/, Keyword::Reserved
+        rule entityName, Keyword::Variable
+        rule /self/, Name::Builtin::Pseudo
+        rule /./, Punctuation, :message
+        rule /\*\+-\//, Operator
+      end
+
+      state :message do
+        rule entityName, Text
+      end
+
+      state :lambda do
+
+      end
+
     end
   end
 end

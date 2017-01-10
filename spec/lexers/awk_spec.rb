ec13c551c6de0b9f027a0b565d25f054be62f2d2
Alexandre Perrin <alex@kaworu.ch>
2017-01-11 00:41:11 +0100

    new lexer: awk
    fixes #424

---
+++ spec/lexers/awk_spec.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- #
+
+describe Rouge::Lexers::Awk do
+  let(:subject) { Rouge::Lexers::Awk.new }
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it %(doesn't let a bad regex mess up the whole lex) do
+      assert_has_token 'Error',          "a = /foo;\n1"
+      assert_has_token 'Literal.Number', "a = /foo;\n1"
+    end
+  end
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.awk'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'application/x-awk'
+    end
+
+    it 'guesses by source' do
+      assert_guess :source => '#!/usr/bin/awk'
+      assert_guess :source => '#!/usr/bin/env awk'
+    end
+  end
+end

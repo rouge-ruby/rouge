13499bb6a31fdf5fd6890bdea2967eb6d4621de8
Charlyzzz <erwincdl@gmail.com>
2017-01-09 22:07:50 -0300

    Added single and multiline comments

--- lib/rouge/lexers/gobstones.rb
+++ lib/rouge/lexers/gobstones.rb
@@ -4,13 +4,31 @@ module Rouge
       title 'Gobstones'
       desc 'Gobstones language'
       tag 'gobstones'
-      filenames *%w(*.gbs)
+      filenames '*.gbs'
 
       def self.analyze_text(_text)
         0.3
       end
 
+      reserved = %w(program interactive procedure function type is return record
+                    field variant case if then else switch to repeat while foreach
+                    in match)
+
+      atoms = %w(False True Verde Rojo Azul Negro Norte Sur Este Oeste)
+
+      state :comments do
+        def comment_between(start, finish)
+          /#{start}(.|\s)*?#{finish}/m
+        end
+
+        rule comment_between('{-', '-}'), Comment::Multiline
+        rule comment_between('\/\*', '\*\/'), Comment::Multiline
+        #TODO is it ''' or """?
+        rule %r{(-|#|//).*$}, Comment::Single
+      end
+
       state :root do
+        mixin :comments
         rule /\s|\S/, Text
       end
     end

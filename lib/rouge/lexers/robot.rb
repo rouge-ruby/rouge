8011378a5fc474c61f7b0327938e049bb691dc85
Iakov Gan <iakovgan+github@gmail.com>
2017-01-14 12:04:06 +0100

    Refactoring and bugfix

--- lib/rouge/lexers/robot.rb
+++ lib/rouge/lexers/robot.rb
@@ -6,7 +6,7 @@ module Rouge
       aliases 'robotframework'
 
       title "Robot"
-      desc 'Test DSL ( http://robotframework.org/ )'
+      desc 'Test DSL ( http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html )'
 
       filenames '*.robot'
       mimetypes 'text/x-robot'
@@ -15,59 +15,63 @@ module Rouge
         return 1 if text =~ /\*\*\*\s+Settings\s+\*\*\*/
       end
 
-      SETTINGS = '(Resource)|(Variables)|(Documentation)|(Metadata)|(Suite Setup)|(Suite Teardown)|(Force Tags)|(Default Tags)|(Test Setup)|(Test Teardown)|(Test Template)|(Test Timeout)'
+      SETTINGS = '(Resource)|(Variables)|(Documentation)|(Metadata)|(Suite Setup)|(Suite Teardown)|(Force Tags)|(Default Tags)|(Test Setup)|(Test Teardown)|(Test Template)|(Test Timeout)|(Library)'
       BDD = 'Given|When|Then|And|But'
       TEST_CASE = 'Documentation|Tags|Setup|Teardown|Template|Timeout'
       KEYWORD = 'Documentation|Tags|Arguments|Return|Teardown|Timeout'
       rest_of_line = /.*?(?=[#\n])/
 
+
       state :basic do
+#        #In robot framework tabs are very important, 
+#        #but highliting with a '→' sign can be an isssue for copypaste
 #        rule /\t/ do |m|
-#          token Punctuation, '→' + m[0]
+#         #token Punctuation, '→' + m[0] 
 #        end
         rule %r(\n), Text
         rule %r(#.*$), Comment
         rule /\$\{.*?\}/, Name::Variable
         rule /\@\{.*?\}/, Name::Variable # list
         rule /\&\{.*?\}/, Name::Variable # dict
-        rule %r([ \r]+), Text
+        rule %r([\r]+), Text
         rule %r([|]), Punctuation
+        rule /".+"/, Str::Double
+        rule /'.+'/, Str::Single
+        rule /".+"/, Str::Double
+        rule  /\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b/, Str # ipv4
+        rule /\W-?(?:0|[1-9]\d*)\.\d+(?:e[+-]\d+)?\W/i, Num::Float 
+        rule /\W-?(?:0|[1-9]\d*)(?:e[+-]\d+)?\W/i, Num::Integer    
       end
 
+      # Token types are set for visual perfofmance in gitlab webui
       state :root do
         mixin :basic
-        rule %r(\*\*\*\s+Test Case(s)?\s+\*\*\*) do 
+        rule /\*\*\*\s+Test Case(s)?\s+\*\*\*/i do 
           reset_stack
           push :test_cases
-          token Name::Namespace
+          token   Name::Tag
         end
-        rule %r(\*\*\*\s+Keywords\s+\*\*\*) do 
+        rule /\*\*\*\s+Keywords\s+\*\*\*/i do 
           reset_stack
           push :keywords
-          token Name::Namespace
+          token    Name::Tag
         end
-        rule %r(\*\*\*\s+Settings\s+\*\*\*) do 
+        rule /\*\*\*\s+Settings\s+\*\*\*/i do 
           reset_stack 
           push :settings
-          token Name::Namespace
+          token   Name::Tag
         end
-        rule %r(\*\*\*\s+Variables?\s+\*\*\*) do 
-          token Name::Namespace
+        rule /\*\*\*\s+Variables?\s+\*\*\*/i do 
+          token   Name::Tag
         end
       end
 
       state :settings do
          mixin :root
-         rule /^#{SETTINGS}/ do
-            reset_stack
-            push :rest
+         rule /^#{SETTINGS}\s/ do
             token Keyword::Reserved
          end
-      end
-
-      state :rest do
-         mixin :settings
-         rule rest_of_line, Text
+        rule /.+?/, Text
       end
 
       state :test_cases do
@@ -81,7 +85,7 @@ module Rouge
       end
       state :test_definition do
         mixin :test_cases 
-        rule /#{BDD}/i, Keyword::Constant
+        rule /^\s*(#{BDD})\s/i, Keyword::Constant
         rule /.+?/, Text # FIXME Arguments
       end
 
@@ -98,7 +102,7 @@ module Rouge
          mixin :keywords 
         rule /.+?/, Text # FIXME Arguments
       end
-
+      
     end
   end
 end

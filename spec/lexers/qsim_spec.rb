5b7f5cd3d17c6a54d93bf57a82a0dfc2e03b3ae3
Erwin Debusschere <Charlyzzz@users.noreply.github.com>
2017-01-26 23:29:01 +0200

    QSim lexer support (#3)
    * ignoring .idea files
    
    * Basic lexer setup
    
    * Basic operands support + example
    
    * Basic example + routines
    
    * Lexing tests
    
    * Fixed wrong whitespace declaration
    
    * Aditional lexing tests
    
    * Reverted gitignore back to normal
    
    * New operands
    
    * Adding examples and translating them to english
    
    * Q6 full support
    
    * Update qsim.rb
    
    * Encoding header

---
+++ spec/lexers/qsim_spec.rb
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*- # 
+
+describe Rouge::Lexers::QSim do
+  let(:subject) { Rouge::Lexers::QSim.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.qsim'
+    end
+  end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it 'operator call' do
+      assert_tokens_equal 'MOV R7, R16',
+                          ['Keyword', 'MOV'],
+                          ['Text.Whitespace', ' '],
+                          ['Name.Builtin.Pseudo', 'R7'],
+                          ['Punctuation', ','],
+                          ['Text.Whitespace', ' '],
+                          ['Error', 'R16']
+    end
+
+    it 'label definition' do
+      assert_tokens_equal 'label1: ',
+                          ['Name.Label', 'label1:'],
+                          ['Text.Whitespace', ' ']
+    end
+
+    it 'comments' do
+      assert_tokens_equal 'MOV --Hey! ',
+                          ['Keyword', 'MOV'],
+                          ['Text.Whitespace', ' '],
+                          ['Comment.Single', '--Hey! ']
+    end
+
+    it 'correct and wrong hexadecimal numbers' do
+      assert_tokens_equal 'MOV 0x11FF 0X111G',
+                          ['Keyword', 'MOV'],
+                          ['Text.Whitespace', ' '],
+                          ['Literal.Number.Hex', '0x11FF'],
+                          ['Text.Whitespace', ' '],
+                          ['Error', '0X111G']
+    end
+  end
+end

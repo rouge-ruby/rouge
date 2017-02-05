505f772d71d0a72136bb616b3029c7b6beeee816
Rik de Groot <rik.de.groot@mendix.com>
2017-02-05 21:42:30 +0100

    Inherit JSX iso TS

--- lib/rouge/lexers/tsx.rb
+++ lib/rouge/lexers/tsx.rb
@@ -1,8 +1,7 @@
 module Rouge
   module Lexers
-    load_lexer 'typescript.rb'
-
-    class TSX < Typescript
+    load_lexer 'jsx.rb'
+    class TSX < JSX
       desc 'tsx'
       tag 'tsx'
       aliases 'tsx', 'typescript-react'
@@ -10,90 +9,30 @@ module Rouge
 
       mimetypes 'text/x-tsx', 'application/x-tsx'
 
-      id = Javascript.id_regex
-
-      def start_embed!
-        @embed ||= TSX.new(options)
-        @embed.reset!
-        @embed.push(:expr_start)
-        push :tsx_embed_root
+      def self.keywords
+        @keywords ||= super + Set.new(%w(
+          is namespace static private protected public
+          implements readonly
+        ))
       end
 
-      def tag_token(name)
-        name[0] =~ /\p{Lower}/ ? Name::Tag : Name::Class
+      def self.declarations
+        @declarations ||= super + Set.new(%w(
+          type abstract
+        ))
       end
 
-      start { @html = HTML.new(options) }
-
-      state :tsx_tags do
-        rule /</, Punctuation, :tsx_element
+      def self.reserved
+        @reserved ||= super + Set.new(%w(
+          string any void number namespace module
+          declare default interface keyof
+        ))
       end
 
-      state :tsx_internal do
-        rule %r(</) do
-          token Punctuation
-          goto :tsx_end_tag
-        end
-
-        rule /{/ do
-          token Str::Interpol
-          start_embed!
-        end
-
-        rule /[^<>{]+/ do
-          delegate @html
-        end
-
-        mixin :tsx_tags
-      end
-
-      prepend :expr_start do
-        mixin :tsx_tags
-      end
-
-      state :tsx_tag do
-        mixin :comments_and_whitespace
-        rule /#{id}/ do |m|
-          token tag_token(m[0])
-        end
-
-        rule /[.]/, Punctuation
-      end
-
-      state :tsx_end_tag do
-        mixin :tsx_tag
-        rule />/, Punctuation, :pop!
-      end
-
-      state :tsx_element do
-        rule /#{id}=/, Name::Attribute, :tsx_attribute
-        mixin :tsx_tag
-        rule />/ do token Punctuation; goto :tsx_internal end
-        rule %r(/>), Punctuation, :pop!
-      end
-
-      state :tsx_attribute do
-        rule /"(\\[\\"]|[^"])*"/, Str::Double, :pop!
-        rule /'(\\[\\']|[^'])*'/, Str::Single, :pop!
-        rule /{/ do
-          token Str::Interpol
-          pop!
-          start_embed!
-        end
-      end
-
-      state :tsx_embed_root do
-        rule /[.][.][.]/, Punctuation
-        rule /}/, Str::Interpol, :pop!
-        mixin :tsx_embed
-      end
-
-      state :tsx_embed do
-        rule /{/ do delegate @embed; push :tsx_embed end
-        rule /}/ do delegate @embed; pop! end
-        rule /[^{}]+/ do
-          delegate @embed
-        end
+      def self.builtins
+        @builtins ||= super + %w(
+          Pick Partial Readonly Record
+        )
       end
     end
   end

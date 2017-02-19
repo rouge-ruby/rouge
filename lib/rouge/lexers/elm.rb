6758173c0f1c986b222f1b1a70c4d154d392e563
Benjamin <benjamin.guy.thomas@gmail.com>
2017-02-19 10:02:20 +0100

    Refactor as requested + add a few tests

--- lib/rouge/lexers/elm.rb
+++ lib/rouge/lexers/elm.rb
@@ -3,9 +3,6 @@
 module Rouge
   module Lexers
     class Elm < RegexLexer
-      # For reference: one liner to debug all files in a given dir at once
-      # tail -n+1 ~/code/github.com/evancz/elm-architecture-tutorial/examples/*elm | sed 's/\(==>.\+<==\)/-- \1/' | ROUGE_DEBUG=1 ./bin/rougify --lexer elm 2>/tmp/rouge_debug | less -RS
-
       title "Elm"
       desc "The Elm programming language (elm-lang.org)"
 
@@ -18,42 +15,48 @@ module Rouge
         @debug = ENV['ROUGE_DEBUG'] == '1'
       end
 
+        state :multi do
+          rule /\{-.*/, Comment::Multiline
+        end
+
       state :root do
-        rule /\-\-.*/, Comment::Single
+        rule /\n|\s/, Text
+        rule /\-\-.+/, Comment::Single
+
+        rule /\{\-.*?\-\}/m, Comment::Multiline
 
         rule /".*?"/m, Literal::String
         rule /\d/, Literal::Number
         rule /\s/, Text::Whitespace
 
-        rule Regexp.union(
-          /#{Regexp.quote('\(')}/,
-          /#{Regexp.quote('(')}/,
-          /#{Regexp.quote(')')}/,
-          /#{Regexp.quote('{')}/,
-          /#{Regexp.quote('}')}/,
-          /#{Regexp.escape('[')}/,
-          /#{Regexp.escape(']')}/,
-          /#{Regexp.escape('.')}/,
-          /,/,
-          /\\/,
-          /'/, # Not sure how single quotes should be handled
-        ), Punctuation
+        rule /\\\(/, Punctuation
+        rule /\(/, Punctuation
+        rule /\)/, Punctuation
+        rule /\{/, Punctuation
+        rule /\}/, Punctuation
+        rule /\[/, Punctuation
+        rule /\]/, Punctuation
+        rule /\./, Punctuation
+        rule /,/, Punctuation
+        rule /\\\\/, Punctuation
+        rule /\\/, Punctuation
+        rule /'/, Punctuation # Not sure how single quotes should be handled for now
 
-        rule Regexp.union(
-          /#{Regexp.escape('++')}/,
-          /#{Regexp.escape('->')}/,
-          /#{Regexp.escape('<|')}/,
-          /#{Regexp.escape('|>')}/,
-          /#{Regexp.escape('|')}/,
-          /#{Regexp.escape('+')}/,
-          /#{Regexp.escape('-')}/,
-          /#{Regexp.escape('*')}/,
-          /#{Regexp.escape('^')}/,
-          /:/,
-          /=/,
-        ), Operator
+        rule /#{Regexp.escape('++')}/, Operator
+        rule /#{Regexp.escape('->')}/, Operator
+        rule /#{Regexp.escape('<|')}/, Operator
+        rule /#{Regexp.escape('|>')}/, Operator
+        rule /#{Regexp.escape('|')}/, Operator
+        rule /#{Regexp.escape('+')}/, Operator
+        rule /#{Regexp.escape('-')}/, Operator
+        rule /#{Regexp.escape('*')}/, Operator
+        rule /#{Regexp.escape('^')}/, Operator
+        rule /:/, Operator
+        rule /&&/, Operator
+        rule /#{Regexp.escape('||')}/, Operator
+        rule /#{Regexp.escape('=')}/, Operator
 
-        rule /#{Regexp.quote('..')}/, Keyword::Declaration
+        rule /\.\./, Keyword::Declaration
 
         rule /\bmodule\b|\bexposing\b|\btype\b/, Keyword::Reserved
 
--- spec/lexers/elm_spec.rb
+++ spec/lexers/elm_spec.rb
@@ -7,6 +7,67 @@ describe Rouge::Lexers::Elm do
     it 'guesses by filename' do
       assert_guess :filename => 'foo.elm'
     end
-
   end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    describe 'numbers' do
+      it 'handles 1 + 1' do
+        assert_tokens_equal(
+          '1 + 1',
+          ['Literal.Number', '1'],
+          ['Text', ' '],
+          ['Operator', '+'],
+          ['Text', ' '],
+          ['Literal.Number', '1']
+        )
+      end
+    end
+
+    describe 'punctuation' do
+      it 'handles 3.14' do
+        assert_tokens_equal(
+          '3.14',
+          ['Literal.Number', '3'],
+          ['Punctuation', '.'],
+          ['Literal.Number', '14'],
+        )
+      end
+    end
+
+    describe 'comments' do
+      it 'handles one single line comment' do
+        assert_tokens_equal(
+          '-- a single line comment',
+          ["Comment.Single", "-- a single line comment"],
+        )
+      end
+
+      it 'handles 2 single line comments' do
+        assert_tokens_equal(
+          "-- comment1\n--comment2",
+          ['Comment.Single', "-- comment1"],
+          ['Text', "\n"],
+          ['Comment.Single', "--comment2"],
+        )
+      end
+
+      it 'handles multiline comments' do
+        skip("Broken, need some kind of state")
+        mlc = <<EOS
+{- a multiline comment
+  {- can be nested -}
+-}
+EOS
+        assert_tokens_equal(
+          mlc,
+          ['Comment.Multiline', "{- a multiline comment\n  {- can be nested -}\n-}"],
+          ['Text', "\n"]
+        )
+      end
+
+    end
+  end
+
 end

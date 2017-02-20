e6823340e53f226502a4fd3810474c26bbae5f18
ellemenno <ellemenno@users.noreply.github.com>
2017-02-20 18:27:46 -0500

    remove confusing colon from rackup message to stdout

--- spec/visual_spec.rb
+++ spec/visual_spec.rb
@@ -3,7 +3,7 @@
 puts <<-msg
 
 ============
-Run `rackup` and visit localhost:9292/:lexer_name to visually test a lexer.
+Run `rackup` and visit localhost:9292/lexer_name to visually test a lexer.
 ============
 
 msg

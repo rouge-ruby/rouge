5a4eb9a46c34f7f05e48aeabbdab20d2f54a2936
Hibariya <hibariya@gmail.com>
2017-03-15 21:34:33 +0900

    Add GraphQL support

---
+++ spec/lexers/graphql_spec.rb
@@ -0,0 +1,16 @@
+describe Rouge::Lexers::GraphQL do
+  let(:subject) { Rouge::Lexers::GraphQL.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.graphql'
+      assert_guess :filename => 'bar.gql'
+    end
+
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'application/graphql'
+    end
+  end
+end

b5ff7b704d04444e51813e8fa21fe636e996fe55
Alexander Belopolsky <a@enlnt.com>
2017-04-13 00:49:54 -0400

    Issue #547: Identifiers, keywords and numbers.

--- lib/rouge/lexers/q.rb
+++ lib/rouge/lexers/q.rb
@@ -8,12 +8,37 @@ module Rouge
       filenames '*.q'
       mimetypes 'text/x-q', 'application/x-q'
 
+      identifier = /\.?[a-z][a-z0-9_.]*/i
+
+      def self.keywords
+        @keywords ||= %w[abs til]
+      end
+
       def self.analyze_text(text)
         return 0
       end
 
       state :root do
-        rule(/.*\n/, Text)
+        rule /\n+/m, Text
+        rule /#{identifier}/ do |m|
+          if self.class.keywords.include? m[0]
+            token Keyword
+          else
+            token Name
+          end
+        end
+
+        rule(/[^\S\n]+/, Text)
+        rule(%r{\/.*$}, Comment)
+
+        rule(/[0-9]+(\.[0-9]+)?[fe]/, Num::Float)
+        rule(/[01]+b?/, Num)
+        rule(/[0-9]+[hij]?/, Num::Integer)
+
+        rule(/(?:<=|>=|<>|::)|[?:$%&@._#*^\-+~,!><=]:?/, Operator)
+
+        rule(/\\.*\n/, Text)
+
       end
     end
   end

8fd8e4cce3a64cd57f4055fe206c3ba6f8cc0c92
http://jneen.net/ <jneen@jneen.net>
2017-04-26 13:12:30 -0700

    shell: add builtins, highlight flags, interp heredocs

--- lib/rouge/lexers/shell.rb
+++ lib/rouge/lexers/shell.rb
@@ -29,6 +29,8 @@ module Rouge
         local logout popd printf pushd pwd read readonly set shift
         shopt source suspend test time times trap true type typeset
         ulimit umask unalias unset wait
+
+        ls tar cat grep sudo
       ).join('|')
 
       state :basic do
@@ -38,6 +40,7 @@ module Rouge
         rule /\bcase\b/, Keyword, :case
 
         rule /\b(#{BUILTINS})\s*\b(?!\.)/, Name::Builtin
+        rule /[.](?=\s)/, Name::Builtin
 
         rule /^\S*[\$%>#] +/, Generic::Prompt
 
@@ -45,21 +48,39 @@ module Rouge
           groups Name::Variable, Operator
         end
 
-        rule /[\[\]{}()=]/, Operator
+        rule /[\[\]{}()!=]/, Operator
         rule /&&|\|\|/, Operator
 
-        rule /<<</, Operator # here-string
-        rule /<<-?\s*(\'?)\\?(\w+)\1/ do |m|
-          lsh = Str::Heredoc
-          token lsh
-          heredocstr = Regexp.escape(m[2])
+        # here-string
+        rule /<<</, Operator
 
-          push do
-            rule /\s*#{heredocstr}\s*\n/, lsh, :pop!
-            rule /.*?\n/, lsh
-          end
+        rule /(<<-?)(\s*)(\'?)(\\?)(\w+)(\3)/ do |m|
+          groups Operator, Text, Str::Heredoc, Str::Heredoc, Name::Constant, Str::Heredoc
+          @heredocstr = Regexp.escape(m[5])
+          push :heredoc
         end
       end
+
+      state :heredoc do
+        rule /\n/, Str::Heredoc, :heredoc_nl
+        rule /[^$\n]+/, Str::Heredoc
+        mixin :interp
+        rule /[$]/, Str::Heredoc
+      end
+
+      state :heredoc_nl do
+        rule /\s*(\w+)\s*\n/ do |m|
+          if m[1] == @heredocstr
+            token Name::Constant
+            pop! 2
+          else
+            token Str::Heredoc
+          end
+        end
+
+        rule(//) { pop! }
+      end
+
 
       state :double_quotes do
         # NB: "abc$" is literally the string abc$.
@@ -95,8 +116,10 @@ module Rouge
 
         rule /\*/, Keyword
 
-        rule /;/, Text
-        rule /[^=\*\s{}()$"'`\\<]+/, Text
+        rule /;/, Punctuation
+
+        rule /--?[\w-]+/, Name::Tag
+        rule /[^=\*\s{}()$"'`;\\<]+/, Text
         rule /\d+(?= |\Z)/, Num
         rule /</, Text
         mixin :interp
@@ -117,7 +140,7 @@ module Rouge
 
       state :math do
         rule /\)\)/, Keyword, :pop!
-        rule %r([-+*/%^|&]|\*\*|\|\|), Operator
+        rule %r([-+*/%^|&!]|\*\*|\|\|), Operator
         rule /\d+(#\w+)?/, Num
         mixin :root
       end
@@ -147,6 +170,7 @@ module Rouge
         rule /\${#?/, Keyword, :curly
         rule /`/, Str::Backtick, :backticks
         rule /\$#?(\w+|.)/, Name::Variable
+        rule /\$[*@]/, Name::Variable
       end
 
       state :root do

21e226850cab7e65f321c1ff4eceb5c45f372f76
ellemenno <ellemenno@users.noreply.github.com>
2017-05-01 10:21:18 -0400

    add spec tests for loomscript where different from actionscript

--- spec/lexers/loomscript_spec.rb
+++ spec/lexers/loomscript_spec.rb
@@ -17,4 +17,47 @@ describe Rouge::Lexers::LoomScript do
       assert_guess :mimetype => 'application/x-loomscript'
     end
   end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    describe 'keywords' do
+      it 'identifies basic keywords' do
+        tokens = subject.lex("if (_a instanceof A) return;").to_a
+        assert_token_type_at(tokens, [0, 5, 10], 'Keyword')
+      end
+
+      it 'identifies constants' do
+        tokens = subject.lex("function f(b:Boolean = false, n:Number = NaN, o:Object = null):void").to_a
+        assert_token_type_at(tokens, [10, 19, 28], 'Keyword.Constant')
+      end
+
+      it 'identifies declarations' do
+        code = <<~EOF
+        delegate ToCompute(s:String, o:Object):Number;
+        public enum Enumeration {}
+        struct P {
+            public var x:Number = 0;
+            public var y:Number = 0;
+            public static operator function =(a:P, b:P):P {}
+        }
+        EOF
+        tokens = subject.lex(code).to_a
+        assert_token_type_at(tokens, [18, 24, 32, 45, 60, 62], 'Keyword.Declaration')
+      end
+
+      it 'identifies builtin types' do
+        tokens = subject.lex("function f(b:Boolean, v:Vector.<Object>, d:Dictionary.<String, Number>):void { trace(b, v, d); }").to_a
+        assert_token_type_at(tokens, [6, 11, 14, 20, 23, 26, 29], 'Keyword.Type')
+      end
+    end
+
+    describe 'names' do
+      it 'identifies attributes' do
+        tokens = subject.lex("private static native var releaseBuild:Boolean;").to_a
+        assert_token_type_at(tokens, [0, 2, 4], 'Name.Attribute')
+      end
+    end
+
+  end
 end

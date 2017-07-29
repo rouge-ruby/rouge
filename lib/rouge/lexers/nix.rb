369bfc266669f2e0f676a83a957066c1e50a7b11
David Asabina <vid@bina.me>
2017-07-29 22:39:47 +0200

    WIP basic exp elements: num, bool, str

--- lib/rouge/lexers/nix.rb
+++ lib/rouge/lexers/nix.rb
@@ -8,8 +8,42 @@ module Rouge
       tag 'nix'
       aliases 'nixos'
       filenames '*.nix'
+
+      state :number do
+        rule /[0-9]/, Num::Integer
+      end
+
+      state :boolean do
+        rule /(true|false)/, Keyword::Pseudo
+      end
+
+      state :string do
+        rule /"/, Str::Double, :double_quoted_string
+      end
+
+      state :interpolated_string_arg do
+        mixin :expression
+        rule /}/, Str::Interpol, :pop!
+      end
+
+      state :double_quoted_string do
+        #rule "\\.", Str::Escape
+        rule /"/, Str::Double, :pop!
+        rule /\${/, Str::Interpol, :interpolated_string_arg
+        rule /./, Str::Double
+      end
+      
+      state :expression do
+        mixin :number
+        mixin :boolean
+        mixin :string
+      end
+
       state :root do
-        rule /.*/m, Text
+        mixin :expression
+      end
+
+      start do
       end
     end
   end

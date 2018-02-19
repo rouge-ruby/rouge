66fe1dfd325036eb4d90ea92e699870531d3d182
julp <e2c2be7ed0f2f336@gmail.com>
2018-02-19 23:08:29 +0100

    add a first support to EEX (Embedded Elixir)

---
+++ spec/lexers/eex_spec.rb
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- #
+
+describe Rouge::Lexers::EEX do
+  let(:subject) { Rouge::Lexers::EEX.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.eex'
+      assert_guess :filename => 'foo.html.eex'
+    end
+  end
+end

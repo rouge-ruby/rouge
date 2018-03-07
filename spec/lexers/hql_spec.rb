fa4b3b93eda4cf5ab79f11e9bfbbe8f9a6368f18
Timo Kluck <timo.kluck@booking.com>
2018-03-07 18:48:16 +0100

    Add Hive Query language as an SQL dialect

---
+++ spec/lexers/hql_spec.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*- #
+
+describe Rouge::Lexers::HQL do
+  let(:subject) { Rouge::Lexers::HQL.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.hql'
+    end
+
+  end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it 'recognizes variable interpolation within a string' do
+      assert_tokens_equal '"${var1}.${var2}"', ['Str.Single', '"', 'Name.Variable', '${var1}', 'Str.Single', '.', 'Name.Variable', '${var2}', 'Str.Single', '"']
+    end
+  end
+end

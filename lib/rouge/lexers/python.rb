07fa3882528d378e82b75d4fcf7e1b76eef1eb45
Ivan Smirnov <i.s.smirnov@gmail.com>
2018-06-16 12:23:32 +0100

    Python: add `nonlocal` keyword

--- lib/rouge/lexers/python.rb
+++ lib/rouge/lexers/python.rb
@@ -19,7 +19,7 @@ module Rouge
           assert break continue del elif else except exec
           finally for global if lambda pass print raise
           return try while yield as with from import yield
-          async await
+          async await nonlocal
         )
       end
 

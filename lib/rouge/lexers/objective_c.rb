cff3a5a8ec352d99e8859ec63b9de27ff58f0f88
Iulian Onofrei <6d0847b9@opayq.com>
2018-06-26 10:25:49 +0300

    Add `objectivec` as another tag for the Objective-C lexer
    Related to #949

--- lib/rouge/lexers/objective_c.rb
+++ lib/rouge/lexers/objective_c.rb
@@ -8,7 +8,7 @@ module Rouge
       tag 'objective_c'
       title "Objective-C"
       desc 'an extension of C commonly used to write Apple software'
-      aliases 'objc', 'obj-c', 'obj_c'
+      aliases 'objc', 'obj-c', 'obj_c', 'objectivec'
       filenames '*.m', '*.h'
 
       mimetypes 'text/x-objective_c', 'application/x-objective_c'

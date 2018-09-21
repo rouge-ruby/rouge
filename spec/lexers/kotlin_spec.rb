9db2bab3b57ba04b170d6b105ced0dd07437a597
Andrew Lord <andrew.lord@getbusy.com>
2018-09-21 10:21:53 +0100

    Add unit tests for functions

--- spec/lexers/kotlin_spec.rb
+++ spec/lexers/kotlin_spec.rb
@@ -22,5 +22,71 @@ describe Rouge::Lexers::Kotlin do
     it 'recognizes one-line comments not followed by a newline (#797)' do
       assert_tokens_equal '// comment', ['Comment.Single', '// comment']
     end
+
+    describe 'functions' do
+      
+      it 'recognizes function' do
+        assert_tokens_equal 'fun makeField()',
+          ['Keyword', 'fun'],
+          ['Text', " "],
+          ['Name.Function', 'makeField'],
+          ['Punctuation', '()']
+      end
+
+      it 'recognizes backticked function' do
+        assert_tokens_equal 'fun `makeField`()',
+          ['Keyword', 'fun'],
+          ['Text', " "],
+          ['Name.Function', '`makeField`'],
+          ['Punctuation', '()']
+      end
+
+      it 'recognizes extension function' do
+        assert_tokens_equal 'fun String.makeField()',
+          ['Keyword', 'fun'],
+          ['Text', " "],
+          ['Name.Class', 'String'],
+          ['Punctuation', '.'],
+          ['Name.Function', 'makeField'],
+          ['Punctuation', '()']
+      end
+
+      it 'recognizes backticked extension function' do
+        assert_tokens_equal 'fun `String`.`makeField`()',
+          ['Keyword', 'fun'],
+          ['Text', " "],
+          ['Name.Class', '`String`'],
+          ['Punctuation', '.'],
+          ['Name.Function', '`makeField`'],
+          ['Punctuation', '()']
+      end
+
+      it 'recognizes generic function' do
+        assert_tokens_equal 'fun <T, ClassT> makeField()',
+          ['Keyword', 'fun'],
+          ['Text', " "],
+          ['Punctuation', '<'],
+          ['Text', 'T, ClassT'],
+          ['Punctuation', '>'],
+          ['Text', " "],
+          ['Name.Function', 'makeField'],
+          ['Punctuation', '()']
+      end
+
+      it 'recognizes generic extension function' do
+        assert_tokens_equal 'fun <T, ClassT> String.makeField()',
+          ['Keyword', 'fun'],
+          ['Text', " "],
+          ['Punctuation', '<'],
+          ['Text', 'T, ClassT'],
+          ['Punctuation', '>'],
+          ['Text', " "],
+          ['Name.Class', 'String'],
+          ['Punctuation', '.'],
+          ['Name.Function', 'makeField'],
+          ['Punctuation', '()']
+      end
+
+    end
   end
 end

be9bcdf1f048d52475051ef65f04ffa1343074f1
Vidar Hokstad <vidar@hokstad.com>
2018-12-23 07:03:02 +0000

    Make Markdown lexer recognise code blocks starting at beginning of input. The same issue causes it to fail recognition at the start of a source, and when parsing line by line, as it depended on a multi-line regex match. This instead matches on either just the first line or the whole block if provided, and if only the first block is provided, it pushes a dynamic state that recognizes the end of the code block but delegates all other input to the sub-lexer.

--- lib/rouge/lexers/markdown.rb
+++ lib/rouge/lexers/markdown.rb
@@ -32,16 +32,32 @@ module Rouge
         rule /^#(?=[^#]).*?$/, Generic::Heading
         rule /^##*.*?$/, Generic::Subheading
 
-        rule /(\n[ \t]*)(```|~~~)(.*?)(\n.*?\n)(\2)/m do |m|
-          sublexer = Lexer.find_fancy(m[3].strip, m[4], @options)
+        rule /^([ \t]*)(```|~~~)([^\n]*\n)((.*?)(\2))?/m do |m|
+          sublexer = Lexer.find_fancy(m[3].strip, m[5], @options)
           sublexer ||= PlainText.new(@options.merge(:token => Str::Backtick))
           sublexer.reset!
 
           token Text, m[1]
           token Punctuation, m[2]
           token Name::Label, m[3]
-          delegate sublexer, m[4]
-          token Punctuation, m[5]
+          if m[5]
+            delegate sublexer, m[5]
+          end
+
+          token Punctuation, m[6]
+          if m[6]
+          else
+            push do
+              rule /^([ \t]*)(#{m[2]})/ do |m|
+                pop!
+                token Text, m[1]
+                token Punctuation, m[2]
+              end
+              rule /^.*\n/ do |m|
+                delegate sublexer, m[1]
+              end
+            end
+          end
         end
 
         rule /\n\n((    |\t).*?\n|\n)+/, Str::Backtick

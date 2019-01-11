09b398cb50be3f1035f819c5815a067773f4927b
espositoandrea <esposito_andrea99@hotmail.com>
2019-01-11 19:46:25 +0100

    Cleaned code

--- lib/rouge/lexers/brainfuck.rb
+++ lib/rouge/lexers/brainfuck.rb
@@ -2,124 +2,28 @@
 # frozen_string_literal: true
 
 module Rouge
-  module Lexers
-    class Brainfuck < RegexLexer
-      tag 'brainfuck'
-      filenames '*.b', '*.bf'
-      mimetypes 'text/x-brainfuck'
+	module Lexers
+		class Brainfuck < RegexLexer
+			tag 'brainfuck'
+			filenames '*.b', '*.bf'
+			mimetypes 'text/x-brainfuck'
 
-      title "Brainfuck"
-      desc "The Brainfuck programming language"
+			title "Brainfuck"
+			desc "The Brainfuck programming language"
 
-      # optional comment or whitespace
-      ws = %r((?:\s|[^><\[\]\+\-\.,]*)+)
-      id = /[a-zA-Z_][a-zA-Z0-9_]*/
-
-      def self.keywords
-        @keywords ||= Set.new %w(
-          > < . , [ ] + -
-        )
-      end
-
-      def self.keywords_type
-        @keywords_type ||= Set.new %w(
-          
-        )
-      end
-
-      def self.reserved
-        @reserved ||= Set.new %w(
-          
-        )
-      end
-
-      def self.builtins
-        @builtins ||= Set.new %w(
-          
-        )
-      end
-
-      start { push :bol }
-
-      state :expr_bol do
-        mixin :inline_whitespace
-
-        rule /#if\s0/, Comment, :if_0
-        rule /#/, Comment::Preproc, :macro
-
-        rule(//) { pop! }
-      end
-
-      # :expr_bol is the same as :bol but without labels, since
-      # labels can only appear at the beginning of a statement.
-      state :bol do
-        rule /#{id}:(?!:)/, Name::Label
-        mixin :expr_bol
-      end
-
-      state :inline_whitespace do
-        rule /[ \t\r]+/, Text
-        rule /\\\n/, Text # line continuation
-        rule %r(/(\\\n)?[*].*?[*](\\\n)?/)m, Comment::Multiline
-      end
-
-      state :whitespace do
-        rule /\n+/m, Text, :bol
-        rule %r((\\[^><\[\]\+\-\.,]|[^><\[\]\+\-\.,])*?$), Comment::Single, :bol
-        mixin :inline_whitespace
-      end
-
-      state :expr_whitespace do
-        rule /\n+/m, Text, :expr_bol
-        mixin :whitespace
-      end
-
-      state :statements do
-        mixin :whitespace
-        rule /(\[|\]|\.|,|\+|\-|<|>)/, Operator
-        rule id do |m|
-          name = m[0]
-
-          if self.class.keywords.include? name
-            token Keyword
-          elsif self.class.keywords_type.include? name
-            token Keyword::Type
-          elsif self.class.reserved.include? name
-            token Keyword::Reserved
-          elsif self.class.builtins.include? name
-            token Name::Builtin
-          else
-            token Name
-          end
-        end
-      end
-
-      state :case do
-      end
-
-      state :root do
-        mixin :expr_whitespace
-
-        
-        rule(//) { push :statement }
-      end
-
-      state :statement do
-        mixin :expr_whitespace
-        mixin :statements
-      end
-
-      state :function do
-      end
-
-      state :string do
-      end
-
-      state :macro do
-      end
-
-      state :if_0 do
-      end
-    end
-  end
+			state :root do
+				# Single line comments (everything that's not a keyword)
+				rule /([^\[\]><\+\-\.,])+?/, Comment::Single
+				
+				# The starting loop-comment (it can be positioned anywhere)
+				rule /\[[^\[\]><\+\-\.,]*?\]/, Comment::Multiline
+			
+				# All keywords
+				rule /(>|<|\.|,|\[|\])/, Keyword
+				
+				# Plus (+) and minus (-) operators
+				rule /(\+|\-)/, Operator
+			end
+		end
+	end
 end

c225adb9dd75dc45d769fbfabd7f6cf912eb6769
espositoandrea <esposito_andrea99@hotmail.com>
2019-01-13 15:59:30 +0100

    Fixed indentation

--- lib/rouge/lexers/brainfuck.rb
+++ lib/rouge/lexers/brainfuck.rb
@@ -2,28 +2,28 @@
 # frozen_string_literal: true
 
 module Rouge
-	module Lexers
-		class Brainfuck < RegexLexer
-			tag 'brainfuck'
-			filenames '*.b', '*.bf'
-			mimetypes 'text/x-brainfuck'
+  module Lexers
+    class Brainfuck < RegexLexer
+      tag 'brainfuck'
+      filenames '*.b', '*.bf'
+      mimetypes 'text/x-brainfuck'
 
-			title "Brainfuck"
-			desc "The Brainfuck programming language"
+      title "Brainfuck"
+      desc "The Brainfuck programming language"
 
-			state :root do
-				# Single line comments (everything that's not a keyword)
-				rule /([^\[\]><\+\-\.,])+?/, Comment::Single
-				
-				# The starting loop-comment (it can be positioned anywhere)
-				rule /\[[^\[\]><\+\-\.,]*?\]/, Comment::Multiline
-			
-				# All keywords
-				rule /(>|<|\.|,|\[|\])/, Keyword
-				
-				# Plus (+) and minus (-) operators
-				rule /(\+|\-)/, Operator
-			end
-		end
-	end
+      state :root do
+        # Single line comments (everything that's not a keyword)
+        rule /([^\[\]><\+\-\.,])+?/, Comment::Single
+        
+        # The starting loop-comment (it can be positioned anywhere)
+        rule /\[[^\[\]><\+\-\.,]*?\]/, Comment::Multiline
+      
+        # All keywords
+        rule /(>|<|\.|,|\[|\])/, Keyword
+        
+        # Plus (+) and minus (-) operators
+        rule /(\+|\-)/, Operator
+      end
+    end
+  end
 end

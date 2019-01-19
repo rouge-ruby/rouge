8a5130775aa403e338c10915dac53d1639b230a6
Seth Bergman <sethbergman@users.noreply.github.com>
2019-01-19 07:17:40 -0600

    Added test for edge case

--- spec/lexers/docker_spec.rb
+++ spec/lexers/docker_spec.rb
@@ -9,6 +9,7 @@ describe Rouge::Lexers::Docker do
 
     it 'guesses by filename' do
       assert_guess :filename => 'Dockerfile'
+      assert_guess :filename => 'Dockerfile_*'
       assert_guess :filename => 'docker.docker'
     end
 

dcd2dede90cc86b5f2e6967d6f409901dfad7f06
Dan Allen <dan.j.allen@gmail.com>
2019-02-03 00:36:03 -0700

    resolves #1082 HTMLTable formatter delegates to format method of inner formatter
    - HTMLTable formatter delegates to format method of inner formatter instead of span method
    - append trailing newline if missing (but don't wrap as whitespace token)
    - add test
    - only yield once in stream method of HTMLTable formatter

--- spec/formatters/html_linewise_spec.rb
+++ spec/formatters/html_linewise_spec.rb
@@ -31,4 +31,12 @@ describe Rouge::Formatters::HTMLLinewise do
       assert { output == %(<div class="line-1"><span class="n">foo</span></div><div class="line-2"><span class="n">bar</span></div>) }
     end
   end
+
+  describe 'inside html table formatter' do
+    let(:input_stream) { [[Token['Text'], "foo\n"], [Token['Name'], "bar\n"]] }
+
+    it 'formats' do
+      assert { Rouge::Formatters::HTMLTable.new(subject).format(input_stream) == %(<table class="rouge-table"><tbody><tr><td class="rouge-gutter gl"><pre class="lineno">1\n2\n</pre></td><td class="rouge-code"><pre><div class="line-1">foo</div><div class="line-2"><span class="n">bar</span></div></pre></td></tr></tbody></table>) }
+    end
+  end
 end

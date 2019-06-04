7043f3c3a35a264d856d62bbcddc551b86650566
Michael Camilleri <mike@inqk.net>
2019-06-04 20:30:24 +0900

    Revert "Improve functionality of HTMLLinewise formatter (#1085)" (#1155)
    This reverts commit 6f8cfdb1797927683123960f3ea826cd2ea6f13d.

--- lib/rouge/formatters/html_linewise.rb
+++ lib/rouge/formatters/html_linewise.rb
@@ -6,17 +6,22 @@ module Rouge
     class HTMLLinewise < Formatter
       def initialize(formatter, opts={})
         @formatter = formatter
-        @tag_name = opts.fetch(:tag_name, 'div')
         @class_format = opts.fetch(:class, 'line-%i')
       end
 
       def stream(tokens, &b)
-        lineno = 0
-        token_lines(tokens) do |line_tokens|
-          yield %(<#{@tag_name} class="#{sprintf @class_format, lineno += 1}">)
-          @formatter.stream(line_tokens) {|formatted| yield formatted }
-          yield %(\n</#{@tag_name}>)
+        token_lines(tokens) do |line|
+          yield "<div class=#{next_line_class}>"
+          line.each do |tok, val|
+            yield @formatter.span(tok, val)
+          end
+          yield '</div>'
         end
+      end
+
+      def next_line_class
+        @lineno ||= 0
+        sprintf(@class_format, @lineno += 1).inspect
       end
     end
   end

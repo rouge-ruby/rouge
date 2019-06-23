3e01e15f30d630601e926b5ca393a82f38ca3114
Ashwin Maroli <ashmaroli@gmail.com>
2019-06-23 18:00:32 +0530

    Add documentation for Formatters::HTMLLineTable

--- lib/rouge/formatters/html_line_table.rb
+++ lib/rouge/formatters/html_line_table.rb
@@ -6,6 +6,21 @@ module Rouge
     class HTMLLineTable < Formatter
       tag 'html_line_table'
 
+      # @param [Rouge::Formatters::Formatter] formatter An instance of a
+      #   `Rouge::Formatters::HTML` or `Rouge::Formatters::HTMLInline`
+      # @param [Hash] opts options for HTMLLineTable instance.
+      # @option opts [Integer] :start_line line number to start from. Defaults to `1`.
+      # @option opts [String] :table_class Class name for the table.
+      #   Defaults to `"rouge-line-table"`.
+      # @option opts [String] :line_id  a `sprintf` template for generating an `id`
+      #   attribute for each table row corresponding to current line number.
+      #   Defaults to `"line-%i"`.
+      # @option opts [String] :line_class Class name for each table row.
+      #   Defaults to `"lineno"`.
+      # @option opts [String] :gutter_class Class name for rendered line-number cell.
+      #   Defaults to `"rouge-gutter"`.
+      # @option opts [String] :code_class Class name for rendered code cell.
+      #   Defaults to `"rouge-code"`.
       def initialize(formatter, opts={})
         @formatter    = formatter
         @start_line   = opts.fetch :start_line,   1

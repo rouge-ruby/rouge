5e7c37515d69dffbbcc984add85e22e36acb5e55
Mat Mariani <mat.mariani@hotmail.com>
2019-07-05 18:26:54 -0400

    changed regex syntax

--- lib/rouge/lexers/squirrel.rb
+++ lib/rouge/lexers/squirrel.rb
@@ -29,37 +29,37 @@ module Rouge
       end
 
       def self.id
-        @id ||= /@?[_a-z]\w*/i
+        @id ||= %r(@?[_a-z]\w*)i
       end
 
       state :root do
-        rule /\s+/m, Text
+        rule %r(\s+)m, Text
 
         # comments
         rule %r((#|//).*?$), Comment::Single
         rule %r(/[*].*?[*]/)m, Comment::Multiline
 
         # strings
-        rule /@"(""|[^"])*"/m, Str
-        rule /"(\\.|.)*?["\n]/, Str
-        rule /'(\\.|.)'/, Str::Char
+        rule %r(@"(""|[^"])*")m, Str
+        rule %r("(\\.|.)*?["\n]), Str
+        rule %r('(\\.|.)'), Str::Char
 
         # keywords
-        rule /(?:#{Squirrel.keywords.join('|')})\b/, Keyword
-        rule /(?:#{Squirrel.declarations.join('|')})\b/, Keyword::Declaration
-        rule /(?:true|false|null)\b/, Keyword::Constant
-        rule /(?:class)\b/, Keyword::Declaration, :class
+        rule %r((?:#{Squirrel.keywords.join('|')})\b), Keyword
+        rule %r((?:#{Squirrel.declarations.join('|')})\b), Keyword::Declaration
+        rule %r((?:true|false|null)\b), Keyword::Constant
+        rule %r((?:class)\b), Keyword::Declaration, :class
 
         # operators and punctuation
-        rule /[~!%^&*()+=|\[\]{}:;,.<>\/?-]/, Punctuation
+        rule %r([~!%^&*()+=|\[\]{}:;,.<>\/?-]), Punctuation
 
-        rule /[0-9]+(([.]([0-9]+)?)(e[-]?[0-9]+)?)/, Num::Float
-        rule /0x[0-9a-fA-F]+|0[0-7]+|[0-9]+/, Num::Integer
+        rule %r([0-9]+(([.]([0-9]+)?)(e[-]?[0-9]+)?)), Num::Float
+        rule %r(0x[0-9a-fA-F]+|0[0-7]+|[0-9]+), Num::Integer
         rule Squirrel.id, Name
       end
 
       state :class do
-        rule /\s+/m, Text
+        rule %r(\s+)m, Text
         rule Squirrel.id, Name::Class, :pop!
       end
 

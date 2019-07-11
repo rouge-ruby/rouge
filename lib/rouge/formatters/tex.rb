4adddf2b9196493f53e5d13e6f71dddbf7dcb9bd
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2019-07-11 06:02:18 +0530

    Add missing magic comments (#1258)
    This adds the coding and frozen_string_literal magic comments to three files.

--- lib/rouge/formatters/tex.rb
+++ lib/rouge/formatters/tex.rb
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
 module Rouge
   module Formatters
     class Tex < Formatter
--- lib/rouge/lexers/escape.rb
+++ lib/rouge/lexers/escape.rb
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
 module Rouge
   module Lexers
     class Escape < Lexer

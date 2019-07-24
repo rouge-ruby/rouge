1a7d7fe52836d87300f5519bac32d3b7b2915555
Michael Camilleri <mike@inqk.net>
2019-07-24 09:01:11 +0900

    Replace locals with memoised class methods

--- lib/rouge/lexers/armasm.rb
+++ lib/rouge/lexers/armasm.rb
@@ -9,46 +9,58 @@ module Rouge
       tag 'armasm'
       filenames '*.s'
 
-      preproc_keyword ||= %w(
-        define elif else endif error if ifdef ifndef include line pragma undef warning
-      )
+      def self.preproc_keyword
+        @preproc_keyword ||= %w(
+          define elif else endif error if ifdef ifndef include line pragma undef warning
+        )
+      end
 
-      file_directive ||= %w(
-        BIN GET INCBIN INCLUDE LNK
-      )
+      def self.file_directive
+        @file_directive ||= %w(
+          BIN GET INCBIN INCLUDE LNK
+        )
+      end
 
-      general_directive ||= %w(
-        ALIAS ALIGN AOF AOUT AREA ARM ASSERT ATTR CN CODE16 CODE32 COMMON CP
-        DATA DCB DCD DCDO DCDU DCFD DCFDU DCFH DCFHU DCFS DCFSU DCI DCI.N DCI.W
-        DCQ DCQU DCW DCWU DN ELIF ELSE END ENDFUNC ENDIF ENDP ENTRY EQU EXPORT
-        EXPORTAS EXTERN FIELD FILL FN FRAME FUNCTION GBLA GBLL GBLS GLOBAL IF
-        IMPORT INFO KEEP LCLA LCLL LCLS LEADR LEAF LTORG MACRO MAP MEND MEXIT
-        NOFP OPT ORG PRESERVE8 PROC QN RELOC REQUIRE REQUIRE8 RLIST RN ROUT
-        SETA SETL SETS SN SPACE STRONG SUBT THUMB THUMBX TTL WEND WHILE
-        \[ \] [|!#*=%&^]
-      )
+      def self.general_directive
+        @general_directive ||= %w(
+          ALIAS ALIGN AOF AOUT AREA ARM ASSERT ATTR CN CODE16 CODE32 COMMON CP
+          DATA DCB DCD DCDO DCDU DCFD DCFDU DCFH DCFHU DCFS DCFSU DCI DCI.N DCI.W
+          DCQ DCQU DCW DCWU DN ELIF ELSE END ENDFUNC ENDIF ENDP ENTRY EQU EXPORT
+          EXPORTAS EXTERN FIELD FILL FN FRAME FUNCTION GBLA GBLL GBLS GLOBAL IF
+          IMPORT INFO KEEP LCLA LCLL LCLS LEADR LEAF LTORG MACRO MAP MEND MEXIT
+          NOFP OPT ORG PRESERVE8 PROC QN RELOC REQUIRE REQUIRE8 RLIST RN ROUT
+          SETA SETL SETS SN SPACE STRONG SUBT THUMB THUMBX TTL WEND WHILE
+          \[ \] [|!#*=%&^]
+        )
+      end
 
-      shift_or_condition ||= %w(
-        ASR LSL LSR ROR RRX AL CC CS EQ GE GT HI HS LE LO LS LT MI NE PL VC VS
-        asr lsl lsr ror rrx al cc cs eq ge gt hi hs le lo ls lt mi ne pl vc vs
-      )
+      def self.shift_or_condition
+        @shift_or_condition ||= %w(
+          ASR LSL LSR ROR RRX AL CC CS EQ GE GT HI HS LE LO LS LT MI NE PL VC VS
+          asr lsl lsr ror rrx al cc cs eq ge gt hi hs le lo ls lt mi ne pl vc vs
+        )
+      end
 
-      builtin ||= %w(
-        ARCHITECTURE AREANAME ARMASM_VERSION CODESIZE COMMANDLINE CONFIG CPU
-        ENDIAN FALSE FPIC FPU INPUTFILE INTER LINENUM LINENUMUP LINENUMUPPER
-        OBJASM_VERSION OPT PC PCSTOREOFFSET REENTRANT ROPI RWPI TRUE VAR
-      )
+      def self.builtin
+        @builtin ||= %w(
+          ARCHITECTURE AREANAME ARMASM_VERSION CODESIZE COMMANDLINE CONFIG CPU
+          ENDIAN FALSE FPIC FPU INPUTFILE INTER LINENUM LINENUMUP LINENUMUPPER
+          OBJASM_VERSION OPT PC PCSTOREOFFSET REENTRANT ROPI RWPI TRUE VAR
+        )
+      end
 
-      operator ||= %w(
-        AND BASE CC CC_ENCODING CHR DEF EOR FATTR FEXEC FLOAD FSIZE INDEX LAND
-        LEFT LEN LEOR LNOT LOR LOWERCASE MOD NOT OR RCONST REVERSE_CC RIGHT ROL
-        ROR SHL SHR STR TARGET_ARCH_[0-9A-Z_]+ TARGET_FEATURE_[0-9A-Z_]+
-        TARGET_FPU_[A-Z_] TARGET_PROFILE_[ARM] UAL UPPERCASE
-      )
+      def self.operator
+        @operator ||= %w(
+          AND BASE CC CC_ENCODING CHR DEF EOR FATTR FEXEC FLOAD FSIZE INDEX LAND
+          LEFT LEN LEOR LNOT LOR LOWERCASE MOD NOT OR RCONST REVERSE_CC RIGHT ROL
+          ROR SHL SHR STR TARGET_ARCH_[0-9A-Z_]+ TARGET_FEATURE_[0-9A-Z_]+
+          TARGET_FPU_[A-Z_] TARGET_PROFILE_[ARM] UAL UPPERCASE
+        )
+      end
 
       state :root do
         rule %r/\n/, Text
-        rule %r/^[ \t]*#[ \t]*((#{preproc_keyword.join('|')})[ \t].*)?\n/, Comment::Preproc
+        rule %r/^[ \t]*#[ \t]*((#{ArmAsm.preproc_keyword.join('|')})[ \t].*)?\n/, Comment::Preproc
         rule %r/[ \t]+/, Text, :command
         rule %r/;.*\n/, Comment
         rule %r/\$[A-Za-z_][0-9A-Za-z_]*\.?/, Name::Namespace, :afterlabel # variable substitution or macro argument
@@ -71,11 +83,11 @@ module Rouge
           goto :args
         end
         rule %r/;.*\n/, Comment, :pop!
-        rule %r/(#{file_directive.join('|')})(?=[ \t])/ do |m|
+        rule %r/(#{ArmAsm.file_directive.join('|')})(?=[ \t])/ do |m|
           token Keyword
           goto :filespec
         end
-        rule %r/(#{general_directive.join('|')})(?=[; \t\n])/, Keyword
+        rule %r/(#{ArmAsm.general_directive.join('|')})(?=[; \t\n])/, Keyword
         rule %r/([A-Z][0-9A-Z]*|[a-z][0-9a-z]*)(\.[NWnw])?(\.[DFIPSUdfipsu]?(8|16|32|64)?){,3}(?=[^0-9A-Za-z_])/, Name::Builtin # rather than attempt to list all opcodes, rely on all-uppercase or all-lowercase rule
         rule %r/([A-Za-z_][0-9A-Za-z_]*|\|[^|\n]+\|)/, Name::Function # probably a macro name
         rule %r/\$[A-Za-z][0-9A-Za-z_]*\.?/, Name::Namespace
@@ -85,7 +97,7 @@ module Rouge
         rule %r/\n/, Text, :pop!
         rule %r/[ \t]+/, Text
         rule %r/;.*\n/, Comment, :pop!
-        rule %r/(?<![0-9A-Za-z_])(#{shift_or_condition.join('|')})(?![0-9A-Za-z_])/, Name::Builtin
+        rule %r/(?<![0-9A-Za-z_])(#{ArmAsm.shift_or_condition.join('|')})(?![0-9A-Za-z_])/, Name::Builtin
         rule %r/([A-Za-z_][0-9A-Za-z_]*|\|[^|\n]+\|)/, Name::Variable # various types of symbol
         rule %r/%[BFbf]?[ATat]?[0-9]+([A-Za-z_][0-9A-Za-z_]*)?/, Name::Label
         rule %r/(&|0[Xx])[0-9A-Fa-f]+(?![0-9A-FPa-fp])/, Literal::Number::Hex
@@ -94,8 +106,8 @@ module Rouge
         rule %r/(2_[01]+|3_[0-2]+|4_[0-3]+|5_[0-4]+|6_[0-5]+|7_[0-6]+|8_[0-7]+|9_[0-8]+|[0-9]+)(?![0-9Ee])/, Literal::Number::Integer
         rule %r/(2_[.01]+|3_[.0-2]+|4_[.0-3]+|5_[.0-4]+|6_[.0-5]+|7_[.0-6]+|8_[.0-7]+|9_[.0-8]+|[.0-9]+)([Ee][-+]?[0-9]+)?/, Literal::Number::Float
         rule %r/[@:](?=[ \t]*(8|16|32|64|128|256)[^0-9])/, Operator
-        rule %r/[.@]|\{(#{builtin.join('|')})\}/, Name::Constant
-        rule %r/([-!#%&()*+,\/<=>?^{|}]|\[|\]|!=|&&|\/=|<<|<=|<>|==|><|>=|>>|\|\||:(#{operator.join('|')}):)/, Operator
+        rule %r/[.@]|\{(#{ArmAsm.builtin.join('|')})\}/, Name::Constant
+        rule %r/([-!#%&()*+,\/<=>?^{|}]|\[|\]|!=|&&|\/=|<<|<=|<>|==|><|>=|>>|\|\||:(#{ArmAsm.operator.join('|')}):)/, Operator
         rule %r/\$[A-Za-z][0-9A-Za-z_]*\.?/, Name::Namespace
         rule %r/'/ do |m|
           token Literal::String::Char

32be2d9218829dd112e68c455f995178abe3aa33
Michael Camilleri <mike@inqk.net>
2019-08-03 03:32:06 +0900

    Improve coverage of lexer

--- lib/rouge/lexers/cython.rb
+++ lib/rouge/lexers/cython.rb
@@ -15,27 +15,29 @@ module Rouge
 
       def initialize(opts = {})
         super opts
-        @indentation = []
+        @indentation = nil
       end
 
       def self.keywords
         @keywords ||= super + %w(
-          by cdef cpdef ctypedef except? fused gil nogil 
+          by except? fused gil nogil 
         )
       end
 
-      def self.ckeywords
+      def self.c_keywords
         @ckeywords ||= %w(
-          public readonly extern api inline struct enum union class
+          public readonly extern api inline enum union
         )
       end
 
       identifier = /[a-z_]\w*/i
-      dotted_identifier = /[a-z_.][a-z0-9_.]*/i
+      dotted_identifier = /[a-z_.][\w.]*/i
       
       prepend :root do
-        rule %r/(cimport)(\s+)(#{dotted_identifier})/ do
-          groups Keyword::Namespace, Text, Name::Namespace
+        rule %r/cp?def|ctypedef/ do
+          token Keyword
+          push :c_definitions
+          push :c_start
         end
 
         rule %r/(from)((?:\\\s|\s)+)(#{dotted_identifier})((?:\\\s|\s)+)(cimport)/ do
@@ -46,76 +48,100 @@ module Rouge
                  Keyword::Namespace
         end
 
-        rule %r/cp?def|ctypedef/, Keyword, :ctype
+        rule %r/(cimport)(\s+)(#{dotted_identifier})/ do
+          groups Keyword::Namespace, Text, Name::Namespace
+        end
 
         rule %r/(struct)((?:\\\s|\s)+)/ do
           groups Keyword, Text
           push :classname
         end
-        
-        rule %r/(#{identifier})(\()/ do |m|
-          if self.class.keywords.include? m[1]
-            groups Keyword, Punctuation
-          elsif self.class.exceptions.include? m[1]
-            groups Name::Builtin, Punctuation
-          elsif self.class.builtins.include? m[1]
-            groups Name::Builtin, Punctuation
-          elsif self.class.builtins_pseudo.include? m[1]
-            groups Name::Builtin::Pseudo, Punctuation
+
+        mixin :func_call_fix
+
+        rule %r/[(,]/, Punctuation, :c_start
+      end
+
+      prepend :classname do
+        rule %r/(?:\\\s|\s)+/, Text
+      end
+
+      prepend :funcname do
+        rule %r/(?:\\\s|\s)+/, Text
+      end
+      # This is a fix for the way that function calls are lexed in the Python
+      # lexer. This should be moved to the Python lexer once confirmed that it
+      # does not cause any regressions.
+      state :func_call_fix do
+        rule %r/#{identifier}(?=\()/ do |m|
+          if self.class.keywords.include? m[0]
+            token Keyword 
+          elsif self.class.exceptions.include? m[0]
+            token Name::Builtin 
+          elsif self.class.builtins.include? m[0]
+            token Name::Builtin 
+          elsif self.class.builtins_pseudo.include? m[0]
+            token Name::Builtin::Pseudo
           else
-            groups Name::Function, Punctuation
+            token Name::Function
           end
-          goto :ctype
         end
       end
 
-      state :ctype do
+      # The Cython lexer adds three states to those already in the Python lexer.
+      # Calls to `cdef`, `cpdef` and `ctypedef` move the lexer into the :c_start
+      # state. The primary purpose of this state is to highlight datatypes. Once
+      # this has been done, the lexer moves to the :c_definitions state where
+      # the majority of text in a definition is lexed. Finally, newlines cause
+      # the lexer to move to :c_indent. This state is used to check whether we
+      # have moved out of a C block.
+
+      state :c_start do
         rule %r/[^\S\n]+/, Text
         
-        rule %r/cp?def|ctypedef/ do |m|
-          token Keyword
-        end
-        
+        rule %r/cp?def|ctypedef/, Keyword
+
+        rule %r/(?:un)?signed/, Keyword::Type
+
+        # This rule matches identifiers that could be type declarations. The
+        # lookahead matches (1) pointers, (2) arrays and (3) variable names.
         rule %r/#{identifier}(?=(?:\*+)|(?:[ \t]*\[)|(?:[ \t]+\w))/ do |m|
           if self.class.keywords.include? m[0]
             token Keyword
-          elsif self.class.ckeywords.include? m[0]
+            pop!
+          elsif %w(def).include? m[0]
+            token Keyword
+            goto :funcname
+          elsif %w(struct class).include? m[0]
+            token Keyword::Reserved
+            goto :classname
+          elsif self.class.c_keywords.include? m[0]
             token Keyword::Reserved
           else
             token Keyword::Type
+            pop!
           end
-          goto :cdef
         end
         
-        rule(//) { goto :cdef }
+        rule(//) { pop! }
       end
 
-      state :cdef do
-        rule %r/\n/, Text, :cindent
-        
-        rule %r/cp?def|ctypedef/ do |m|
-          token Keyword
-          goto :ctype
-        end
-
-        rule %r/(public|readonly|extern|api|inline)\b/, Keyword::Reserved
-        rule %r/(struct|enum|union|class)\b/, Keyword
-
+      state :c_definitions do
+        rule %r/\n/, Text, :c_indent
         mixin :root
       end
 
-      state :cindent do
+      state :c_indent do
         rule %r/[ \t]+/ do |m|
           token Text
+          goto :c_start
 
           if @indentation.nil?
             @indentation = m[0]
           elsif @indentation.length > m[0].length
             @indentation = nil
-            pop!
+            pop! 2 # Pop :c_start and :c_definitions
           end
-
-          goto :ctype
         end
         
         rule(//) { @indentation = nil; reset_stack }

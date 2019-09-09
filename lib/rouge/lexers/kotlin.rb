b86776496ba7d6ee02582517864b95428504df94
Andrew Lord <andrewlord1990@gmail.com>
2019-09-09 10:43:07 +0100

    Highlight constructors/functions in Kotlin lexer (#1321)
    This commit improves the syntax highlighting of constructor and function calls.

--- lib/rouge/lexers/kotlin.rb
+++ lib/rouge/lexers/kotlin.rb
@@ -29,14 +29,9 @@ module Rouge
 
       id = %r'(#{name_backtick})'
 
+      upper = %r'[\p{Lu}]'
+
       state :root do
-        rule %r'(\s*)(:)(\s+)(#{name_backtick})(<)' do
-          groups Text, Punctuation, Text, Name::Class, Punctuation
-          push :generic_parameters
-        end
-        rule %r'(\s*)(:)(\s+)(#{name_backtick})' do
-          groups Text, Punctuation, Text, Name::Class
-        end
         rule %r'\b(companion)(\s+)(object)\b' do
           groups Keyword, Text, Keyword
         end
@@ -47,13 +42,6 @@ module Rouge
         rule %r'\b(fun)(\s+)' do
           groups Keyword, Text
           push :function
-        end
-        rule %r'(#{name_backtick})(:)(\s+)(#{name_backtick})(<)' do
-          groups Name::Variable, Punctuation, Text, Name::Class, Punctuation
-          push :generic_parameters
-        end
-        rule %r'(#{name_backtick})(:)(\s+)(#{name_backtick})' do
-          groups Name::Variable, Punctuation, Text, Name::Class
         end
         rule %r'\b(package|import)(\s+)' do
           groups Keyword, Text
@@ -85,7 +73,13 @@ module Rouge
         rule %r'"(\\\\|\\"|[^"\n])*["\n]'m, Str
         rule %r"'\\.'|'[^\\]'", Str::Char
         rule %r"[0-9](\.[0-9]+)?([eE][+-][0-9]+)?[flFL]?|0[xX][0-9a-fA-F]+[Ll]?", Num
-        rule %r/@#{id}/, Name::Decorator
+        rule %r/(@#{upper}#{name_backtick})/, Name::Decorator
+        rule %r'(#{upper}#{name_backtick})(<)' do
+          groups Name::Class, Punctuation
+          push :generic_parameters
+        end
+        rule %r'(#{upper}#{name_backtick})', Name::Class
+        rule %r'(#{name_backtick})(?=\s*[({])', Name::Function
         rule id, Name
       end
 

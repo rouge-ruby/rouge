8031e830f1ce216cfe63b61fd80e3a34b9d983c2
Andrew Lord <andrewlord1990@gmail.com>
2019-09-10 00:38:40 +0100

    Simplify regular expressions used in Kotlin lexer (#1326)
    This commit simplifies the regular expressions used for matching
    names/identifiers.
    
    In particular:
    
    - include as an optional match backticks at the start and end of an
      identifier;
    - remove the unnecessary `id` regular expression;
    - fix annotation matching;
    - use a consistent pattern for rules creating `Name::Class` tokens;
    - remove `@` from the name pattern

--- lib/rouge/lexers/kotlin.rb
+++ lib/rouge/lexers/kotlin.rb
@@ -24,12 +24,10 @@ module Rouge
         while yield
       )
 
-      name = %r'@?[_\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Nl}][\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Nl}\p{Nd}\p{Pc}\p{Cf}\p{Mn}\p{Mc}]*'
-      name_backtick = %r'#{name}|`#{name}`'
+      name_chars = %r'[\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Nl}\p{Nd}\p{Pc}\p{Cf}\p{Mn}\p{Mc}]*'
 
-      id = %r'(#{name_backtick})'
-
-      upper = %r'[\p{Lu}]'
+      class_name = %r'`?[\p{Lu}]#{name_chars}`?'
+      name = %r'`?[_\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Nl}]#{name_chars}`?'
 
       state :root do
         rule %r'\b(companion)(\s+)(object)\b' do
@@ -55,8 +53,8 @@ module Rouge
           groups Keyword::Declaration, Text
           push :property
         end
-        rule %r/\bfun\b/, Keyword
-        rule %r/\b(?:#{keywords.join('|')})\b/, Keyword
+        rule %r'\bfun\b', Keyword
+        rule %r'\b(?:#{keywords.join('|')})\b', Keyword
         rule %r'^\s*\[.*?\]', Name::Attribute
         rule %r'[^\S\n]+', Text
         rule %r'\\\n', Text # line continuation
@@ -73,35 +71,35 @@ module Rouge
         rule %r'"(\\\\|\\"|[^"\n])*["\n]'m, Str
         rule %r"'\\.'|'[^\\]'", Str::Char
         rule %r"[0-9](\.[0-9]+)?([eE][+-][0-9]+)?[flFL]?|0[xX][0-9a-fA-F]+[Ll]?", Num
-        rule %r/(@#{upper}#{name_backtick})/, Name::Decorator
-        rule %r'(#{upper}#{name_backtick})(<)' do
+        rule %r'(@#{class_name})', Name::Decorator
+        rule %r'(#{class_name})(<)' do
           groups Name::Class, Punctuation
           push :generic_parameters
         end
-        rule %r'(#{upper}#{name_backtick})', Name::Class
-        rule %r'(#{name_backtick})(?=\s*[({])', Name::Function
-        rule id, Name
+        rule class_name, Name::Class
+        rule %r'(#{name})(?=\s*[({])', Name::Function
+        rule name, Name
       end
 
       state :package do
-        rule %r/\S+/, Name::Namespace, :pop!
+        rule %r'\S+', Name::Namespace, :pop!
       end
 
       state :class do
-        rule id, Name::Class, :pop!
+        rule class_name, Name::Class, :pop!
       end
 
       state :function do
         rule %r'(<)', Punctuation, :generic_parameters
         rule %r'(\s+)', Text
-        rule %r'(#{name_backtick})(\.)' do
+        rule %r'(#{class_name})(\.)' do
           groups Name::Class, Punctuation
         end
-        rule id, Name::Function, :pop!
+        rule name, Name::Function, :pop!
       end
 
       state :generic_parameters do
-        rule id, Name::Class
+        rule class_name, Name::Class
         rule %r'(<)', Punctuation, :generic_parameters
         rule %r'(,)', Punctuation
         rule %r'(\s+)', Text
@@ -109,14 +107,14 @@ module Rouge
       end
 
       state :property do
-        rule id, Name::Property, :pop!
+        rule name, Name::Property, :pop!
       end
 
       state :destructure do
         rule %r'(,)', Punctuation
         rule %r'(\))', Punctuation, :pop!
         rule %r'(\s+)', Text
-        rule id, Name::Property
+        rule name, Name::Property
       end
 
       state :comment do

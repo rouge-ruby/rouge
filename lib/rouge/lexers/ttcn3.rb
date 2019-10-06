88df300c8dec58ce249b769c1bc2a198ddb5c9b7
Michael Camilleri <mike@inqk.net>
2019-10-07 08:48:51 +0900

    Wrap lists of defined words

--- lib/rouge/lexers/ttcn3.rb
+++ lib/rouge/lexers/ttcn3.rb
@@ -12,16 +12,33 @@ module Rouge
       mimetypes 'text/x-ttcn3', 'text/x-ttcn'
 
       keywords = %w(
-          module import group type port component signature external execute const template function altstep testcase var timer if else select case for while do label goto start stop return break int2char int2unichar int2bit int2enum int2hex int2oct int2str int2float float2int char2int char2oct unichar2int unichar2oct bit2int bit2hex bit2oct bit2str hex2int hex2bit hex2oct hex2str oct2int oct2bit oct2hex oct2str oct2char oct2unichar str2int str2hex str2oct str2float enum2int any2unistr lengthof sizeof ispresent ischosen isvalue isbound istemplatekind regexp substr replace encvalue decvalue encvalue_unichar decvalue_unichar encvalue_o decvalue_o get_stringencoding remove_bom rnd hostid send receive setverdict
-        )
+        module import group type port component signature external
+        execute const template function altstep testcase var timer if
+        else select case for while do label goto start stop return break
+        int2char int2unichar int2bit int2enum int2hex int2oct int2str
+        int2float float2int char2int char2oct unichar2int unichar2oct
+        bit2int bit2hex bit2oct bit2str hex2int hex2bit hex2oct hex2str
+        oct2int oct2bit oct2hex oct2str oct2char oct2unichar str2int
+        str2hex str2oct str2float enum2int any2unistr lengthof sizeof
+        ispresent ischosen isvalue isbound istemplatekind regexp substr
+        replace encvalue decvalue encvalue_unichar decvalue_unichar
+        encvalue_o decvalue_o get_stringencoding remove_bom rnd hostid
+        send receive setverdict
+      )
 
       reserved = %w(
-          all alt apply assert at configuration conjunct const control delta deterministic disjunct duration fail finished fuzzy history implies inconc inv lazy mod mode notinv now omit onentry onexit par pass prev realtime seq setstate static stepsize stream timestamp until values wait
-        )
+        all alt apply assert at configuration conjunct const control
+        delta deterministic disjunct duration fail finished fuzzy
+        history implies inconc inv lazy mod mode notinv now omit onentry
+        onexit par pass prev realtime seq setstate static stepsize stream
+        timestamp until values wait
+      )
 
       types = %w(
-        anytype address boolean bitstring bytestring charstring component enumerated float integer hexstring octetstring port record set of union universal
-        )
+        anytype address boolean bitstring bytestring charstring
+        component enumerated float integer hexstring octetstring port
+        record set of union universal
+      )
 
       # optional comment or whitespace
       ws = %r((?:\s|//.*?\n|/[*].*?[*]/)+)

fd1b87d3d0c72cf15b27c91dab7bce047a94e84c
Paul Chaignon <paul.chaignon@orange.com>
2019-10-10 18:10:36 +0200

    bpf: highlight helper names
    Signed-off-by: Paul Chaignon <paul.chaignon@orange.com>

--- lib/rouge/lexers/bpf.rb
+++ lib/rouge/lexers/bpf.rb
@@ -21,6 +21,9 @@ module Rouge
         rule %r/(call)(\s+)(\d+)/i do
           groups Keyword, Text::Whitespace, Literal::Number::Integer
         end
+        rule %r/(call)(\s+)(\w+)(#)(\d+)/i do
+          groups Keyword, Text::Whitespace, Name::Builtin, Punctuation, Literal::Number::Integer
+        end
 
         # Unconditional jumps
         rule %r/(goto)(\s*)(\+\d+)?(\s*)(<?\w+>?)/i do

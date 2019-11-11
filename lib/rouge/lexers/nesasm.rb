b74b75c17d0e808a98bc1adb2301e54cb2fc27b8
Michael Camilleri <mike@inqk.net>
2019-11-11 15:10:03 +0900

    Rationalise function rules

--- lib/rouge/lexers/nesasm.rb
+++ lib/rouge/lexers/nesasm.rb
@@ -37,14 +37,13 @@ module Rouge
         rule %r/\s+/m, Text
         rule %r(;.*), Comment::Single
 
-        rule %r/\w*\:/, Name::Function #label:
         rule %r/[\(\)\,\.\[\]]/, Punctuation 
         rule %r/\#?\%[0-1]+/, Num::Bin # #%00110011 %00110011
         rule %r/\#?\$\h+/, Num::Hex  # $1f #$1f
         rule %r/\#?\d+/, Num # 10 #10
         rule %r([~&*+=\|?:<>/-]), Operator
 
-        rule %r/\b\#?\w+\b/i do |m|
+        rule %r/\#?\w+:?/i do |m|
           name = m[0].upcase
           
           if self.class.keywords.include? name

adfe5147919ce3c45b564eed19e825bfe382c479
Alex Liheng Wang <alw1@GS17003.SP.CS.CMU.EDU>
2019-11-14 00:35:38 -0500

    bibtex lexer

---
+++ lib/rouge/lexers/bibtex.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
+module Rouge
+  module Lexers
+    class BibTeX < RegexLexer
+      title 'BibTeX'
+      desc "BibTeX"
+      tag 'bibtex'
+      filenames '*.bib'
+
+      state :root do
+        rule %r/%.*/, Comment
+        rule %r/\s+/, Text::Whitespace
+        rule %r/(^\s*)@(?:(?:preamble|string(?=\s*[({]))|comment(?=\s*[{]))/, Name::Namespace
+        rule %r/(^\s*)@[^,={}'"\s]+(?=\s*{)/, Name::Class
+        rule %r/[^,={}'"\s]+(?=\s*[,}])/, Keyword
+        rule %r/([,{(]\s*)[^,={}'"\s]+(?=\s*=)/, Name::Attribute
+        rule %r/(=\s*)[0-9]+(?=\s*[,}])/, Literal::Number
+        rule %r/([=#]\s*){(?:[^{}]*|{(?:[^{}]*|{(?:[^{}]*|{[^}]*})*})*})*}/, Literal::String
+        rule %r/([=#]\s*)[^,={}'"\s]+(?=\s*[#,}])/, Literal::String
+        rule %r/("|')(?:(?!\1)[^\\]|\\(?:\r\n|[\s\S]))*\1/, Literal::String::Char
+        rule %r/#/, Literal::String::Symbol
+        rule %r/[=,{}]/, Text
+      end
+    end
+  end
+end

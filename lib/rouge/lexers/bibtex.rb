d91c6b6c221dc05eace94bc9046c2ac2413cf872
Alex Liheng Wang <alw1@GS17003.SP.CS.CMU.EDU>
2019-11-15 00:29:08 -0500

    updated bibtex lexer

--- lib/rouge/lexers/bibtex.rb
+++ lib/rouge/lexers/bibtex.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- #
 # frozen_string_literal: true
 
-# regex based on https://github.com/SaswatPadhi/prismjs-bibtex
+# regex based on https://github.com/SaswatPadhi/prismjs-bibtex and https://github.com/alecthomas/chroma/blob/master/lexers/b/bibtex.go
 
 module Rouge
   module Lexers
@@ -11,19 +11,92 @@ module Rouge
       tag 'bibtex'
       filenames '*.bib'
 
+      valid_name = /[a-z\_\@\!\$\&\*\+\-\.\/\:\;\<\>\?\[\\\]\^\`\|\~][\w\@\!\$\&\*\+\-\.\/\:\;\<\>\?\[\\\]\^\`\|\~]*/i
+
       state :root do
-        rule %r/%.*/, Comment
-        rule %r/\s+/, Text::Whitespace
-        rule %r/(^\s*)@(?:(?:preamble|string(?=\s*[({]))|comment(?=\s*[{]))/, Name::Namespace
-        rule %r/(^\s*)@[^,={}'"\s]+(?=\s*{)/, Name::Class
-        rule %r/[^,={}'"\s]+(?=\s*[,}])/, Keyword
-        rule %r/([,{(]\s*)[^,={}'"\s]+(?=\s*=)/, Name::Attribute
-        rule %r/(=\s*)[0-9]+(?=\s*[,}])/, Literal::Number
-        rule %r/([=#]\s*){(?:[^{}]*|{(?:[^{}]*|{(?:[^{}]*|{[^}]*})*})*})*}/, Literal::String
-        rule %r/([=#]\s*)[^,={}'"\s]+(?=\s*[#,}])/, Literal::String
-        rule %r/("|')(?:(?!\1)[^\\]|\\(?:\r\n|[\s\S]))*\1/, Literal::String::Char
-        rule %r/#/, Literal::String::Symbol
-        rule %r/[=,{}]/, Text
+        mixin :whitespace
+        rule %r/@comment/i, Comment
+        rule %r/@preamble/i do
+          token Name::Class
+          push :closing_brace
+          push :value
+          push :opening_brace
+        end
+        rule %r/@string/i do
+          token Name::Class
+          push :closing_brace
+          push :field
+          push :opening_brace
+        end
+        rule %r/@#{valid_name}/i do
+          token Name::Class
+          push :closing_brace
+          push :command_body
+          push :opening_brace
+        end
+        rule %r/.+/i, Comment
+      end
+
+      state :opening_brace do
+        mixin :whitespace
+        rule %r/[{(]/i, Punctuation, :pop!
+      end
+
+      state :closing_brace do
+        mixin :whitespace
+        rule %r/[})]/i, Punctuation, :pop!
+      end
+
+      state :command_body do
+        mixin :whitespace
+        rule %r/[^\s\,\}]+/i do
+          token Name::Label
+          pop!
+          push :fields
+        end
+      end
+
+      state :fields do
+        mixin :whitespace
+        rule %r/,/i, Punctuation, :field
+        rule(//) { pop! }
+      end
+
+      state :field do
+        mixin :whitespace
+        rule(/#{valid_name}/i) { token Name::Attribute; push :value; push :equal_sign }
+        rule(//) { pop! }
+      end
+
+      state :equal_sign do
+        mixin :whitespace
+        rule %r/=/i, Punctuation, :pop!
+      end
+
+      state :value do
+        mixin :whitespace
+        rule %r/#{valid_name}/i, Name::Variable
+        rule %r/"/i, Literal::String, :quoted_string
+        rule %r/\{/i, Literal::String, :braced_string
+        rule %r/[\d]+/i, Literal::Number
+        rule %r/#/i, Punctuation
+        rule(//) { pop! }
+      end
+
+      state :quoted_string do
+        rule %r/\{/i, Literal::String, :braced_string
+        rule %r/"/i, Literal::String, :pop!
+        rule %r/[^\{\"]+/i, Literal::String
+      end
+
+      state :braced_string do
+        rule %r/\{/i, Literal::String, :braced_string
+        rule %r/\}/i, Literal::String, :pop!
+        rule %r/[^\{\}]+/i, Literal::String
+      end
+
+      state :whitespace do
+        rule %r/\s+/i, Text
       end
     end
   end

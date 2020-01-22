75ca8188e6148bcdce55abc67e4f37235ca1c5b7
Jeanine Adkisson <jeanine.adkisson@gmail.com>
2020-01-21 23:59:13 -0500

    Fix crash in Terminal256 formatter with escaped tokens (#1402)
    Escaped tokens were causing a crash in the 256-color terminal formatter.
    This commit fixes the bug and adds tests.

---
+++ spec/lexers/escape_spec.rb
@@ -0,0 +1,26 @@
+describe Rouge::Lexers::Escape do
+  let(:lexer) { Rouge::Lexers::Escape.new(lang: 'json') }
+  let(:text) { '{ "foo": <!<bar>!> }' }
+  let(:result) {
+    Rouge::Formatter.with_escape do
+      formatter.format(lexer.lex(text))
+    end
+  }
+
+  describe 'html' do
+    let(:formatter) { Rouge::Formatters::HTML.new }
+
+    it 'unescapes' do
+      assert { result =~ /<bar>/ }
+    end
+  end
+
+  describe 'terminal256' do
+    let(:formatter) { Rouge::Formatters::Terminal256.new }
+    let(:text) { %({ "foo": <!\e123!> }) }
+
+    it 'unescapes' do
+      assert { result =~ /\e123/ }
+    end
+  end
+end

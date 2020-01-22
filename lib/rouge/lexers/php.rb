f441f10c8f574cadc83c62e98f2837d4449c21a6
julp <e2c2be7ed0f2f336@gmail.com>
2020-01-22 19:07:34 +0100

    pull out `true`, `false` and `null` from keywords: we already had a rule to handle their lower case form, just make it case insensitive.

--- lib/rouge/lexers/php.rb
+++ lib/rouge/lexers/php.rb
@@ -65,12 +65,12 @@ module Rouge
       def self.keywords
         @keywords ||= Set.new %w(
           and E_PARSE old_function E_ERROR or as E_WARNING parent eval
-          PHP_OS break exit case extends PHP_VERSION cfunction false
+          PHP_OS break exit case extends PHP_VERSION cfunction
           print for require continue foreach require_once declare return
-          default static do switch die stdclass echo else true elseif
+          default static do switch die stdclass echo else elseif
           var empty if xor enddeclare include virtual endfor include_once
           while endforeach global __file__ endif list __line__ endswitch
-          new __sleep endwhile not array __wakeup E_ALL null final
+          new __sleep endwhile not array __wakeup E_ALL final
           php_user_filter interface implements public private protected
           abstract clone try catch finally throw this use namespace yield
           fn callable insteadof trait __trait__ goto __namespace__ __dir__
@@ -139,7 +139,7 @@ module Rouge
           groups Keyword, Text, Name::Constant
         end
 
-        rule %r/(true|false|null)\b/, Keyword::Constant
+        rule %r/(true|false|null)\b/i, Keyword::Constant
         rule %r/(?:void|\??(?:int|float|bool|string|iterable|self|callable))\b/i, Keyword::Type
         rule %r/\$\{\$+#{id}\}/i, Name::Variable
         rule %r/\$+#{id}/i, Name::Variable

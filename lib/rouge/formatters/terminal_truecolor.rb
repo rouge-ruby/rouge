5ce4617d72f90dc40eec75d93ebb77f16ed2b5c0
http://jneen.net/ <jneen@jneen.net>
2020-01-25 23:47:08 -0500

    cache the style string

--- lib/rouge/formatters/terminal_truecolor.rb
+++ lib/rouge/formatters/terminal_truecolor.rb
@@ -5,10 +5,13 @@ module Rouge
 
       class TruecolorEscapeSequence < Terminal256::EscapeSequence
         def style_string
-          out = String.new('')
-          out << escape(['48', '2', *get_rgb(style.bg)]) if style.bg
-          out << escape(['38', '2', *get_rgb(style.fg)]) if style.fg
-          out << escape(['1']) if style[:bold] || style[:italic]
+          @style_string ||= begin
+            out = String.new('')
+            out << escape(['48', '2', *get_rgb(style.bg)]) if style.bg
+            out << escape(['38', '2', *get_rgb(style.fg)]) if style.fg
+            out << escape(['1']) if style[:bold] || style[:italic]
+            out
+          end
         end
 
         def get_rgb(color)

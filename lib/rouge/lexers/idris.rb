8694af5d83bdd7c558e5ed218e18049dd131b470
Misha Behersky <bmwant@gmail.com>
2020-03-14 19:08:00 +0200

    Keyword constants

--- lib/rouge/lexers/idris.rb
+++ lib/rouge/lexers/idris.rb
@@ -71,7 +71,7 @@ module Rouge
       end
 
       state :prelude do
-        rule %r/\b(Just|Nothing|Left|Right|True|False|LT|LTE|EQ|GT|GTE)\b/, Keyword.Constant
+        rule %r/\b(Just|Nothing|Left|Right|True|False|LT|LTE|EQ|GT|GTE)\b/, Keyword::Constant
         rule %r/\b(Type|Exists|World|IO|IntTy|FTy|File|Mode|Dec|Bool|Ordering|Either|IsJust|List|Maybe|Nat|Stream|StrM|Not|Lazy|Inf)\b/, Keyword::Type
         rule %r/\b(Eq|Ord|Num|MinBound|MaxBound|Integral|Applicative|Alternative|Cast|Foldable|Functor|Monad|Traversable|Uninhabited|Semigroup|Monoid)\b/, Name::Class
         rule %r/\b(?:#{prelude_function.join('|')})\b/, Name::Builtin

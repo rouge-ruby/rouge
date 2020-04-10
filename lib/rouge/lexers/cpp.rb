3aa615cc9d0c7d875fcdaecdd7394ce468e389f0
Michael Camilleri <mike@inqk.net>
2020-04-10 11:45:36 +0900

    Treat struct names the same as class names

--- lib/rouge/lexers/cpp.rb
+++ lib/rouge/lexers/cpp.rb
@@ -57,7 +57,7 @@ module Rouge
       dq = /\d('?\d)*/
 
       prepend :statements do
-        rule %r/class\b/, Keyword, :classname
+        rule %r/(class|struct)\b/, Keyword, :classname
         rule %r/\d+(\.\d+)?(?:h|(?:min)|s|(?:ms)|(?:us)|(?:ns))/, Num::Other
         rule %r((#{dq}[.]#{dq}?|[.]#{dq})(e[+-]?#{dq}[lu]*)?)i, Num::Float
         rule %r(#{dq}e[+-]?#{dq}[lu]*)i, Num::Float

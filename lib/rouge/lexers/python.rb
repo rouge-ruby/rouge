9f1f582fc13d16d7c02d83d6fbc61ddb05c1e3d5
Niko Strijbol <strijbol.niko@gmail.com>
2020-05-12 11:45:01 +0200

    Allow Unicode in Python identifiers (#1510)
    Python supports the use of Unicode in identifiers. This commit uses
    POSIX bracket expressions that match against Unicode characters rather
    than the more common character classes that only match ASCII characters.
    
    Co-authored-by: Michael Camilleri <mike@inqk.net>

--- lib/rouge/lexers/python.rb
+++ lib/rouge/lexers/python.rb
@@ -67,8 +67,8 @@ module Rouge
         )
       end
 
-      identifier =        /[a-z_][a-z0-9_]*/i
-      dotted_identifier = /[a-z_.][a-z0-9_.]*/i
+      identifier =        /[[:alpha:]_][[:alnum:]_]*/
+      dotted_identifier = /[[:alpha:]_.][[:alnum:]_.]*/
 
       def current_string
         @string_register ||= StringRegister.new

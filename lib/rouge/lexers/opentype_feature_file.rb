1b82bfaed6c7dfcff0291dd17e4d7828e9f6eba6
Zachary Scheuren <scheuren@adobe.com>
2020-05-25 12:39:12 -0700

    Update OpenType Feature file lexer

--- lib/rouge/lexers/opentype_feature_file.rb
+++ lib/rouge/lexers/opentype_feature_file.rb
@@ -12,24 +12,26 @@ module Rouge
 
       def self.keywords
         @keywords ||= %w(
-          Ascender Attach CapHeight CaretOffset CodePageRange Descender FontRevision FSType
-          GlyphClassDef HorizAxis.BaseScriptList HorizAxis.BaseTagList HorizAxis.MinMax
-          IgnoreBaseGlyphs IgnoreLigatures IgnoreMarks LigatureCaretByDev LigatureCaretByIndex
-          LigatureCaretByPos LineGap MarkAttachClass MarkAttachmentType NULL Panose RightToLeft
-          TypoAscender TypoDescender TypoLineGap UnicodeRange UseMarkFilteringSet Vendor
-          VertAdvanceY VertAxis.BaseScriptList VertAxis.BaseTagList VertAxis.MinMax VertOriginY
+          Ascender Attach AxisValue CapHeight CaretOffset CodePageRange DesignAxis Descender
+          ElidedFallbackName ElidedFallbackNameID ElidableAxisValueName FeatUILabelNameID
+          FeatUITooltipTextNameID FontRevision FSType GlyphClassDef HorizAxis.BaseScriptList
+          HorizAxis.BaseTagList HorizAxis.MinMax IgnoreBaseGlyphs IgnoreLigatures IgnoreMarks
+          LigatureCaretByDev LigatureCaretByIndex LigatureCaretByPos LineGap MarkAttachClass
+          MarkAttachmentType NULL OlderSiblingFontAttribute Panose ParamUILabelNameID RightToLeft
+          SampleTextNameID TypoAscender TypoDescender TypoLineGap UnicodeRange UseMarkFilteringSet
+          Vendor VertAdvanceY VertAxis.BaseScriptList VertAxis.BaseTagList VertAxis.MinMax VertOriginY
           VertTypoAscender VertTypoDescender VertTypoLineGap WeightClass WidthClass XHeight
 
           anchorDef anchor anonymous anon by contour cursive device enumerate enum
-          exclude_dflt featureNames feature from ignore include_dflt include languagesystem
-          language lookupflag lookup markClass mark nameid name parameters position pos required
-          reversesub rsub script sizemenuname substitute subtable sub table useExtension
+          exclude_dflt featureNames feature flag from ignore include_dflt include languagesystem
+          language location lookupflag lookup markClass mark nameid name parameters position pos
+          required reversesub rsub script sizemenuname substitute subtable sub table useExtension
           valueRecordDef winAscent winDescent
         )
       end
 
 
-      identifier = %r/[a-z_][a-z0-9\/_]*/i
+      identifier = %r/[a-z_][a-z0-9\/_.-]*/i
 
       state :root do
         rule %r/\s+/m, Text::Whitespace
@@ -41,17 +43,18 @@ module Rouge
           push :featurename
         end
         # } <tag> ;
-        rule %r/(\})((?:\s)*)/ do
+        rule %r/(\})((?:\s))/ do
           groups Punctuation, Text
           push :featurename
         end
         # solve include( ../path)
-        rule %r/(include)/i, Keyword, :includepath
+        rule %r/(include)(?!_dflt)/i, Keyword, :includepath
 
         rule %r/[\-\[\]\/(){},.:;=%*<>']/, Punctuation
 
         rule %r/`.*?/, Str::Backtick
         rule %r/\"/, Str, :dqs
+        rule %r/\\[^.*\s]+/i, Str::Escape
 
         # classes, start with @<nameOfClass>
         rule %r/@#{identifier}/, Name::Class
@@ -66,6 +69,7 @@ module Rouge
         end
 
         rule identifier, Name
+        rule %r/0x|\\[0-9A-Fa-f]+/, Num::Hex
         rule %r/-?\d+/, Num::Integer
       end
 
@@ -77,7 +81,7 @@ module Rouge
         rule %r/\s+/, Text::Whitespace
         rule %r/\)/, Punctuation, :pop!
         rule %r/\(/, Punctuation
-        rule %r/[a-z0-9\/_.]*/i, Str
+        rule %r/[a-z0-9\/_.-]*/i, Str
       end
 
       state :strings do
@@ -85,26 +89,12 @@ module Rouge
       end
 
       state :strings_double do
-        rule %r/[^\\"%\n]+/, Str
+        rule %r/[^"%\n]+/, Str
         mixin :strings
-      end
-
-      state :escape do
-        rule %r(\\
-          ( [\\abfnrtv"']
-          | \n
-          | N{[a-zA-z][a-zA-Z ]+[a-zA-Z]}
-          | u[a-fA-F0-9]{4}
-          | U[a-fA-F0-9]{8}
-          | x[a-fA-F0-9]{2}
-          | [0-7]{1,3}
-          )
-        )x, Str::Escape
       end
 
       state :dqs do
         rule %r/"/, Str, :pop!
-        mixin :escape
         mixin :strings_double
       end
 

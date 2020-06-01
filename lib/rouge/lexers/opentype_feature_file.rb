37ea838133120c1c469003d54cb809f3384803d6
Zachary Quinn Scheuren <scheuren@adobe.com>
2020-06-01 12:55:09 -0700

    Add new keywords to and fix bugs in OpenType feature file lexer (#1519)
    This commit updates the OpenType feature file lexer with new keywords
    and fixes to identifiers, include paths, strings and numbers. It also
    adds additional examples to the visual sample.

--- lib/rouge/lexers/opentype_feature_file.rb
+++ lib/rouge/lexers/opentype_feature_file.rb
@@ -12,24 +12,30 @@ module Rouge
 
       def self.keywords
         @keywords ||= %w(
-          Ascender Attach CapHeight CaretOffset CodePageRange Descender FontRevision FSType
-          GlyphClassDef HorizAxis.BaseScriptList HorizAxis.BaseTagList HorizAxis.MinMax
-          IgnoreBaseGlyphs IgnoreLigatures IgnoreMarks LigatureCaretByDev LigatureCaretByIndex
-          LigatureCaretByPos LineGap MarkAttachClass MarkAttachmentType NULL Panose RightToLeft
-          TypoAscender TypoDescender TypoLineGap UnicodeRange UseMarkFilteringSet Vendor
-          VertAdvanceY VertAxis.BaseScriptList VertAxis.BaseTagList VertAxis.MinMax VertOriginY
-          VertTypoAscender VertTypoDescender VertTypoLineGap WeightClass WidthClass XHeight
+          Ascender Attach AxisValue CapHeight CaretOffset CodePageRange
+          DesignAxis Descender ElidedFallbackName ElidedFallbackNameID
+          ElidableAxisValueName FeatUILabelNameID FeatUITooltipTextNameID
+          FontRevision FSType GlyphClassDef HorizAxis.BaseScriptList
+          HorizAxis.BaseTagList HorizAxis.MinMax IgnoreBaseGlyphs
+          IgnoreLigatures IgnoreMarks LigatureCaretByDev LigatureCaretByIndex
+          LigatureCaretByPos LineGap MarkAttachClass MarkAttachmentType NULL
+          OlderSiblingFontAttribute Panose ParamUILabelNameID RightToLeft
+          SampleTextNameID TypoAscender TypoDescender TypoLineGap UnicodeRange
+          UseMarkFilteringSet Vendor VertAdvanceY VertAxis.BaseScriptList
+          VertAxis.BaseTagList VertAxis.MinMax VertOriginY VertTypoAscender
+          VertTypoDescender VertTypoLineGap WeightClass WidthClass XHeight
 
-          anchorDef anchor anonymous anon by contour cursive device enumerate enum
-          exclude_dflt featureNames feature from ignore include_dflt include languagesystem
-          language lookupflag lookup markClass mark nameid name parameters position pos required
-          reversesub rsub script sizemenuname substitute subtable sub table useExtension
+          anchorDef anchor anonymous anon by contour cursive device enumerate
+          enum exclude_dflt featureNames feature flag from ignore include_dflt
+          include languagesystem language location lookupflag lookup markClass
+          mark nameid name parameters position pos required reversesub rsub
+          script sizemenuname substitute subtable sub table useExtension
           valueRecordDef winAscent winDescent
         )
       end
 
 
-      identifier = %r/[a-z_][a-z0-9\/_]*/i
+      identifier = %r/[a-z_][a-z0-9\/_.-]*/i
 
       state :root do
         rule %r/\s+/m, Text::Whitespace
@@ -41,17 +47,18 @@ module Rouge
           push :featurename
         end
         # } <tag> ;
-        rule %r/(\})((?:\s)*)/ do
+        rule %r/(\})((?:\s))/ do
           groups Punctuation, Text
           push :featurename
         end
         # solve include( ../path)
-        rule %r/(include)/i, Keyword, :includepath
+        rule %r/include\b/i, Keyword, :includepath
 
         rule %r/[\-\[\]\/(){},.:;=%*<>']/, Punctuation
 
         rule %r/`.*?/, Str::Backtick
-        rule %r/\"/, Str, :dqs
+        rule %r/\"/, Str, :strings
+        rule %r/\\[^.*\s]+/i, Str::Escape
 
         # classes, start with @<nameOfClass>
         rule %r/@#{identifier}/, Name::Class
@@ -66,6 +73,7 @@ module Rouge
         end
 
         rule identifier, Name
+        rule %r/(?:0x|\\)[0-9A-Fa-f]+/, Num::Hex
         rule %r/-?\d+/, Num::Integer
       end
 
@@ -77,37 +85,14 @@ module Rouge
         rule %r/\s+/, Text::Whitespace
         rule %r/\)/, Punctuation, :pop!
         rule %r/\(/, Punctuation
-        rule %r/[a-z0-9\/_.]*/i, Str
+        rule %r/[^\s()]+/, Str
       end
 
       state :strings do
+        rule %r/"/, Str, :pop!
+        rule %r/[^"%\n]+/, Str
         rule %r/(\([a-z0-9_]+\))?[-#0 +]*([0-9]+|[*])?(\.([0-9]+|[*]))?/i, Str
       end
-
-      state :strings_double do
-        rule %r/[^\\"%\n]+/, Str
-        mixin :strings
-      end
-
-      state :escape do
-        rule %r(\\
-          ( [\\abfnrtv"']
-          | \n
-          | N{[a-zA-z][a-zA-Z ]+[a-zA-Z]}
-          | u[a-fA-F0-9]{4}
-          | U[a-fA-F0-9]{8}
-          | x[a-fA-F0-9]{2}
-          | [0-7]{1,3}
-          )
-        )x, Str::Escape
-      end
-
-      state :dqs do
-        rule %r/"/, Str, :pop!
-        mixin :escape
-        mixin :strings_double
-      end
-
     end
   end
 end

416247a18f127a3cc48a95a7f069194aad054a19
sogaiu <983021772@users.noreply.github.com>
2020-07-01 04:59:37 +0100

    Refine floating point recognition

--- lib/rouge/lexers/janet.rb
+++ lib/rouge/lexers/janet.rb
@@ -113,7 +113,8 @@ module Rouge
         rule %r/\s+/m, Text::Whitespace
 
         rule %r/-?0x[0-9a-fA-F]+/, Num::Hex
-        rule %r/-?\d+\.\d+/, Num::Float
+        rule %r/-?\d+(\.\d+)?([eE][+-]?\d+)?/, Num::Float
+        rule %r/-?\.\d+([eE][+-]?\d+)?/, Num::Float
         rule %r/-?\d+/, Num::Integer
         # XXX: r
         # XXX: exponent

d99adf47ac9eac72478e46b0cd829f928abf076c
http://jneen.net/ <jneen@jneen.net>
2020-07-20 17:08:58 -0400

    allow setting an environment variable to enable legacy behaviour

--- lib/rouge/string_scanner.rb
+++ lib/rouge/string_scanner.rb
@@ -5,7 +5,9 @@ require 'strscan'
 
 module Rouge
   class << self
-    if ::StringScanner.method_defined?(:fixed_anchor?)
+    legacy_specified = "#{ENV['ROUGE_LEGACY_ANCHORS']}" != ""
+
+    if !legacy_specified && ::StringScanner.method_defined?(:fixed_anchor?)
       def fixed_anchor?; true end
       def string_scanner(str) StringScanner.new(str, fixed_anchor: true) end
     else

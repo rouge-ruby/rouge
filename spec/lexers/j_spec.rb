59776ad2d7b1ed24f8518f3743a5d4b3d5b66584
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2020-09-18 15:39:50 +0900

    Add primitive `}::`

--- spec/lexers/j_spec.rb
+++ spec/lexers/j_spec.rb
@@ -43,7 +43,7 @@ describe Rouge::Lexers::J do
         %w'
                ^:     ~           .           : :. ::
             ;.          !. !:     / /.       \\ \.
-          }           "           `    `:     @ @. @:
+          }       }:: "           `    `:     @ @. @:
           & &. &: &.:
           d. D. D: f. F. F.. F.: F: F:. F:: H. L: M. S:
         '.each do |modifier|

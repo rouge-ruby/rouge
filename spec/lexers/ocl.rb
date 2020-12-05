d52d6864f5941a1a483c0d0e01467e011d01d0c6
Michael Camilleri <mike@inqk.net>
2020-12-05 11:50:28 +0900

    Use 2 spaces for indentation

--- spec/lexers/ocl.rb
+++ spec/lexers/ocl.rb
@@ -2,25 +2,25 @@
 # frozen_string_literal: true
 
 describe Rouge::Lexers::OCL do
-    let(:subject) { Rouge::Lexers::OCL.new }
-  
-    describe 'guessing' do
-      include Support::Guessing
-  
-      it 'guesses by filename' do
-        assert_guess :filename => 'foo.ocl'
-      end
-  
-      it 'guesses by mimetype' do
-        assert_guess :mimetype => 'text/x-ocl'
-      end
+  let(:subject) { Rouge::Lexers::OCL.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+    assert_guess :filename => 'foo.ocl'
     end
-  
-    describe 'lexing' do
-      include Support::Lexing
-  
-      it 'recognizes one line comment on last line even when not terminated by a new line (#360)' do
-        assert_tokens_equal '-- comment', ['Comment.Single', '-- comment']
-      end
+
+    it 'guesses by mimetype' do
+    assert_guess :mimetype => 'text/x-ocl'
     end
+  end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it 'recognizes one line comment on last line even when not terminated by a new line (#360)' do
+    assert_tokens_equal '-- comment', ['Comment.Single', '-- comment']
+    end
+  end
 end

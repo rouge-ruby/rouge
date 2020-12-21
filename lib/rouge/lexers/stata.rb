6786ab4f542b28f9f4653694ce6de9b432d2f08f
Julian Reif <42722121+reifjulian@users.noreply.github.com>
2020-12-21 15:27:11 -0600

    Updated highlighting

--- lib/rouge/lexers/stata.rb
+++ lib/rouge/lexers/stata.rb
@@ -10,14 +10,22 @@ module Rouge
       filenames '*.do', '*.ado'
 	  mimetypes 'application/x-stata', 'text/x-stata'
 
-      KEYWORDS = %w(if else foreach forval to while in continue break program end)
+      # Partial list of common programming and estimation commands
+	  KEYWORDS = %w(if else foreach forval to while in of continue break program define end set mata by using use sysuse insheet save
+	     cap capture confirm creturn ereturn return list error estimates exit file open read write close frame
+		 gettoken levelsof macro shift mkmat svmat more  pause plugin preserve restore syntax adopath tokenize trace
+		 unab varabbrev version clear run do net tempfile tempvar tempname drop rename mi missing label var order compress 
+		 gen generate egen replace isid duplicates di display
+		 lincom test testnl predict suest 
+		 reg regress probit logit ivregress logistic svy gmm ivprobit ivtobit 
+	  )
 	  
-	  KEYWORD_TYPES = %w(byte int long float double str strL local global scalar matrix)
+	  KEYWORD_TYPES = %w(byte int long float double str strL local global numlist varlist newlist scalar matrix)
 
       # Complete list of functions by name, as of Stata 16
       PRIMITIVE_FUNCTIONS = %w(
         abbrev abs acos acosh age age_frac asin asinh atan atan2 atanh autocode
-		betaden binomial binomialp binomialtail binormal birthday bofd byteorder
+		betaden binomial bino#mialp binomialtail binormal birthday bofd byteorder
 		c _caller cauchy cauchyden cauchytail Cdhms ceil char chi2 chi2den chi2tail Chms 
 		chop cholesky clip Clock clock clockdiff cloglog Cmdyhms Cofc cofC Cofd cofd coleqnumb 
 		collatorlocale collatorversion colnfreeparms colnumb colsof comb cond corr cos cosh 
@@ -44,7 +52,7 @@ module Rouge
 		s scalar seconds sign sin sinh smallestdouble soundex soundex_nara sqrt ss ssC strcat strdup string stritrim strlen strlower 
 		strltrim strmatch strofreal strpos strproper strreverse strrpos strrtrim strtoname strtrim strupper subinstr subinword substr sum sweep 
 		t tan tanh tC tc td tden th tin tm tobytes tq trace trigamma trunc ttail tukeyprob tw twithin 
-		uchar udstrlen udsubstr uisdigit uisletter ustrcompare ustrcompareex ustrfix ustrfrom ustrinvalidcnt ustrleft ustrlen ustrlower
+		uchar udstrlen udsubstr uisdigit uisletter uniform ustrcompare ustrcompareex ustrfix ustrfrom ustrinvalidcnt ustrleft ustrlen ustrlower
 		ustrltrim ustrnormalize ustrpos ustrregexm ustrregexra ustrregexrf ustrregexs ustrreverse ustrright ustrrpos ustrrtrim ustrsortkey
 		ustrsortkeyex ustrtitle ustrto ustrtohex ustrtoname ustrtrim ustrunescape ustrupper ustrword ustrwordcount usubinstr usubstr
 		vec vecdiag week weekly weibull weibullden weibullph weibullphden weibullphtail weibulltail wofd word wordbreaklocale wordcount
@@ -55,21 +63,27 @@ module Rouge
 	  
 	    rule %r/\s+/m, Text::Whitespace
 	  
-		# Comments: *, //, /*
-		#rule %r/^\*.*$/, Comment::Single
-		rule %r/^\s*\*.*$/, Comment::Single
-		
+		# Single-line comment: *
+		# Ideally, this match would be anchored to start of line + optional white space
+		# That would avoid matching when * is used as an operator (eg 3*5)
+		#   Example: match "   * text" and "*text" but not "text*text"
+		#   Solution: rule %r/^\s*\*.*$/, Comment::Single
+		#   Problem: This solution above works on rubular.com, but the '^' does not match "   * text" in rouge for some reason
+		# Instead, use solution below, which matchs all * comments correctly but incorrectly matches the * operator
+		rule %r/\*.*$/, Comment::Single
+
+		# in-line comment: //
 		rule %r/\/\/.*?$/, Comment::Single
+		
+		# Multi-line comment: /* and */
 		rule %r(/(\\\n)?[*].*?[*](\\\n)?/)m, Comment::Multiline
 		
 		# Strings indicated by double-quotes
-        rule %r/"(\\.|.)*?"/m, Str::Double
+		 rule %r/"(\\.|.)*?"/, Str::Double
 		
-		# Format locals (`') and globals($) as strings
-		rule %r/`(\\.|.)*?'/m, Str::Double
+		# Format locals (`') and globals ($) as strings
+		rule %r/`(\\.|.)*?'/, Str::Double
 		rule %r/(?<!\w)\$\w+/, Str::Double
-
-        rule %r/%[^%]*?%/, Operator
 
         rule %r/0[xX][a-fA-F0-9]+([pP][0-9]+)?[Li]?/, Num::Hex
         rule %r/[+-]?(\d+([.]\d+)?|[.]\d+)([eE][+-]?\d+)?[Li]?/, Num
@@ -92,7 +106,7 @@ module Rouge
 
         rule %r/[\[\]{}();,]/, Punctuation
 
-		rule %r([-<>?*+^/\\!.=~:&|]), Operator
+		rule %r([-<>?*+^/\\!#.=~:&|]), Operator
       end
     end
   end

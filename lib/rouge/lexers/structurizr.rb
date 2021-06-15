25dc1bc0c5ce55b9ed85ffeb680e833e6208e35a
David Asabina <vid@bina.me>
2021-06-15 18:04:44 +0200

    feat: Implement basic lexing of Structurizr
    based on implementation details in https://github.com/structurizr/dsl/blob/master/src/main/java/com/structurizr/dsl/StructurizrDslParser.java
    
    feat: Lex Structurizr comments
    
    feat: Stub dumb lexing of Structurizr constants
    
    fix: Add .structurizr as Structurizr extension
    
    fix: Lazy match multiline Structurizr comment body
    
    chore: Define Structurizr indentifier lexer
    
    chore: Match Structurizr constant definition
    
    fix: Only match 1-liner Structurizr constant defs
    
    feat: Lex Structurizr include clauses
    
    feat: Mark .c4 as Structurizr extension
    
    feat: Lex Structurizr !adrs and !docs
    
    refactor: Modify Structurizr :string for reuse
    
    feat: Roughly lex constructs and body
    
    feat: Lex Structurizr props & refactor to pass
    
    Structurizr properties are being highlighted correctly.
    All tests green.
    
    chore: Fix regex boundaries + lex http URL strings
    
    fix: Lex implied Structurizr relationship
    
    fix: Add element and relationship to keywords
    
    fix: Sloppily lex dynamic branch blocks
    
    fix: Remove string pop + pop before block entry
    
    Removed the pop on the :string state that is only used as mixin and thus
    causes invalid state transitions (we traverse back too far) -- pop! may
    have been useful if we pushed into the :string state but since we mixin
    the :string state there is no need.
    
    Pop out of :construct_arg upon detection of a "{" and before traversing
    into the :construct_body state. This may be redesigned again as this the
    pop before push seems a bit smelly, although the double pop upon "}" was
    also a questionable design choice. WIP
    
    fix: Swap state upon string_body entry
    
    It is tricky to determine the pop! condition after the :string_body has
    been exited, so instead of double popping upon detection of the
    concluding double-quote in :string_body to get to the parent of (:string
    -> :string_body), we simply swap :string for :string_body and just
    assume that a single pop returns the cursor to the calling state.
    
    fix: Refactor (simplify) + fix (ref,themes)

--- lib/rouge/lexers/structurizr.rb
+++ lib/rouge/lexers/structurizr.rb
@@ -7,14 +7,190 @@ module Rouge
       title "Structurizr"
       desc "The Structurizr DSL (https://github.com/structurizr/dsl)"
       tag "structurizr"
-      filenames "*.dsl"
+      filenames "*.c4", "*.dsl", "*.structurizr"
+
+      # TODO: Verify all examples parse from https://github.com/structurizr/dsl/tree/master/examples
+
+      def self.keywords
+        @keywords ||= Set.new %w(workspace model enterprise group person
+        softwareSystem container component deploymentEnvironment
+        deploymentGroup deploymentNode infrastructureNode
+        softwareSystemInstance containerInstance healthCheck element tags url
+        properties perspectives views systemLandscape systemContext container
+        component filtered dynamic deployment custom include exclude autoLayout
+        animation title styles element relationship theme themes branding
+        terminology configuration users)
+      end
+
+      def self.keywords_include
+        @keywords_include ||= Set.new %w(!include)
+      end
+
+      def self.keywords_doc_include
+        @keywords_doc_include ||= Set.new %w(!docs !adrs)
+      end
+
+      keywords = self.keywords
+      keywords_include = self.keywords_include
+      keywords_doc_include = self.keywords_doc_include
+
+      def self.identifier
+        %r(\w+)
+      end
+
+      identifier = self.identifier
+
+      state :identifier do
+        mixin :whitespace
+        rule %r/(#{identifier})\b/, Name::Variable
+      end
 
       state :whitespace do
-        rule /\s+/m, Text::Whitespace
+        rule %r/\s+/m, Text::Whitespace
+      end
+
+      state :comment do
+        rule %r/\/\/.*$/, Comment
+        rule %r/#.*$/, Comment
+        rule %r(\/\*.*?\*\/$)m, Comment::Multiline
+      end
+
+      state :include do
+        mixin :whitespace
+        rule %r/(#{keywords_include.join('|')})\b(\p{Blank}+)([^\n]+)/i do
+          groups Keyword, Text::Whitespace, Text
+        end
+        rule %r/(#{keywords_doc_include.join('|')})\b(\p{Blank}+)([^\n]+)/i do
+          groups Comment, Text::Whitespace, Text
+        end
+      end
+
+      state :constant do
+        mixin :whitespace
+        rule %r/(!constant)\b(\p{Blank}+)(#{identifier})\b(\p{Blank}+)/i do
+          groups Keyword::Declaration, Text::Whitespace, Name::Constant, Text::Whitespace
+          push :value
+        end
+      end
+
+      state :value do
+        rule %r/\n/, Text::Whitespace, :pop!
+        mixin :whitespace
+        mixin :string
+        rule %r/\*/, Str::Symbol
+        rule %r/\b(?:true|false)\b/, Keyword::Constant, :pop!
+        # TODO: Use identifier mixin and explore refactoring for statelessness
+        rule %r/(#{identifier})\b/, Name::Variable, :pop!
+        rule %r/#([a-fA-F0-9]{3,8})\b/, Str::Symbol, :pop!
+        rule %r/-?(?:0|[1-9]\d*)\.\d+(?:e[+-]?\d+)?/i, Num::Float, :pop!
+        rule %r/-?(?:0|[1-9]\d*)(?:e[+-]?\d+)?/i, Num::Integer, :pop!
+      end
+
+      state :string do
+        rule %r/https?:\S+/, Str::Other
+        rule %r/"/, Str::Double, :string_body
+      end
+
+      state :string_body do
+        #rule %r/[$][{]/, Str::Interpol, :string_intp
+        rule %r/($\{)([a-zA-Z0-9-_.]+?)(\})/i do
+          groups Str::Interpol, Name::Variable, Str::Interpol
+        end
+        rule %r/"/, Str::Double, :pop!
+        rule %r/./, Str
+      end
+
+      state :string_intp do
+        rule %r/}/, Str::Interpol, :pop!
+        rule %r/(#{identifier})\b/, Name::Variable, :pop!
+      end
+
+      state :property do
+        rule %r/(#{identifier})\b(\p{Blank}*)/i do
+          groups Keyword::Declaration, Text::Whitespace
+          push :value
+        end
+      end
+
+      state :construct do
+        # TODO: Avoid interp
+        # https://github.com/rouge-ruby/rouge/blob/master/docs/LexerDevelopment.md#special-words
+        rule %r/\b(#{keywords.join('|')})\b/i, Keyword::Type, :construct_args
+      end
+
+      state :construct_args do
+        rule %r/\n/, Text::Whitespace, :pop!
+        rule %r/\*/, Str::Symbol
+
+        mixin :whitespace
+        mixin :string
+        mixin :identifier
+
+        rule %r/{/ do
+          token Punctuation
+          pop!
+          push :context
+        end
+      end
+
+      state :context do
+        mixin :whitespace
+        mixin :comment
+
+        mixin :base
+
+        rule %r/}/, Punctuation, :pop!
+        rule %r/{/, Punctuation, :context
+      end
+
+      # NOTE: State definitions in line with StructurizrDslParser.java
+      state :assignment do
+        rule %r/(#{identifier})\b(\p{Blank}*)(=)/i do
+          groups Keyword::Declaration, Text::Whitespace, Operator
+        end
+      end
+
+      state :relationship_explicit do
+        rule %r/(#{identifier})\b(\p{Blank}*)(->)/i do
+          groups Keyword::Declaration, Text::Whitespace, Operator
+        end
+      end
+
+      state :relationship_implicit do
+        rule %r/(->)(\p{Blank}*)/i do
+          groups Operator, Text::Whitespace
+        end
+      end
+
+      state :rel do
+        mixin :relationship_explicit
+        mixin :relationship_implicit
+      end
+
+      state :ref do
+        rule %r/(ref)(\p{Blank}+)({)/i do
+          groups Keyword::Declaration, Text::Whitespace, Punctuation
+        end
+
+        rule %r/(ref)(\p{Blank}+)/i, Keyword::Declaration, :construct_args
+      end
+
+      state :base do
+        mixin :whitespace
+        mixin :comment
+
+        mixin :assignment
+        mixin :rel
+        mixin :ref
+
+        mixin :construct
+        mixin :property
+        mixin :include
+        mixin :constant
       end
 
       state :root do
-        mixin :whitespace
+        mixin :base
       end
     end
   end

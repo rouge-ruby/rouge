1facc23565e6af09273d47c59ea9f7d8d05dbe51
Toru Niina <niina.toru.68u@gmail.com>
2022-07-02 19:25:13 +0900

    lexer: TOML: Support more integer and floating formats (#1832)
    * Fix floating point format in TOML lexer
    
    in TOML, the decimal point must be surrounded by at least one digit on
    each side.
    
    * Support sign of number in TOML lexer
    
    * Support special float (nan/inf) in TOML lexer
    
    * Support hex, oct, bin integers in TOML lexer
    
    * Support underscores between digits in TOML lexer
    
    * Add more int/float examples to TOML visual sample
    
    * Use Num::Oct and non-captured group for oct int
    
    Co-authored-by: Tan Le <tan.le@hey.com>
    
    * Use Num::Bin and non-captured group for bin int
    
    Co-authored-by: Tan Le <tan.le@hey.com>
    
    * Use Num::Hex and non-captured group for hex int
    
    Co-authored-by: Tan Le <tan.le@hey.com>
    
    * Use non-captured group if capture is not needed
    
    Co-authored-by: Tan Le <tan.le@hey.com>
    
    * Use non-captured group if capture is not needed
    
    Co-authored-by: Tan Le <tan.le@hey.com>
    
    * Use non-captured group if capture is not needed
    
    Co-authored-by: Tan Le <tan.le@hey.com>
    
    Co-authored-by: Tan Le <tan.le@hey.com>

--- lib/rouge/lexers/toml.rb
+++ lib/rouge/lexers/toml.rb
@@ -28,9 +28,14 @@ module Rouge
 
         rule %r/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/, Literal::Date
 
-        rule %r/(\d+\.\d*|\d*\.\d+)([eE][+-]?[0-9]+)?j?/, Num::Float
-        rule %r/\d+[eE][+-]?[0-9]+j?/, Num::Float
-        rule %r/\-?\d+/, Num::Integer
+        rule %r/[+-]?\d+(?:_\d+)*\.\d+(?:_\d+)*(?:[eE][+-]?\d+(?:_\d+)*)?/, Num::Float
+        rule %r/[+-]?\d+(?:_\d+)*[eE][+-]?\d+(?:_\d+)*/, Num::Float
+        rule %r/[+-]?(?:nan|inf)/, Num::Float
+
+        rule %r/0x\h+(?:_\h+)*/, Num::Hex
+        rule %r/0o[0-7]+(?:_[0-7]+)*/, Num::Oct
+        rule %r/0b[01]+(?:_[01]+)*/, Num::Bin
+        rule %r/[+-]?\d+(?:_\d+)*/, Num::Integer
       end
 
       state :root do

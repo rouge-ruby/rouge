2cd924346c8622eb2b85c099b37341e7526315f1
Raghu R <rraghu.11502@gmail.com>
2022-10-07 11:10:04 +0530

    Add basic regex rules

--- lib/rouge/lexers/apl.rb
+++ lib/rouge/lexers/apl.rb
@@ -1,8 +1,9 @@
 module Rouge
   module Lexers
     class APL < RegexLexer
+      # Many rules taken from https://github.com/Alhadis/language-apl/blob/master/grammars/apl.cson
       title "APL"
-      desc "APL, a tool of thought for array processing"
+      desc "APL, a tool of thought for array programming"
       tag 'apl'
       filenames '*.apl', '*.apla', '*.aplf', '*.aplo', '*.apln', '*.aplc', '*.apli', '*.mipage'
       mimetypes 'text/apl'
@@ -14,6 +15,14 @@ module Rouge
       state :root do
         rule %r/'/, Str::Single, :str
         rule %r/⍝.*/, Comment::Single
+        rule %r/¯?[0-9][¯0-9A-Za-z]*(?:\.[¯0-9Ee][¯0-9A-Za-z]*)*|¯?\.[0-9Ee][¯0-9A-Za-z]/, Num::Number
+        rule %r/(?x)^\s*([A-Z_a-zÀ-ÖØ-Ýßà-öø-üþ∆⍙Ⓐ-Ⓩ][A-Z_a-zÀ-ÖØ-Ýßà-öø-üþ∆⍙Ⓐ-Ⓩ¯0-9]*)(:)/, Name::Label
+        rule %r'[+×÷⌊⌈⍟○!∧∨⍲⍱<≤=≥>≠?⍷,⍪⌷⍳⍴↑↓⊣⊢⊤⊥\/⌿⍀⌽⊖⍉⍋⍒⌹≡≢⊂⊃∩∪⍎⍕⊆⍸]', Keyword
+        rule %r/[¨⍤⌸⍨⍣\\.∘⍠&⌶⌺@]/, Operator
+        rule %r/[⍺⍵⍶⍹χ∇λ]/, Keyword::Variable
+        rule %r/◊;¯←→\[\]\{\}\(\)/, Punctuation
+        rule %r/[A-Z_a-zÀ-ÖØ-Ýßà-öø-üþ∆⍙Ⓐ-Ⓩ][A-Z_a-zÀ-ÖØ-Ýßà-öø-üþ∆⍙Ⓐ-Ⓩ¯0-9]*/, Name::Variable
+        rule %/\s+/, Text
       end
 
       state :str do

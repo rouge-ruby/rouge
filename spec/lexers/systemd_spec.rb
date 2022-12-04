ccc7b6b888ddd53edc39df8a26b3e7f48bc60bde
Michael Herold <opensource@michaeljherold.com>
2022-12-04 10:56:00 -0600

    Allow quoteless continuations in Systemd
    When continuing a command on the next line, Systemd's file format allows
    you to elide the quote if you're escaping the newline.
    
    This change enables support for this syntax.

--- spec/lexers/systemd_spec.rb
+++ spec/lexers/systemd_spec.rb
@@ -15,4 +15,26 @@ describe Rouge::Lexers::SystemD do
       assert_guess :mimetype => 'text/x-systemd-unit'
     end
   end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it 'handles continuations without quotes' do
+      systemd_file = <<~SYSTEMD
+        [Service]
+        ExecStart=/exec \\
+                  without-a-quote
+      SYSTEMD
+
+      assert_no_errors systemd_file
+      assert_tokens_equal systemd_file,
+        ['Keyword', '[Service]'],
+        ['Text', "\n"],
+        ['Name.Tag', 'ExecStart'],
+        ['Punctuation', '='],
+        ['Text', '/exec '],
+        ['Literal.String.Escape', "\\\n"],
+        ['Text', "          without-a-quote\n"]
+    end
+  end
 end

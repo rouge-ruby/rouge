ad5f989bb46c85bc37b073f5f2b5f107542c3bdd
Demian Ferreiro <epidemian@gmail.com>
2023-05-04 02:51:09 -0300

    Highlight Ruby's and/or/not logical operators

--- lib/rouge/lexers/ruby.rb
+++ lib/rouge/lexers/ruby.rb
@@ -106,8 +106,8 @@ module Rouge
       end
 
       keywords = %w(
-        BEGIN END alias begin break case defined\? do else elsif end
-        ensure for if in next redo rescue raise retry return super then
+        BEGIN END alias and begin break case defined\? do else elsif end
+        ensure for if in next not or redo rescue raise retry return super then
         undef unless until when while yield
       )
 

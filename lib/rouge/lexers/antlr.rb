a104d0db28ff1fcd9d30c5cf416b1f34f005decc
msagca <51697294+msagca@users.noreply.github.com>
2023-05-08 22:39:38 +0300

    improve antlr lexer

--- lib/rouge/lexers/antlr.rb
+++ lib/rouge/lexers/antlr.rb
@@ -8,54 +8,106 @@ module Rouge
       desc "ANother Tool for Language Recognition"
       tag 'antlr'
       filenames '*.g4'
-      word = /[a-z][a-zA-Z0-9_]*/
-      WORD = /[A-Z][a-zA-Z0-9_]*/
       def self.keywords
         @keywords ||= Set.new %w(
-          import fragment lexer parser grammar protected public private returns
-          locals throws catch finally mode options tokens channels channel type
-          popMode pushMode skip more
+          catch channel channels finally fragment grammar import lexer locals
+          mode more options parser popMode private protected public pushMode
+          returns skip throws tokens type
         )
       end
-      state :string do
-        rule %r/'/, Str, :pop!
-        rule %r/\\./, Str::Escape
-        rule %r/[^\\'\n]+/, Str
-      end
-      state :charset do
-        rule %r/\]/, Name::Variable, :pop!
-        rule %r/\\./, Str::Escape
-        rule %r/[^\\\[\]\n]+/, Name::Variable
-      end
-      state :label do
-        rule %r/[a-zA-Z0-9_]+/, Name::Label, :pop!
+      identifier = %r/[A-Za-z][a-zA-Z0-9_]*/
+      integer = %r/0|[1-9][0-9]*/
+      parse_rule_name = false
+      rule_name = %r/[a-z][a-zA-Z0-9_]*/
+      token_name = %r/[A-Z][a-zA-Z0-9_]*/
+      state :whitespace do
         rule %r/\s+/, Text
       end
-      state :block do
-        rule %r/{/, Name::Builtin, :block
-        rule %r/}/, Name::Builtin, :pop!
-        rule %r/\\./, Str::Escape
-        rule %r/[^\\{}\s]+/, Name::Builtin
-        rule %r/\s+/, Text
-      end
-      state :root do
-        rule %r/\s+/, Text
+      state :comment_and_whitespace do
+        mixin :whitespace
         rule %r(//.*?$), Comment::Single
         rule %r(/\*.*?\*/)m, Comment::Multiline
-        rule %r/'/, Str, :string
-        rule %r/\[/, Name::Variable, :charset
-        rule %r/#/, Name::Label, :label
-        rule %r/{/, Name::Builtin, :block
-        rule %r/0|[1-9][0-9]*/, Num::Integer
-        rule %r/[-?@*+<=>~$.]/, Operator
-        rule %r/[;:()|]/, Punctuation
+      end
+      state :escape_sequence do
         rule %r/\\./, Str::Escape
-        rule WORD, Name::Class
-        rule word do |m|
+      end
+      state :string do
+        mixin :escape_sequence
+        rule %r/'/, Str, :pop!
+        rule %r/[^\\'\n]+/, Str
+      end
+      state :options_spec do
+        mixin :comment_and_whitespace
+        rule %r/{/, Punctuation
+        rule %r/}/, Punctuation, :pop!
+        rule %r/=/, Operator, :option_value
+        rule identifier, Name::Variable
+      end
+      state :option_value do
+        mixin :comment_and_whitespace
+        rule %r/;/, Punctuation, :pop!
+        rule %r/./, Punctuation
+        rule %r/'/, Str, :string
+        rule %r/{/, Punctuation, :action_block
+        rule identifier, Name::Constant
+        rule integer, Num::Integer
+      end
+      state :action_block do
+        mixin :escape_sequence
+        mixin :whitespace
+        rule %r/[^\\{}\s]+/, Name::Function
+        rule %r/{/, Punctuation, :action_block
+        rule %r/}/, Punctuation, :pop!
+      end
+      state :arg_action_block do
+        mixin :escape_sequence
+        mixin :whitespace
+        rule %r/[^\\\[\]]+/, Str
+        rule %r/\]/, Str, :pop!
+      end
+      state :label do
+        mixin :comment_and_whitespace
+        rule rule_name, Name::Label, :pop!
+      end
+      state :root do
+        mixin :comment_and_whitespace
+        rule %r/'/, Str, :string
+        rule %r/[|,.()]/, Punctuation
+        rule %r/[@<>=~\-+?*]/, Operator
+        rule %r/{/, Punctuation, :action_block
+        rule %r/\[/, Str, :arg_action_block
+        rule %r/#/, Name::Label, :label
+        rule %r/0|[1-9][0-9]*/, Num::Integer
+        rule %r/:/ do
+          token Punctuation
+          parse_rule_name = false
+        end
+        rule %r/;/ do
+          token Punctuation
+          parse_rule_name = true
+        end
+        rule token_name do
+          if parse_rule_name
+            token Name::Label
+          else
+            token Name::Class
+          end
+        end
+        rule rule_name do |m|
           if self.class.keywords.include? m[0]
             token Keyword
+            case m[0]
+            when 'grammar'
+              parse_rule_name = true
+            when 'options'
+              push :options_spec
+            when 'throws'
+              parse_rule_name = false
+            end
+          elsif parse_rule_name
+            token Name::Label
           else
-            token Name::Function
+            token Name::Variable
           end
         end
       end

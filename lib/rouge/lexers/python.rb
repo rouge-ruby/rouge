4c385e5daddc6790a1e371598efba88771cb47ae
Frederick Cheung <frederick.cheung@gmail.com>
2023-07-28 10:37:05 +0100

    Add python support for match/case
    This adds match & case as keywords, used by python 3.10 structural pattern matching ( https://peps.python.org/pep-0634/ )

--- lib/rouge/lexers/python.rb
+++ lib/rouge/lexers/python.rb
@@ -21,7 +21,7 @@ module Rouge
           assert break continue del elif else except exec
           finally for global if lambda pass print raise
           return try while yield as with from import yield
-          async await nonlocal
+          async await nonlocal match case
         )
       end
 

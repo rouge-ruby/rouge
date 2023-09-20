bd84a15f35d2883818cfd9d4343752391f171c50
fnand <fwschenck@gmail.com>
2023-09-20 22:38:18 +0200

    Add builtins

--- lib/rouge/lexers/mojo.rb
+++ lib/rouge/lexers/mojo.rb
@@ -2,19 +2,30 @@
 # frozen_string_literal: true
 
 module Rouge
-    module Lexers
-      load_lexer 'python.rb'
+  module Lexers
+    load_lexer 'python.rb'
   
-      class Mojo < Python
-        title "Mojo"
-        desc "The Mojo programming language"
-        tag 'mojo'
-        filenames '*.mojo', '*.🔥'
+    class Mojo < Python
+      title "Mojo"
+      desc "The Mojo programming language"
+      tag 'mojo'
+      filenames '*.mojo', '*.🔥'
+      mimetypes 'text/x-mojo', 'application/x-mojo'
 
 
-
-
+    
+      def self.builtins
+        @builtins ||= %w(
+          Float16 Float32 Float64 Int8 Int16 Int32 Int64
+          UInt8 UInt16 UInt32 UInt64
+        )
+      end
+      state :root do
         rule %r/(fn)((?:\s|\\\s)+)/ do
             groups Keyword, Text
             push :funcname
         end
+      end
+    end
+  end
+end

ae2403b8c94f90c8f532d08208272e23822981d3
Jake Duddy <evaluationcontext@gmail.com>
2024-04-08 23:02:17 +0100

    Added Dax Lexer, spec and samples

---
+++ spec/lexers/dax_spec.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
+describe Rouge::Lexers::DAX do
+  let(:subject) { Rouge::Lexers::DAX.new }
+
+  describe 'guessing' do
+    include Support::Guessing
+
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.dax'
+    end
+
+  end
+
+end

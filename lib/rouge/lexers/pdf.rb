82785e3e76463c8e0faf2a75052eb558030f115b
petervwyatt <26521615+petervwyatt@users.noreply.github.com>
2024-07-02 15:09:55 +1000

    Update pdf.rb

--- lib/rouge/lexers/pdf.rb
+++ lib/rouge/lexers/pdf.rb
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*- #
 # frozen_string_literal: true
 
-# Adapted from Rouge lib/rouge/lexers/PostScript.rb
+#
+# PDF = Portable Document Format page description language
+# As defined by ISO 32000-2:2020 including resolved errata from https://pdf-issues.pdfa.org/
+#
+# The PDF syntax is also know as "COS" and can also be used with FDF (Forms Data Field) files. 
+#
+# This is a token-based parser ONLY! It is intended to syntax highlight full or partial fragments 
+# of nicely written hand-writteen PDF syntax in documentation such as ISO specifications. It is NOT
+# intended to cope with real-world PDFs that will contain arbitrary binary data (that form invalid
+# UTF-8 sequences and generate "ArgumentError: invalid byte sequence in UTF-8" Ruby errors) and 
+# other types of malformation/syntax errors. 
+#
+# Author: Peter Wyatt, CTO, PDF Association. 2024
+#
 module Rouge
   module Lexers
     class Pdf < RegexLexer
@@ -13,79 +26,80 @@ module Rouge
       mimetypes "application/pdf", "application/fdf" # IANA registered media types
 
       # PDF and FDF files must start with "%PDF-x.y" or "%FDF-x.y"
-      # where x is the emajor version (1-9) and y is the minor version (0-9)
-      # Supports invalid PDF versions.
+      # where x is the single digit major version and y is the single digit minor version.
       def self.detect?(text)
-        return true if /^%[PF]DF-[1-9]\.\d/ =~ text
+        return true if /^%(P|F)DF-\d.\d/ =~ text
       end
 
-      # PDF Delimiters (ISO 32000-2:2020, Table 2) including Ruby whitespace 
-      delimiter = %s"()<>\[\]/%\s"
-
-      delimiter_end = Regexp.new("(?=[#{delimiter}])")
-      valid_name_chars = Regexp.new("[^#{delimiter}]")
+      # PDF Delimiters (ISO 32000-2:2020, Table 1 and Table 2).
+      # Ruby whitespace "\s" is /[ \t\r\n\f\v]/ which does not include NUL (ISO 32000-2:2020, Table 1).
+      # PDF also support 2 character EOL sequences.
+      delimiter = %r/\(\)<>\[\]\/%\s/
 
       state :root do
-        # PDF only has single-line comments: from "%"" to EOL
-        rule %r'%.*?$', Comment::Single
+        # Start-of-file header comment is special (comment is up to EOL)
+        rule %r/^%(P|F)DF-\d\.\d.*$/, Comment::Special
 
-        # PDF Boolean object
-        rule %r'(false|true)#{delimiter_end}', Keyword::Constant
+        # End-of-file marker comment is special (comment is up to EOL)
+        rule %r/^%%EOF.*$/, Comment::Special
 
-        # PDF Null object
-        rule %r'(null)#{delimiter_end}', Keyword::Constant
+        # PDF only has single-line comments: from "%" to EOL
+        rule %r/%.*$/, Comment::Single
 
-        # PDF Hex string - can contain whitespace and span multiple lines
-        rule %r/<[0-9A-Fa-f\s]+>/m, String::Other
+        # PDF Boolean and null object keywords
+        rule %r/(false|true|null)/, Keyword::Constant
 
-        # PDF Dictionary
-        rule %r/<</, Variable::Instance
-        rule %r/>>/, Variable::Instance
+        # PDF Dictionary and array object start and end tokens
+        rule %r/(<<|>>|\[|\])/, Punctuation
 
-        # PDF Arrays
-        rule %r/\[/, Variable::Instance
-        rule %r/\]/, Variable::Instance
+        # PDF Hex string - can contain whitespace and span multiple lines.
+        # This rule must be after "<<"/">>"
+        rule %r/<[0-9A-Fa-f\s]*>/m, Str::Other
 
-        # PDF literal strings are complex (multi-line, escapes, etc.); enter separate state.
-        rule %r'\(', String, :stringliteral
+        # PDF literal strings are complex (multi-line, escapes, etc.). Use separate state machine.
+        rule %r/\(/, Str, :stringliteral
 
-        # PDF Name objects - can be empty (nothing after "/")
-        # No special processing needed for 2-digit hex codes starting with "#"
-        rule %r'/\/#{valid_name_chars}*#{delimiter_end}', Name::Variable
+        # PDF Name objects - can be empty (i.e., nothing after "/").
+        # No special processing required for 2-digit hex codes that start with "#".
+        rule %r/\/[^\(\)<>\[\]\/%\s]*/, Name::Entity
 
-        # PDF objects and stream (no checking of object number)
-        rule %r/\d+\s\d+obj#{delimiter_end}/, Keyword::Declaration
-        rule %r/stream/, Keyword::Declaration
-        rule %r/endstream/, Keyword::Declaration
-        rule %r/endobj/, Keyword::Declaration
+        # PDF objects and stream (no checking of object ID)
+        # Note that object number and generation numbers do not have sign.
+        rule %r/\d+\s\d+\sobj/, Keyword::Declaration
+        rule %r/(endstream|endobj|stream)/, Keyword::Declaration
 
-        # PDF file layout
-        rule %r/xref/, Keyword::Constant
-        rule %r/trailer/, Keyword::Constant
-        rule %r/startxref/, Keyword::Constant
+        # PDF conventional file layout keywords
+        rule %r/(startxref|trailer|xref)/, Keyword::Constant
 
-        # PDF cross reference section entries (supposedly 20 bytes including EOL)
-        rule %r/^\d{10} \d{5} [nf]\s?/, Keyword::Namespace
+        # PDF cross reference section entries (20 bytes including EOL).
+        # Explicit single SPACE separators.
+        rule %r/^\d{10} \d{5} (n|f)\s*$/, Keyword::Namespace
 
-        # PDF Indirect reference (lax, allows zero as the object number)
-        rule %r/\d+\s\d+R#{delimiter_end}/. Keyword::Pseudo
+        # PDF Indirect reference (lax, allows zero as the object number).
+        # Requires terminating delimiter lookahead to disambiguate from "RG" operator
+        rule %r/\d+\s\d+\sR(?=[\(\)<>\[\]\/%\s])/, Keyword::Variable
 
         # PDF Real object
-        rule %r/(\-|\+)?([0-9]+\.?|[0-9]*\.[0-9]+|[0-9]+\.[0-9]*)#{delimiter_end}/, Number::Float
+        rule %r/(\-|\+)?([0-9]+\.?|[0-9]*\.[0-9]+|[0-9]+\.[0-9]*)/, Num::Float
 
         # PDF Integer object
-        rule %r/(\-|\+)?[0-9]+#{delimiter_end}/, Number::Integer
+        rule %r/(\-|\+)?[0-9]+/, Num::Integer
 
-        # most likely PDF content stream operators
-        rule valid_name_chars, Operator::Word
+        # A run of non-delimiters is most likely a PDF content stream 
+        # operator (ISO 32000-2:2020, Annex A).
+        rule %r/[^\(\)<>\[\]\/%\s]+/, Operator::Word
+
+        # Whitespace (except inside strings and comments) is ignored = /[ \t\r\n\f\v]/.
+        # Ruby doesn't include NUL as whitespace (vs ISO 32000-2:2020 Table 1)
+        rule %r/\s+/, Text::Whitespace
       end
 
-      # PDF literal string - see ISO 32000-2:2020 clause 7.3.4.2 and Table 3
+      # PDF literal string. See ISO 32000-2:2020 clause 7.3.4.2 and Table 3
       state :stringliteral do
-        rule %r/\(/, String, :stringliteral     # recursive for internal balanced literal strings
-        rule %r/\)/, String, :pop!
-        rule %r/\\([0-7]{3}|n|r|t|b|f|\\)/, String::Escape
-        rule %r/[^\(\)\\]+/, String
+        rule %r/\(/, Str, :stringliteral     # recursive for internal balanced(!) literal strings
+        rule %r/\)/, Str, :pop!
+        rule %r/\\([0-7]{3}|n|r|t|b|f|\\)/, Str::Escape
+        rule %r/[^\(\)\\]+/, Str
       end
     end
   end

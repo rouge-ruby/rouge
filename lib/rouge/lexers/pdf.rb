9cf372f293ea7f617330ce410b964f7440bf0fe4
petervwyatt <26521615+petervwyatt@users.noreply.github.com>
2024-07-02 15:47:46 +1000

    Fixups

--- lib/rouge/lexers/pdf.rb
+++ lib/rouge/lexers/pdf.rb
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*- #
 # frozen_string_literal: true
+# vim: set ts=2 sw=2 et:
 
 # PDF = Portable Document Format page description language
 # As defined by ISO 32000-2:2020 including resolved errata from https://pdf-issues.pdfa.org/
 #
-# The PDF syntax is also know as "COS" and can also be used with FDF (Forms Data Field) files. 
+# The PDF syntax is also known as "COS" and can be used with FDF (Forms Data Field) files as
+# per ISO 32000-2:2020 clause 12.7.8. 
 #
 # This is a token-based parser ONLY! It is intended to syntax highlight full or partial fragments 
 # of nicely written hand-writteen PDF syntax in documentation such as ISO specifications. It is NOT
 # intended to cope with real-world PDFs that will contain arbitrary binary data (that form invalid
 # UTF-8 sequences and generate "ArgumentError: invalid byte sequence in UTF-8" Ruby errors) and 
-# other types of malformation/syntax errors. 
+# other types of malformations or syntax errors. 
 #
 # Author: Peter Wyatt, CTO, PDF Association. 2024
 #
@@ -19,10 +21,10 @@ module Rouge
     class Pdf < RegexLexer
       title "PDF"
       desc "PDF - Portable Document Format (ISO 32000)"
-      tag "Pdf"
-      aliases "fdf", "cos"
-      filenames "*.pdf", "*.fdf"
-      mimetypes "application/pdf", "application/fdf" # IANA registered media types
+      tag 'pdf'
+      aliases "fdf", 'cos'
+      filenames '*.pdf', '*.fdf'
+      mimetypes 'application/pdf', 'application/fdf' # IANA registered media types
 
       # PDF and FDF files must start with "%PDF-x.y" or "%FDF-x.y"
       # where x is the single digit major version and y is the single digit minor version.
@@ -33,7 +35,7 @@ module Rouge
       # PDF Delimiters (ISO 32000-2:2020, Table 1 and Table 2).
       # Ruby whitespace "\s" is /[ \t\r\n\f\v]/ which does not include NUL (ISO 32000-2:2020, Table 1).
       # PDF also support 2 character EOL sequences.
-      delimiter = %r/\(\)<>\[\]\/%\s/
+      # NOT USED: delimiter = %r/\(\)<>\[\]\/%\s/
 
       state :root do
         # Start-of-file header comment is special (comment is up to EOL)

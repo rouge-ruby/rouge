48313bb06c8f4b13b8cb22bac4985bd0d6c90e0e
Bozhidar <bobi@microweber.com>
2024-08-21 19:40:56 +0300

    Update keywords.rb

--- lib/rouge/lexers/davi/keywords.rb
+++ lib/rouge/lexers/davi/keywords.rb
@@ -10,7 +10,6 @@ module Rouge
     class DAVI
       def self.builtins
         @builtins ||= {}.tap do |b|
-          b["Array"] = Set.new ["array_change_key_case", "array_chunk", "array_column", "array_combine", "array_count_values", "array_diff_assoc", "array_diff_key", "array_diff_uassoc", "array_diff_ukey", "array_diff", "array_fill_keys", "array_fill", "array_filter", "array_flip", "array_intersect_assoc", "array_intersect_key", "array_intersect_uassoc", "array_intersect_ukey", "array_intersect", "array_key_exists", "array_key_first", "array_key_last", "array_keys", "array_map", "array_merge_recursive", "array_merge", "array_multisort", "array_pad", "array_pop", "array_product", "array_push", "array_rand", "array_reduce", "array_replace_recursive", "array_replace", "array_reverse", "array_search", "array_shift", "array_slice", "array_splice", "array_sum", "array_udiff_assoc", "array_udiff_uassoc", "array_udiff", "array_uintersect_assoc", "array_uintersect_uassoc", "array_uintersect", "array_unique", "array_unshift", "array_values", "array_walk_recursive", "array_walk", "array", "arsort", "asort", "compact", "count", "current", "each", "end", "extract", "in_array", "key_exists", "key", "krsort", "ksort", "list", "natcasesort", "natsort", "next", "pos", "prev", "range", "reset", "rsort", "shuffle", "sizeof", "sort", "uasort", "uksort", "usort"]
           b["Array"] = Set.new ["append", "range", "slice", "sort"]
           b["Conversion"] = Set.new ["int", "str"]
           b["File System"] = Set.new ["fileGetContents"]

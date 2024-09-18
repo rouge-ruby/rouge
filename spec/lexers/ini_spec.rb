4d24220334b05df8492e6fb34d2444bf9c7ade65
Tan Le <numbat@fastmail.com>
2024-09-18 12:09:32 +1000

    Add more extensions and mimetypes for INI lexer

--- spec/lexers/ini_spec.rb
+++ spec/lexers/ini_spec.rb
@@ -11,10 +11,13 @@ describe Rouge::Lexers::INI do
       assert_guess :filename => 'foo.ini'
       assert_guess :filename => '.gitconfig'
       assert_guess :filename => 'setup.cfg', :source => "[metadata]\nname = my_package"
+      assert_guess :filename => '.editorconfig'
+      assert_guess :filename => 'request.inf'
     end
 
     it 'guesses by mimetype' do
       assert_guess :mimetype => 'text/x-ini'
+      assert_guess :mimetype => 'text/inf'
     end
   end
 end

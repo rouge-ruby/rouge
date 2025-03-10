c3a502b846107e33c4994fa250e3b9f6354a113a
Maximov Valery <maximovvalery@gmail.com>
2025-03-10 08:34:40 +0300

    Update xbsl_spec.rb

--- spec/lexers/xbsl_spec.rb
+++ spec/lexers/xbsl_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- #
 # frozen_string_literal: true
 
-describe Rouge::Lexers::Bsl do
+describe Rouge::Lexers::XBsl do
   let(:subject) { Rouge::Lexers::XBsl.new }
 
   describe 'guessing' do

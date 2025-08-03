9ec84b064b370ed2186a81e2dbc6bbb8e0d912a6
Dylan Beattie <dylan@dylanbeattie.net>
2025-08-03 10:28:20 +0100

    Replaces " with ' for consistency with existing code

--- spec/lexers/css_spec.rb
+++ spec/lexers/css_spec.rb
@@ -4,19 +4,19 @@
 describe Rouge::Lexers::CSS do
   let(:subject) { Rouge::Lexers::CSS.new }
 
-  describe "guessing" do
+  describe 'guessing' do
     include Support::Guessing
-    it "guesses by filename" do
-      assert_guess :filename => "foo.css"
+    it 'guesses by filename' do
+      assert_guess :filename => 'foo.css'
     end
 
-    it "guesses by mimetype" do
-      assert_guess :mimetype => "text/css"
+    it 'guesses by mimetype' do
+      assert_guess :mimetype => 'text/css'
     end
   end
 
-  describe "nested CSS rules" do
-    it "should highlight nested selectors correctly" do
+  describe 'nested CSS rules' do
+    it 'should highlight nested selectors correctly' do
       nested_css = <<~CSS
         .container {
           width: 100%;
@@ -45,23 +45,23 @@ describe Rouge::Lexers::CSS do
       nested_selectors = tokens.select { |token, _| token == Rouge::Token::Tokens::Name::Tag }
       nested_selector_texts = nested_selectors.map(&:last)
 
-      assert nested_selector_texts.include?(".header ")
-      assert nested_selector_texts.include?(".title ")
-      assert nested_selector_texts.include?("&:hover ")
-      assert nested_selector_texts.include?("&.active ")
+      assert nested_selector_texts.include?('.header ')
+      assert nested_selector_texts.include?('.title ')
+      assert nested_selector_texts.include?('&:hover ')
+      assert nested_selector_texts.include?('&.active ')
 
       # Check that properties are still correctly tokenized
       properties = tokens.select { |token, _| token == Rouge::Token::Tokens::Name::Label }
       property_texts = properties.map(&:last)
 
-      assert property_texts.include?("width")
-      assert property_texts.include?("font-size")
-      assert property_texts.include?("font-weight")
-      assert property_texts.include?("background-color")
-      assert property_texts.include?("border")
+      assert property_texts.include?('width')
+      assert property_texts.include?('font-size')
+      assert property_texts.include?('font-weight')
+      assert property_texts.include?('background-color')
+      assert property_texts.include?('border')
     end
 
-    it "should handle deeply nested CSS rules" do
+    it 'should handle deeply nested CSS rules' do
       deep_nested_css = <<~CSS
         .level1 {
           color: black;
@@ -86,12 +86,12 @@ describe Rouge::Lexers::CSS do
       nested_selectors = tokens.select { |token, _| token == Rouge::Token::Tokens::Name::Tag }
       nested_selector_texts = nested_selectors.map(&:last)
 
-      assert nested_selector_texts.include?(".level2 ")
-      assert nested_selector_texts.include?(".level3 ")
-      assert nested_selector_texts.include?(".level4 ")
+      assert nested_selector_texts.include?('.level2 ')
+      assert nested_selector_texts.include?('.level3 ')
+      assert nested_selector_texts.include?('.level4 ')
     end
 
-    it "should handle nested CSS within @media queries" do
+    it 'should handle nested CSS within @media queries' do
       media_nested_css = <<~CSS
         @media (max-width: 768px) {
           .container {
@@ -109,12 +109,12 @@ describe Rouge::Lexers::CSS do
       # Check that @media is recognized as keyword
       media_tokens = tokens.select { |token, _| token == Rouge::Token::Tokens::Keyword }
       media_texts = media_tokens.map(&:last)
-      assert media_texts.include?("@media")
+      assert media_texts.include?('@media')
 
       # Check that nested selectors within media query work
       nested_selectors = tokens.select { |token, _| token == Rouge::Token::Tokens::Name::Tag }
       nested_selector_texts = nested_selectors.map(&:last)
-      assert nested_selector_texts.include?(".header ")
+      assert nested_selector_texts.include?('.header ')
     end
   end
 end

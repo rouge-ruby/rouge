6c030e597194ef07d77a2d6e83c541922830fe22
gabrielhdt <7418676+gabrielhdt@users.noreply.github.com>
2025-09-02 13:35:29 +0200

    Fix tests and add test sample

--- spec/lexers/hll_spec.rb
+++ spec/lexers/hll_spec.rb
@@ -1,3 +1,11 @@
-if "guesses by filename" do
-    assert_guess :filename => "foo.hll"
+describe Rouge::Lexers::HLL do
+    let(:subject) { Rouge::Lexers::HLL.new }
+
+    describe 'guessing' do
+        include Support::Guessing
+
+        it "guesses by filename" do
+            assert_guess :filename => "foo.hll"
+        end
+    end
 end

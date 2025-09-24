482d61c9af606759db1ba3ec9f3f14d89e3dfb44
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-24 05:50:57 +0200

    chore: Scaffolded lexers. Added demos for gjs and gts.

---
+++ lib/rouge/lexers/gts.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
+module Rouge
+  module Lexers
+    load_lexer 'typescript.rb'
+
+    class Gts < Typescript
+      title "Template Tag (gts)"
+      desc "Ember.js, TypeScript with <template> tags"
+      tag "gts"
+      filenames "*.gts"
+      mimetypes "text/x-gts", "application/x-gts"
+
+      def initialize(*)
+        super
+      end
+    end
+  end
+end

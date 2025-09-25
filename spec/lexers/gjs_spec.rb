0c28f30e2a3578fa4d1bed9080f02af7882ca71a
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-25 09:24:33 +0200

    refactor: Added specs, demos, and visual samples for gjs and gts

--- spec/lexers/gjs_spec.rb
+++ spec/lexers/gjs_spec.rb
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
 describe Rouge::Lexers::Gjs do
   let(:subject) { Rouge::Lexers::Gjs.new }
 
@@ -5,7 +8,9 @@ describe Rouge::Lexers::Gjs do
     include Support::Guessing
 
     it 'guesses by filename' do
-      assert_guess :filename => 'foo.gjs'
+      assert_guess :filename => 'app/components/ui/form.gjs'
+      assert_guess :filename => 'app/templates/form.gjs'
+      assert_guess :filename => 'tests/integration/components/ui/form-test.gjs'
     end
 
     it 'guesses by mimetype' do
--- spec/lexers/gts_spec.rb
+++ spec/lexers/gts_spec.rb
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*- #
+# frozen_string_literal: true
+
 describe Rouge::Lexers::Gts do
   let(:subject) { Rouge::Lexers::Gts.new }
 
@@ -5,7 +8,9 @@ describe Rouge::Lexers::Gts do
     include Support::Guessing
 
     it 'guesses by filename' do
-      assert_guess :filename => 'foo.gts'
+      assert_guess :filename => 'app/components/ui/form.gts'
+      assert_guess :filename => 'app/templates/form.gts'
+      assert_guess :filename => 'tests/integration/components/ui/form-test.gts'
     end
 
     it 'guesses by mimetype' do

17587ee234fa56c2c021f931407e8bb075c333fb
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-10-06 09:58:36 +0200

    Add specs from rouge-ruby#2163 (#3)
    * chore: Copied specs from #2163
    
    * refactor: Extracted specs for lexing
    
    ---------
    
    Co-authored-by: Dan Knutsen <dknutsen@gmail.com>

--- spec/lexers/gjs_spec.rb
+++ spec/lexers/gjs_spec.rb
@@ -18,4 +18,97 @@ describe Rouge::Lexers::Gjs do
       assert_guess :mimetype => 'application/x-gjs'
     end
   end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it 'lexes the opening <template> tag' do
+      assert_tokens_equal '<template>',
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", ">"]
+    end
+
+    it 'lexes the closing </template> tag' do
+      assert_tokens_equal '<template></template>',
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", "></"], ["Keyword", "template"], ["Name.Tag", ">"]
+      assert_tokens_equal '<template> </template>',
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", ">"], ["Text", " "], ["Name.Tag", "</"], ["Keyword", "template"], ["Name.Tag", ">"]
+    end
+
+    it 'lexes a Glimmer component' do
+      code = <<~FILE
+        import Component from '@glimmer/component';
+        import { t } from 'ember-intl';
+
+        export default class Hello extends Component {
+          <template>
+            <div data-test-message>
+              {{t "hello.message" name=@name}}
+            </div>
+          </template>
+        }
+      FILE
+
+      assert_no_errors code
+    end
+
+    it 'lexes a template-only component (1)' do
+      code = <<~FILE
+        import { t } from 'ember-intl';
+
+        const Hello = <template>
+          <div data-test-message>
+            {{t "hello.message" name=@name}}
+          </div>
+        </template>;
+
+        export default Hello;
+      FILE
+
+      assert_no_errors code
+    end
+
+    it 'lexes a template-only component (2)' do
+      code = <<~FILE
+        import { t } from 'ember-intl';
+
+        <template>
+          <div data-test-message>
+            {{t "hello.message" name=@name}}
+          </div>
+        </template>;
+      FILE
+
+      assert_no_errors code
+    end
+
+    it 'lexes a rendering test' do
+      code = <<~FILE
+        import { render } from '@ember/test-helpers';
+        import { setupIntl } from 'ember-intl/test-support';
+        import { setupRenderingTest } from 'ember-qunit';
+        import Hello from 'my-app/components/hello';
+        import { module, test } from 'qunit';
+
+        module('Integration | Component | hello', function (hooks) {
+          setupRenderingTest(hooks);
+          setupIntl(hooks, 'en-us');
+
+          test('it renders', async function (assert) {
+            await render(
+              <template>
+                <Hello @name="Zoey" />
+              </template>
+            );
+
+            assert.dom('[data-test-message]').hasText('Hello, Zoey!');
+          });
+        });
+      FILE
+
+      # Currently unable to assert with `assert_no_errors` because
+      # the `handlebars` lexer doesn't support arguments like `@name`
+      assert_tokens_includes code,
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", ">"]
+    end
+  end
 end
--- spec/lexers/gts_spec.rb
+++ spec/lexers/gts_spec.rb
@@ -18,4 +18,117 @@ describe Rouge::Lexers::Gts do
       assert_guess :mimetype => 'application/x-gts'
     end
   end
+
+  describe 'lexing' do
+    include Support::Lexing
+
+    it 'lexes the opening <template> tag' do
+      assert_tokens_equal '<template>',
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", ">"]
+    end
+
+    it 'lexes the closing </template> tag' do
+      assert_tokens_equal '<template></template>',
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", "></"], ["Keyword", "template"], ["Name.Tag", ">"]
+      assert_tokens_equal '<template> </template>',
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", ">"], ["Text", " "], ["Name.Tag", "</"], ["Keyword", "template"], ["Name.Tag", ">"]
+    end
+
+    it 'lexes a Glimmer component' do
+      code = <<~FILE
+        import Component from '@glimmer/component';
+        import { t } from 'ember-intl';
+
+        interface HelloSignature {
+          Args: {
+            name: string;
+          };
+        }
+
+        export default class Hello extends Component<HelloSignature> {
+          <template>
+            <div data-test-message>
+              {{t "hello.message" name=@name}}
+            </div>
+          </template>
+        }
+      FILE
+
+      assert_no_errors code
+    end
+
+    it 'lexes a template-only component (1)' do
+      code = <<~FILE
+        import type { TOC } from '@ember/component/template-only';
+        import { t } from 'ember-intl';
+
+        interface HelloSignature {
+          Args: {
+            name: string;
+          };
+        }
+
+        const Hello: TOC<HelloSignature> = <template>
+          <div data-test-message>
+            {{t "hello.message" name=@name}}
+          </div>
+        </template>;
+
+        export default Hello;
+      FILE
+
+      assert_no_errors code
+    end
+
+    it 'lexes a template-only component (2)' do
+      code = <<~FILE
+        import type { TOC } from '@ember/component/template-only';
+        import { t } from 'ember-intl';
+
+        interface HelloSignature {
+          Args: {
+            name: string;
+          };
+        }
+
+        <template>
+          <div data-test-message>
+            {{t "hello.message" name=@name}}
+          </div>
+        </template> satisfies TOC<HelloSignature>;
+      FILE
+
+      assert_no_errors code
+    end
+
+    it 'lexes a rendering test' do
+      code = <<~FILE
+        import { render } from '@ember/test-helpers';
+        import { setupIntl } from 'ember-intl/test-support';
+        import { setupRenderingTest } from 'ember-qunit';
+        import Hello from 'my-app/components/hello';
+        import { module, test } from 'qunit';
+
+        module('Integration | Component | hello', function (hooks) {
+          setupRenderingTest(hooks);
+          setupIntl(hooks, 'en-us');
+
+          test('it renders', async function (assert) {
+            await render(
+              <template>
+                <Hello @name="Zoey" />
+              </template>
+            );
+
+            assert.dom('[data-test-message]').hasText('Hello, Zoey!');
+          });
+        });
+      FILE
+
+      # Currently unable to assert with `assert_no_errors` because
+      # the `handlebars` lexer doesn't support arguments like `@name`
+      assert_tokens_includes code,
+        ["Name.Tag", "<"], ["Keyword", "template"], ["Name.Tag", ">"]
+    end
+  end
 end

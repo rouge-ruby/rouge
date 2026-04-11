7ac8f91e9a3e1df6a44b873cc8b01fa15256e9bc
jneen <jneen@jneen.net>
2026-04-10 21:12:45 -0400

    fix sasscommon

--- lib/rouge/lexers/sass/common.rb
+++ lib/rouge/lexers/sass/common.rb
@@ -156,12 +156,9 @@ module Rouge
 
       state :attr_common do
         mixin :has_interp
-        rule id do |m|
-          if CSS.properties.include? m[0]
-            token Name::Label
-          else
-            token Name::Attribute
-          end
+        keywords id do
+          rule CSS::PROPERTIES, Name::Label
+          default Name::Attribute
         end
       end
 

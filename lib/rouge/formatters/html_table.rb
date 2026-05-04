988151ebcf03bd737d2895465066ed6ab9cf4202
jneen <jneen@jneen.net>
2026-05-04 13:31:19 -0400

    Revert "Update HTMLTable formatter to delegate to inner formatter (#1083)"
    This reverts commit a050f9e6155326f11b6ca73fa3a66bc16fafde65.

--- lib/rouge/formatters/html_table.rb
+++ lib/rouge/formatters/html_table.rb
@@ -16,35 +16,46 @@ module Rouge
       end
 
       def style(scope)
-        yield %(#{scope} .rouge-table { border-spacing: 0 })
-        yield %(#{scope} .rouge-gutter { text-align: right })
+        yield "#{scope} .rouge-table { border-spacing: 0 }"
+        yield "#{scope} .rouge-gutter { text-align: right }"
       end
 
       def stream(tokens, &b)
-        last_val = nil
-        num_lines = tokens.reduce(0) {|count, (_, val)| count + (last_val = val).count(?\n) }
-        formatted = @inner.format(tokens)
-        unless last_val && last_val.end_with?(?\n)
+        num_lines = 0
+        last_val = ''
+        formatted = String.new('')
+
+        tokens.each do |tok, val|
+          last_val = val
+          num_lines += val.scan(/\n/).size
+          formatted << @inner.span(tok, val)
+        end
+
+        # add an extra line for non-newline-terminated strings
+        if last_val[-1] != "\n"
           num_lines += 1
-          formatted << ?\n unless formatted.end_with?(?\n)
+          @inner.span(Token::Tokens::Text::Whitespace, "\n") { |str| formatted << str }
         end
 
         # generate a string of newline-separated line numbers for the gutter>
-        formatted_line_numbers = (@start_line..(@start_line + num_lines - 1)).map do |i|
-          sprintf(@line_format, i)
-        end.join(?\n) << ?\n
+        formatted_line_numbers = (@start_line..num_lines+@start_line-1).map do |i|
+          sprintf("#{@line_format}", i) << "\n"
+        end.join('')
 
-        buffer = [%(<table class="#@table_class"><tbody><tr>)]
+        numbers = %(<pre class="lineno">#{formatted_line_numbers}</pre>)
+
+        yield %(<table class="#@table_class"><tbody><tr>)
+
         # the "gl" class applies the style for Generic.Lineno
-        buffer << %(<td class="#@gutter_class gl" aria-hidden="true">)
-        buffer << %(<pre class="lineno">#{formatted_line_numbers}</pre>)
-        buffer << '</td>'
-        buffer << %(<td class="#@code_class"><pre><code>)
-        buffer << formatted
-        buffer << '</code></pre></td>'
-        buffer << '</tr></tbody></table>'
+        yield %(<td class="#@gutter_class gl" aria-hidden="true">)
+        yield numbers
+        yield '</td>'
 
-        yield buffer.join
+        yield %(<td class="#@code_class"><pre>)
+        yield formatted
+        yield '</pre></td>'
+
+        yield "</tr></tbody></table>"
       end
     end
   end
--- spec/formatters/html_linewise_spec.rb
+++ spec/formatters/html_linewise_spec.rb
@@ -40,12 +40,4 @@ describe Rouge::Formatters::HTMLLinewise do
       assert { output == %(<span class="line-1">foo\n</span><span class="line-2"><span class="n">bar</span>\n</span>) }
     end
   end
-
-  describe 'inside html table formatter' do
-    let(:input_stream) { [[Token['Text'], "foo\n"], [Token['Name'], "bar\n"]] }
-
-    it 'should delegate to linewise formatter' do
-      assert { Rouge::Formatters::HTMLTable.new(subject).format(input_stream) == %(<table class="rouge-table"><tbody><tr><td class="rouge-gutter gl" aria-hidden="true"><pre class="lineno">1\n2\n</pre></td><td class="rouge-code"><pre><code><div class="line-1">foo\n</div><div class="line-2"><span class="n">bar</span>\n</div></code></pre></td></tr></tbody></table>) }
-    end
-  end
 end

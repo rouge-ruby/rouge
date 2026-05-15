63c996ec688c66752fc38730591d5e21aba721f6
jneen <jneen@jneen.net>
2026-05-15 11:53:54 -0400

    stop using interpolated regexes in jsp

--- lib/rouge/lexers/jsp.rb
+++ lib/rouge/lexers/jsp.rb
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*- #
 # frozen_string_literal: true
 
+# [jneen] TODO: this lexer needs an overhaul, and probably needs
+# to be split into two delegating lexers, since interpolation is
+# allowed even in the values of specialized tags.
+#
+# Right now something like:
+#
+#    <c:set var="realm" value='<%=AppConfig.getRealm().name()%>' />
+#
+# doesn't highlight the AppConfig part, because <c:set> is a specialized tag.
 module Rouge
   module Lexers
     class JSP < TemplateLexer
@@ -17,15 +26,19 @@ module Rouge
       directives = %w(page include taglib)
       actions = %w(scriptlet declaration expression)
 
-      state :root do
-
+      state :whitespace do
+        rule %r/\s+/, Text
         rule %r/<%--/, Comment, :jsp_comment
+      end
 
-        rule %r/<%@\s*(#{directives.join('|')})\s*/, Name::Tag, :jsp_directive
+      state :root do
+        mixin :whitespace
 
-        rule %r/<jsp:directive\.(#{directives.join('|')})/, Name::Tag, :jsp_directive2
+        rule %r/<%@\s*\w+/, Name::Tag, :jsp_directive
 
-        rule %r/<jsp:(#{actions.join('|')})>/, Name::Tag, :jsp_expression
+        rule %r/<jsp:directive[.]\w+/, Name::Tag, :jsp_directive2
+
+        rule %r/<jsp:\w+>/, Name::Tag, :jsp_expression
 
         # start of tag, e.g. <c:if>
         rule %r/<[a-zA-Z]*:[a-zA-Z]*\s*/, Name::Tag, :jsp_tag
@@ -42,36 +55,38 @@ module Rouge
 
       state :jsp_comment do
         rule %r/(--%>)/, Comment, :pop!
+        rule %r/[^-]+/m, Comment
         rule %r/./m, Comment
       end
 
       state :jsp_directive do
         rule %r/(%>)/, Name::Tag, :pop!
         mixin :attributes
-        rule(/(.+?)(?=%>)/m) { delegate parent }
+        rule(/.+?(?=%>)/m) { delegate parent }
       end
 
       state :jsp_directive2 do
         rule %r!(/>)!, Name::Tag, :pop!
         mixin :attributes
-        rule(/(.+?)(?=\/>)/m) { delegate parent }
+        rule(/.+?(?=\/>)/m) { delegate parent }
       end
 
       state :jsp_expression do
-        rule %r/<\/jsp:(#{actions.join('|')})>/, Name::Tag, :pop!
+        rule %r(</jsp:\w+>), Name::Tag, :pop!
         mixin :attributes
-        rule(/[^<\/]+/) { delegate @java }
+        rule(%r([^<\/]+)) { delegate @java }
       end
 
       state :jsp_expression2 do
         rule %r/%>/, Name::Tag, :pop!
-        rule(/[^%>]+/) { delegate @java }
+        rule(/.+?(?=%>|\z)/m) { delegate @java }
       end
 
       state :jsp_tag do
         rule %r/\/?>/, Name::Tag, :pop!
         mixin :attributes
         rule(/(.+?)(?=\/?>)/m) { delegate parent }
+        # rule(%r{.+?(?=/?>)}m) { delegate parent }
       end
 
       state :attributes do

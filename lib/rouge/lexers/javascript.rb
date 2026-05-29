4f354d162cd5c3f2f2124da9c3e2fafe25a7cc26
jneen <jneen@jneen.net>
2026-05-29 11:33:43 -0400

    use the keywords api for javascript, but more

--- lib/rouge/lexers/javascript.rb
+++ lib/rouge/lexers/javascript.rb
@@ -190,13 +190,9 @@ module Rouge
 
         rule %r/function(?=(\(.*\)))/, Keyword::Declaration # For anonymous functions
 
-        rule %r/(#?#{id})[ \t]*(?=(\(.*\)))/m do |m|
-          if self.class.keywords.include?(m[1])
-            # "if" in "if (...)" or "switch" in "switch (...)" are recognized as keywords.
-            token Keyword
-          else
-            token Name::Function
-          end
+        keywords %r/#?#{id}(?=[ \t]*[(])/m do
+          rule :keywords, Keyword
+          default Name::Function
         end
 
         rule %r/[{}]/, Punctuation, :statement

d75b2900347bd40bf80cd3e3614012b7bc7b2373
jneen <jneen@jneen.net>
2026-06-09 22:40:36 -0400

    use the keywords api for OCL

--- lib/rouge/lexers/ocl.rb
+++ lib/rouge/lexers/ocl.rb
@@ -8,47 +8,37 @@ module Rouge
       filenames '*.ocl'
       mimetypes 'text/x-ocl'
 
-      def self.keywords
-        @keywords ||= Set.new %w(
-          context pre post inv init body def derive if then else endif import
-          package endpackage let in
-        )
-      end
+      KEYWORDS = Set.new %w(
+        context pre post inv init body def derive if then else endif import
+        package endpackage let in
+      )
 
-      def self.keywords_type
-        @keywords_type ||= Set.new %w(
-          Boolean Integer UnlimitedNatural Real String OrderedSet Tuple Bag Set
-          Sequence OclInvalid OclVoid TupleType OclState Collection OclMessage
-        )
-      end
+      KEYWORDS_TYPE = Set.new %w(
+        Boolean Integer UnlimitedNatural Real String OrderedSet Tuple Bag Set
+        Sequence OclInvalid OclVoid TupleType OclState Collection OclMessage
+      )
 
-      def self.builtins
-        @builtins ||= Set.new %w(
-          self null result true false invalid @pre
-          )
-      end
+      BUILTINS = Set.new %w(
+        self null result true false invalid @pre
+      )
 
-      def self.operators
-        @operators ||= Set.new %w(
-          or xor and not implies
-        )
-      end
+      OPERATORS = Set.new %w(
+        or xor and not implies
+      )
 
-      def self.functions
-        @functions ||= Set.new %w(
-          oclAsSet oclIsNew oclIsUndefined oclIsInvalid oclAsType oclIsTypeOf
-          oclIsKindOf oclInState oclType oclLocale hasReturned result
-          isSignalSent isOperationCallabs floor round max min toString div mod
-          size substring concat toInteger toReal toUpperCase toLowerCase
-          indexOf equalsIgnoreCase at characters toBoolean includes excludes
-          count includesAll excludesAll isEmpty notEmpty sum product
-          selectByKind selectByType asBag asSequence asOrderedSet asSet flatten
-          union intersection including excluding symmetricDifferencecount
-          append prepend insertAt subOrderedSet first last reverse subSequence
-          any closure collect collectNested exists forAll isUnique iterate one
-          reject select sortedBy allInstances average conformsTo
-        )
-      end
+      FUNCTIONS = Set.new %w(
+        oclAsSet oclIsNew oclIsUndefined oclIsInvalid oclAsType oclIsTypeOf
+        oclIsKindOf oclInState oclType oclLocale hasReturned result
+        isSignalSent isOperationCallabs floor round max min toString div mod
+        size substring concat toInteger toReal toUpperCase toLowerCase
+        indexOf equalsIgnoreCase at characters toBoolean includes excludes
+        count includesAll excludesAll isEmpty notEmpty sum product
+        selectByKind selectByType asBag asSequence asOrderedSet asSet flatten
+        union intersection including excluding symmetricDifferencecount
+        append prepend insertAt subOrderedSet first last reverse subSequence
+        any closure collect collectNested exists forAll isUnique iterate one
+        reject select sortedBy allInstances average conformsTo
+      )
 
       state :single_string do
         rule %r/\\./, Str::Escape
@@ -63,20 +53,14 @@ module Rouge
         rule %r/'/, Str::Single, :single_string
         rule %r([-|+*/<>=~!@#%&?^]), Operator
         rule %r/[;:()\[\],.]/, Punctuation
-        rule %r/[a-zA-Z]\w*/ do |m|
-          if self.class.operators.include? m[0]
-            token Operator
-          elsif self.class.keywords_type.include? m[0]
-            token Keyword::Declaration
-          elsif self.class.keywords.include? m[0]
-            token Keyword
-          elsif self.class.builtins.include? m[0]
-            token Name::Builtin
-          elsif self.class.functions.include? m[0]
-            token Name::Function
-          else
-            token Name
-          end
+
+        keywords %r/[a-zA-Z]\w*/ do
+          rule OPERATORS, Operator
+          rule KEYWORDS_TYPE, Keyword::Declaration
+          rule KEYWORDS, Keyword
+          rule BUILTINS, Name::Builtin
+          rule FUNCTIONS, Name::Function
+          default Name
         end
       end
     end
